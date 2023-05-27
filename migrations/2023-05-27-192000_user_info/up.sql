830c1a4d2bd2115df1ebf2da14dee26b47427688
Nicholas Khami <nicholas.khami@gmail.com>
2023-05-27 16:14:22 -0500

    migration: users table now has website username and visible_email

--- migrations/2023-05-27-192000_user_info/up.sql
+++ migrations/2023-05-27-192000_user_info/up.sql
@@ -1,4 +1,9 @@
 -- Your SQL goes here
 ALTER TABLE users
-ADD COLUMN username TEXT NULL,
-ADD COLUMN visible_email BOOLEAN NULL DEFAULT false;
+ADD COLUMN username TEXT NULL UNIQUE,
+ADD COLUMN website TEXT NULL,
+ADD COLUMN visible_email BOOLEAN DEFAULT false;
+
+-- Set visible_email to false for all existing rows
+UPDATE users
+SET visible_email = false;
