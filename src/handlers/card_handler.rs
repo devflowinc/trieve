c8e7e14cd5824faf861a328721593cae47286172
Denzell Ford <fdenzell@gmail.com>
2023-05-27 20:22:04 -0500

    feat: get_card_by_id

--- src/handlers/card_handler.rs
+++ src/handlers/card_handler.rs
@@ -4,9 +4,12 @@ use serde::{Deserialize, Serialize};
 use serde_json::json;
 
 use crate::data::models::{CardMetadata, Pool};
-use crate::operators::card_operator::{create_openai_embedding, insert_card_metadata_query, get_metadata_from_point_ids};
 use crate::operators::card_operator::{
-    get_point_by_id_query, get_qdrant_connection, retrieved_point_to_card_dto, search_card_query,
+    create_openai_embedding, get_metadata_from_point_ids, insert_card_metadata_query,
+};
+use crate::operators::card_operator::{
+    get_metadata_from_id_query, get_qdrant_connection,
+    search_card_query,
 };
 
 use super::auth_handler::LoggedUser;
@@ -31,7 +34,7 @@ pub async fn create_card(
         let mut similarity_threashold = 0.95;
         if card.content.len() < 200 {
             similarity_threashold = 0.9;
-        } 
+        }
 
         if score_card.score >= similarity_threashold {
             return Ok(HttpResponse::BadRequest().json(json!({
@@ -46,22 +49,23 @@ pub async fn create_card(
         .map_err(|err| actix_web::error::ErrorBadRequest(err.message))?;
 
     let payload: qdrant_client::prelude::Payload = json!({}).try_into().unwrap();
-    
+
     let point_id = uuid::Uuid::new_v4();
     let point = PointStruct::new(point_id.clone().to_string(), embedding_vector, payload);
 
     web::block(move || {
         insert_card_metadata_query(
             CardMetadata::from_details(&card.content, user.id, point_id),
-            &pool
+            &pool,
         )
-    }).await?.map_err(actix_web::error::ErrorBadRequest)?;
+    })
+    .await?
+    .map_err(actix_web::error::ErrorBadRequest)?;
 
     qdrant
         .upsert_points_blocking("debate_cards".to_string(), vec![point], None)
         .await
         .map_err(actix_web::error::ErrorBadRequest)?;
-
 
     Ok(HttpResponse::NoContent().finish())
 }
@@ -82,7 +86,6 @@ pub async fn search_card(
     page: Option<web::Path<u64>>,
     pool: web::Data<Pool>,
 ) -> Result<HttpResponse, actix_web::Error> {
-
     let page = page.map(|page| page.into_inner()).unwrap_or(1);
     let embedding_vector = create_openai_embedding(&data.content).await?;
 
@@ -100,26 +103,22 @@ pub async fn search_card(
     let score_cards: Vec<ScoreCardDTO> = metadata_cards
         .iter()
         .zip(search_results.iter())
-        .map(|(card, point)| {
-            ScoreCardDTO {
-                metadata: (*card).clone(),
-                score: point.score,
-            }
-        }).collect();
+        .map(|(card, point)| ScoreCardDTO {
+            metadata: (*card).clone(),
+            score: point.score,
+        })
+        .collect();
 
     Ok(HttpResponse::Ok().json(score_cards))
 }
 
 pub async fn get_card_by_id(
     card_id: web::Path<uuid::Uuid>,
+    pool: web::Data<Pool>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    let card = match get_point_by_id_query(card_id.into_inner()).await? {
-        Some(point) => retrieved_point_to_card_dto(point).await,
-        None => return Ok(HttpResponse::BadRequest().finish()),
-    };
+    let card = web::block(|| get_metadata_from_id_query(card_id.into_inner(), pool))
+        .await?
+        .map_err(actix_web::error::ErrorBadRequest)?;
 
-    match card {
-        Some(card) => Ok(HttpResponse::Ok().json(card)),
-        None => Ok(HttpResponse::BadRequest().finish()),
-    }
+    Ok(HttpResponse::Ok().json(card))
 }
