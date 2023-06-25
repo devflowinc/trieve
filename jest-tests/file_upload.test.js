f4cd083a472f388582504b3a724bbbc0fb4a22ff
Nicholas Khami <nicholas.khami@gmail.com>
2023-06-25 15:53:08 -0500

    bugfix: testing suite base64 encode type

--- jest-tests/file_upload.test.js
+++ jest-tests/file_upload.test.js
@@ -1,9 +1,13 @@
 import { readdirSync, readFileSync } from 'fs';
 import mime from 'mime';
 import fetch from 'node-fetch';
+import { getAuthCookie } from './auth';
+
+const api_endpoint = process.env.API_ENDPOINT || 'http://localhost:8090/api';
 
 describe('File Upload Test', () => {
-  test('Uploads the first docx file in the directory', async () => {
+  test('User can upload the first docx file in the directory', async () => {
+    const authCookie = await getAuthCookie();
     const directoryPath = './demo-files/';
 
     // Read the first docx file in the directory
@@ -12,7 +16,7 @@ describe('File Upload Test', () => {
     const filePath = `${directoryPath}/${docxFile}`;
 
     // Read the file contents and convert to base64
-    const fileContent = readFileSync(filePath, 'base64');
+    const fileContent = readFileSync(filePath, 'base64url');
     const mimeType = mime.getType(filePath);
 
     // Prepare the request body
@@ -23,16 +27,20 @@ describe('File Upload Test', () => {
       private: true,
     };
 
-    const api_endpoint = process.env.API_ENDPOINT || 'http://localhost:8090/api';
-
     // Make the POST request
     const response = await fetch(`${api_endpoint}/upload_file`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        'Cookie': authCookie,
       },
+      credentials: 'include',
       body: JSON.stringify(requestBody),
     });
+
+    if (!response.ok) {
+      console.error(await response.json());
+    }
 
     // Assert the response or perform further checks if needed
     expect(response.status).toBe(200);
