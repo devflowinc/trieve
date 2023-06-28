cb0d7347649e8012dde44d508316ea6cd19a1210
densumesh <dens.sumesh79@gmail.com>
2023-06-28 13:24:04 -0700

    Cleanup: fixed typo in migration

--- migrations/2023-06-27-225525_file-to-collection-map/up.sql
+++ migrations/2023-06-27-225525_file-to-collection-map/up.sql
@@ -2,7 +2,7 @@
 CREATE TABLE collections_from_files (
     id UUID PRIMARY KEY,
     collection_id UUID NOT NULL REFERENCES card_collection (id) ON DELETE CASCADE,
-    file_id UUID NOT NULL REFERENCES files (id) ON DeLETE CASCADE,
+    file_id UUID NOT NULL REFERENCES files (id) ON DELETE CASCADE,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
