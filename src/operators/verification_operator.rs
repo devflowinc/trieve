d413b99bf835dc04958ad9d33c11e5d893380134
densumesh <dens.sumesh79@gmail.com>
2023-07-14 16:07:55 -0700

    WIP: convert pdfs to html

--- src/operators/verification_operator.rs
+++ src/operators/verification_operator.rs
@@ -1,8 +1,12 @@
 use diesel::prelude::*;
 use headless_chrome::{Browser, LaunchOptionsBuilder};
+
 use regex::Regex;
 use soup::{NodeExt, QueryBuilderExt};
-use std::sync::{Arc, Mutex};
+use std::{
+    process::Command,
+    sync::{Arc, Mutex},
+};
 
 use actix_web::web;
 
@@ -57,17 +61,61 @@ pub async fn get_webpage_text_headless(url: &str) -> Result<String, DefaultError
 }
 
 pub async fn get_webpage_text_fetch(url: &str) -> Result<String, DefaultError> {
-    let html = reqwest::get(url)
-        .await
-        .map_err(|_| DefaultError {
-            message: "Could not fetch page",
-        })?
-        .text()
-        .await
-        .map_err(|_| DefaultError {
+    let response = reqwest::get(url).await.map_err(|_| DefaultError {
+        message: "Could not fetch page",
+    })?;
+
+    let headers = response.headers().get("content-type").ok_or(DefaultError {
+        message: "Could not get content type",
+    })?;
+    let html;
+    if headers.to_str().unwrap_or("").contains("text/html") {
+        html = response.text().await.map_err(|_| DefaultError {
             message: "Could not parse text",
         })?;
+    } else if headers == "application/pdf" {
+        let pdf_file_path = format!("./tmp/{}.pdf", response.url().to_string().replace('/', ""));
+        let html_file_path = format!("./tmp/{}.html", response.url().to_string().replace('/', ""));
 
+        let pdf = response.bytes().await.map_err(|_| DefaultError {
+            message: "Could not parse pdf",
+        })?;
+
+        std::fs::write(&pdf_file_path, &pdf).map_err(|_| DefaultError {
+            message: "Could not write file to disk",
+        })?;
+
+        let conversion_command_output =
+            Command::new(std::env::var("LIBREOFFICE_PATH").expect("LIBREOFFICE_PATH must be set"))
+                .arg("--headless")
+                .arg("--convert-to")
+                .arg("html")
+                .arg("--outdir")
+                .arg("./tmp")
+                .arg(&pdf_file_path)
+                .output();
+
+        if conversion_command_output.is_err() {
+            return Err(DefaultError {
+                message: "Could not convert file",
+            });
+        }
+
+        html = std::fs::read_to_string(&html_file_path).map_err(|_| DefaultError {
+            message: "Could not read text file",
+        })?;
+
+        std::fs::remove_file(&pdf_file_path).map_err(|_| DefaultError {
+            message: "Could not remove temp docx file",
+        })?;
+        std::fs::remove_file(&html_file_path).map_err(|_| DefaultError {
+            message: "Could not remove temp html file",
+        })?;
+    } else {
+        return Err(DefaultError {
+            message: "Could not parse content type",
+        });
+    }
     let soup = soup::Soup::new(&html);
 
     let body = soup.tag("body").find().ok_or(DefaultError {
@@ -76,6 +124,7 @@ pub async fn get_webpage_text_fetch(url: &str) -> Result<String, DefaultError> {
 
     // Replace multiple whitesapces chars with a single space
     let text = body.text();
+
     let re = Regex::new(r"\s+").unwrap();
     let clean_text = re.replace_all(&text, " ").to_string();
 
