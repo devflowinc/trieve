33b2ea284e750f27dc7a89353b1d8fb188e9f944
Nick K <nick@arguflow.gg>
2023-07-17 13:04:28 -0400

    wip: use python for card parsing

---
+++ convenience.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Function to reset the Qdrant database
+reset_qdrant_database() {
+    echo "Resetting the Qdrant database..."
+    sudo docker-compose stop qdrant-database
+    sudo docker-compose rm -f qdrant-database
+    sudo docker volume rm ai-editor_qdrant_data
+    diesel db reset
+}
+
+# Function to set up the Python environment
+setup_python_environment() {
+    echo "Setting up the Python environment..."
+    virtualenv venv
+    source venv/bin/activate
+    pip install -r ./python-scripts/requirements.txt
+}
+
+# Function to reset the script database
+reset_script_redis() {
+    echo "Resetting the script Redis database..."
+    sudo docker-compose stop script-redis
+    sudo docker-compose rm -f script-redis
+    sudo docker volume rm ai-editor_script-redis-data
+}
+
+# Main script logic
+while getopts ":qps" opt; do
+    case $opt in
+        q)
+            reset_qdrant_database
+            ;;
+        p)
+            setup_python_environment
+            ;;
+        s)
+            reset_script_redis
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
+done
+
--- src/handlers/verification_handler.rs
+++ src/handlers/verification_handler.rs
@@ -27,7 +27,7 @@ pub async fn get_webpage_score(
         .map_err(|err| ServiceError::BadRequest(format!("Could not fetch: {}", err)))?;
 
     let fuzzy_script_result = Command::new("python3")
-        .arg("./validator-scripts/fuzzy-text-match.py")
+        .arg("./python-scripts/fuzzy-text-match.py")
         .arg(content)
         .arg(webpage_content)
         .output();
@@ -63,7 +63,7 @@ pub async fn get_webpage_score(
             .map_err(|err| ServiceError::BadRequest(format!("Could not fetch: {}", err)))?;
 
         let fuzzy_script_result = Command::new("python3")
-            .arg("./validator-scripts/fuzzy-text-match.py")
+            .arg("./python-scripts/fuzzy-text-match.py")
             .arg(content)
             .arg(webpage_content)
             .output();
