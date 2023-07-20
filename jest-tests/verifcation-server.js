51ea8a9834a62f5d2a30557fc97d84c34c1fbac9
fastfist <fdenzell@gmail.com>
2023-07-20 15:40:17 -0500

    feature: added logging to verification server

--- jest-tests/verifcation-server.js
+++ jest-tests/verifcation-server.js
@@ -1,5 +1,6 @@
 import http from 'http';
 import puppeteer from 'puppeteer-extra';
+import winston from 'winston';
 
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 puppeteer.use(StealthPlugin())
@@ -10,6 +11,28 @@ puppeteer.launch({ headless: true }).then(br => {
 }).catch(err => {
   console.error(err);
 });
+
+
+
+// Define log format
+const logFormat = winston.format.printf(({ level, message, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`;
+});
+
+// Create logger instance
+const log = winston.createLogger({
+  level: 'info', // Set the minimum log level
+  format: winston.format.combine(
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    logFormat
+  ),
+  transports: [
+    // Define transports (where the logs will be stored)
+    new winston.transports.Console(), // Log to console
+    new winston.transports.File({ filename: 'logs.log' }), // Log to a file
+  ],
+});
+
 
 async function fetchPageContents(url) {
   const page = await browser.newPage();
@@ -55,29 +78,33 @@ const server = http.createServer((req, res) => {
     req.on('end', () => {
       try {
         const jsonData = JSON.parse(requestBody);
-        console.log(jsonData);
         if (jsonData.url === undefined) {
           res.statusCode = 400;
+          log.info(`${requestBody} 400 Invalid JSON data`);
           res.end(JSON.stringify({ error: 'Invalid JSON data' }));
         }
         fetchPageContents(jsonData.url).then((pageContent) => {
           res.statusCode = 200;
+          log.info(`${requestBody} 200 valid`);
           res.end(JSON.stringify({ content: pageContent }));
         }).catch((err) => {
           console.error(err);
           res.statusCode = 400;
+          log.info(`${requestBody} 400 Invalid JSON data`);
           res.end(JSON.stringify({ error: 'Failed fetching page contents' }));
         })
       } catch (error) {
         console.error(error);
         // Handle JSON parsing error
         res.statusCode = 400;
+        log.info(`${requestBody} 400 Invalid JSON data`);
         res.end(JSON.stringify({ error: 'Invalid JSON data' }));
       }
     });
   } else {
     // Handle other routes
     res.statusCode = 404;
+    log.info('404 Not Found');
     res.end(JSON.stringify({ error: 'Route not found' }));
   }
 });
@@ -85,5 +112,5 @@ const server = http.createServer((req, res) => {
 // Start the server
 const port = 8091;
 server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  log.info(`Server running on port ${port}`);
 });
