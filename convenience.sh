3eb03393164da29318f1da82de4a31ca54e7c9f4
Nicholas Khami <nicholas.khami@gmail.com>
2023-07-23 09:58:12 -0500

    feature: renames to prepare for release

--- convenience.sh
+++ convenience.sh
@@ -5,7 +5,7 @@ reset_qdrant_database() {
     echo "Resetting the Qdrant database..."
     sudo docker-compose stop qdrant-database
     sudo docker-compose rm -f qdrant-database
-    sudo docker volume rm ai-editor_qdrant_data
+    sudo docker volume rm vault_qdrant_data
     sudo docker compose up -d qdrant-database
     diesel db reset
 }
@@ -14,7 +14,7 @@ reset_s3_service() {
     echo "Resetting the S3 service..."
     sudo docker-compose stop s3
     sudo docker-compose rm -f s3
-    sudo docker volume rm ai-editor_s3-data
+    sudo docker volume rm vault_s3-data
     sudo docker compose up -d s3
 }
 
@@ -23,7 +23,7 @@ setup_python_environment() {
     echo "Setting up the Python environment..."
     virtualenv venv
     source venv/bin/activate
-    pip install -r ./python-scripts/requirements.txt
+    pip install -r ./vault-python/requirements.txt
 }
 
 # Function to reset the script database
@@ -31,7 +31,7 @@ reset_script_redis() {
     echo "Resetting the script Redis database..."
     sudo docker compose stop script-redis
     sudo docker compose rm -f script-redis
-    sudo docker volume rm ai-editor_script-redis-data
+    sudo docker volume rm vault_script-redis-data
     sudo docker compose up -d script-redis
 }
 
