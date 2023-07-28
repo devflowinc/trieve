4f4f2842a64c936054abf43073f8f5cee7e8cab3
Nick K <nick@arguflow.gg>
2023-07-28 18:01:57 -0400

    feature: use qdrant to handle private flags

--- src/operators/mod.rs
+++ src/operators/mod.rs
@@ -5,6 +5,7 @@ pub mod file_operator;
 pub mod message_operator;
 pub mod notification_operator;
 pub mod password_reset_operator;
+pub mod qdrant_operator;
 pub mod stripe_customer_operator;
 pub mod topic_operator;
 pub mod user_operator;
