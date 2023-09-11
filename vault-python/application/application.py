3caee0ff77d1dfec365644d9d216887c9847b524
densumesh <dens.sumesh79@gmail.com>
2023-09-11 15:31:05 -0700

    feature: add ability to switch models

--- vault-python/application/application.py
+++ vault-python/application/application.py
@@ -1,3 +1,4 @@
+from typing import Union
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 from pydantic import BaseModel
@@ -7,6 +8,7 @@ from transformers import pipeline, AutoTokenizer
 import nltk
 import uvicorn
 from fastapi.responses import HTMLResponse
+from extractive import ExtractiveSummarizer
 
 nltk.download("punkt")
 application = FastAPI()
@@ -25,26 +27,38 @@ summarizer = pipeline(
     device=0,
 )
 
+model = ExtractiveSummarizer.load_from_checkpoint(
+    "~/transformersum/src/trained_models/transformerextsum-private/xxr7yguk/checkpoints/epoch=2-step=256146.ckpt"
+)
+
 
 class AutoCutReqeuest(BaseModel):
     input: str
+    num_sentences: Union[int, None] = None
+    model: Union[str, None] = None
 
 
 @application.post("/cut", response_class=HTMLResponse)
 async def auto_cut(rq: AutoCutReqeuest):
     sentences = nltk.sent_tokenize(rq.input)
-    summary = summarizer(
-        {"sentences": sentences},
-        strategy="count",
-        strategy_args=round(len(sentences) * 0.3),
-    )
-
-    # Create a list to store the closest match for each target sentence
-
-    for target_sentence in summary[0]:
-        # Tokenize and process the target sentence
-        # Create a list of sentences from the larger list
-
+    if rq.model == None:
+        summary = summarizer(
+            {"sentences": sentences},
+            strategy="count",
+            strategy_args=rq.num_sentences
+            if rq.num_sentences
+            else round(len(sentences) * 0.3),
+        )
+        summary_sentences = summary[0]
+    else:
+        summary = model.predict(
+            rq.input,
+            num_summary_sentences=rq.num_sentences
+            if rq.num_sentences
+            else round(len(sentences) * 0.3),
+        )
+        summary_sentences = [sum for sum in summary.split(".") if sum]
+    for target_sentence in summary_sentences:
         # Vectorize the sentences using CountVectorizer
         vectorizer = CountVectorizer().fit_transform([target_sentence] + sentences)
 
@@ -59,6 +73,9 @@ async def auto_cut(rq: AutoCutReqeuest):
         sentences[closest_match_index - 1] = (
             "<b><u>" + sentences[closest_match_index - 1] + "</u></b>"
         )
+
+    # Create a list to store the closest match for each target sentence
+
     return " ".join(sentences)
 
 
