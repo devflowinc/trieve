e4f0c3cfdbef41bb7d48becf9eb054109fadff06
fastfist <fdenzell@gmail.com>
2023-09-15 16:39:42 -0500

    feat: Added Caddy file and public directory for chat

---
+++ chat/public/sw.js
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+self.addEventListener("install", (_event) => {
+  // Do nothing
+});
+
+self.addEventListener("activate", (_event) => {
+  // Do nothing
+});
+
+self.addEventListener("fetch", (_event) => {
+  // Do nothing
+});
