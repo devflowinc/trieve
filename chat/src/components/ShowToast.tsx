5237dfa8b1b27560f6bab35a77f3ee5c1bdb8090
Nick K <nick@arguflow.gg>
2023-09-23 16:15:57 -0500

    feature: always include Toast component so it can be triggered, styling: round toast and allow for infinite number

--- chat/src/components/ShowToast.tsx
+++ chat/src/components/ShowToast.tsx
@@ -1,42 +1,77 @@
-import { Show, createEffect, createSignal, onCleanup } from "solid-js";
-import { BsCheck2Circle } from 'solid-icons/bs'
-import { BiRegularErrorCircle } from 'solid-icons/bi'
-import { VsClose } from 'solid-icons/vs'
+import { For, Show, createEffect, createSignal, onCleanup } from "solid-js";
+import { BsCheck2Circle } from "solid-icons/bs";
+import { BiRegularErrorCircle } from "solid-icons/bi";
+import { VsClose } from "solid-icons/vs";
+
+export interface ToastDetail {
+  type: string;
+  message: string;
+}
+
+export interface ToastEvent {
+  detail: ToastDetail;
+}
 
 const ShowToast = () => {
-  const [isVisible, setIsVisible] = createSignal(false);
-  const [toastDetails, setToastDetails] = createSignal({ type: "", message: "" });
-  let timeOutId: number;
+  const [toastDetails, setToastDetails] = createSignal<ToastDetail[]>([]);
 
   createEffect(() => {
-    const showToastEvent = (event: any) => {
-      setIsVisible(true);
-      setToastDetails(event.detail);
+    let timeOutId: number;
+
+    const showToastEvent = (event: Event) => {
+      const toastEvent = event as unknown as ToastEvent;
+      setToastDetails((prev) => prev.concat(toastEvent.detail));
 
       timeOutId = setTimeout(() => {
-        setIsVisible(false);
+        setToastDetails((prev) =>
+          prev.filter(
+            (prevToastDetail) => prevToastDetail !== toastEvent.detail,
+          ),
+        );
       }, 5000);
     };
 
     window.addEventListener("show-toast", showToastEvent);
+
     onCleanup(() => {
       clearTimeout(timeOutId);
+      window.removeEventListener("show-toast", showToastEvent);
     });
   });
 
-  function handleToast() {
-    setIsVisible(false);
-  }
-
   return (
-    <Show when={isVisible()}>
-      <div class={`flex items-center dark:bg-neutral-900 bg-gray-100 text-center z-100 justify-between space-x-4 w-auto fixed top-10 right-5 px-5 py-2 border-t-4 ${toastDetails().type === "success" ? "border-green-600" : "border-red-700"} shadow-lg`}>
-        {toastDetails().type === "success" ? <BsCheck2Circle class="text-green-600" size={25}/> : <BiRegularErrorCircle class="text-red-700" size={20}/>}
-        <p class="text-md">{toastDetails().message}</p>
-        <VsClose cursor="pointer" onClick={handleToast} size={25}/>
-      </div>
-    </Show>
+    <div class="z-100 fixed right-5 top-10 flex flex-col space-y-2 rounded">
+      <For each={toastDetails()}>
+        {(toastDetail) => (
+          <div
+            class={`flex w-auto items-center justify-between space-x-4 rounded border-t-4 bg-gray-100 px-5 py-2 text-center dark:bg-neutral-900 ${
+              toastDetail.type === "success"
+                ? "border-green-600"
+                : "border-red-700"
+            } shadow-lg`}
+          >
+            {toastDetail.type === "success" ? (
+              <BsCheck2Circle class="text-green-600" size={25} />
+            ) : (
+              <BiRegularErrorCircle class="text-red-700" size={20} />
+            )}
+            <p class="text-md">{toastDetail.message}</p>
+            <VsClose
+              cursor="pointer"
+              onClick={() => {
+                setToastDetails((prev) =>
+                  prev.filter(
+                    (prevToastDetail) => prevToastDetail !== toastDetail,
+                  ),
+                );
+              }}
+              size={25}
+            />
+          </div>
+        )}
+      </For>
+    </div>
   );
-}
+};
 
 export default ShowToast;
