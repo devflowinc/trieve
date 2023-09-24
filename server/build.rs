88dc345327f80090dd0931284f7bdebd048a9d7a
fastfist <fdenzell@gmail.com>
2023-09-24 07:04:51 -0500

    devops: add feature flag to use .env.dist if needed

--- server/build.rs
+++ server/build.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::error::Error;
 
+#[cfg(feature = "require-env")]
 fn main() -> Result<(), Box<dyn Error>> {
     dotenvy::dotenv().expect("Failed to read .env file. Did you `cp .env.dist .env` ?");
 
@@ -11,4 +12,18 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("cargo:rerun-if-changed=.env");
 
     Ok(())
+}
+
+#[cfg(not(feature = "require-env"))]
+fn main() -> Result<(), Box<dyn Error>> {
+    dotenvy::from_filename(".env.dist").expect("Failed to read from .env.dist file");
+
+    for (key, value) in env::vars() {
+        println!("cargo:rustc-env={key}={value}");
+    }
+
+    println!("cargo:rerun-if-changed=.env.dist");
+
+    Ok(())
+
 }
