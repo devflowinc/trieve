5200d93072172a9b7d7a16c70138ee57f46c6d48
Nick K <nick@arguflow.gg>
2023-09-24 17:53:41 -0500

    feature: completed rag chat and normal chat with new system

--- chat/src/components/ScoreCard.tsx
+++ chat/src/components/ScoreCard.tsx
@@ -1,9 +1,9 @@
-import { Show, createEffect, createSignal } from "solid-js";
-import type {
-  CardBookmarksDTO,
-  CardCollectionDTO,
-  CardMetadataWithVotes,
-  FileDTO,
+import { For, Show, createEffect, createSignal } from "solid-js";
+import {
+  indirectHasOwnProperty,
+  type CardBookmarksDTO,
+  type CardCollectionDTO,
+  type CardMetadataWithVotes,
 } from "../utils/apiTypes";
 import { BiRegularChevronDown, BiRegularChevronUp } from "solid-icons/bi";
 import {
@@ -38,6 +38,7 @@ export interface ScoreCardProps {
   score: number;
   initialExpanded?: boolean;
   bookmarks: CardBookmarksDTO[];
+  showExpand?: boolean;
 }
 
 export const getLocalTime = (strDate: string | Date) => {
@@ -54,6 +55,11 @@ export const getLocalTime = (strDate: string | Date) => {
 
 const ScoreCard = (props: ScoreCardProps) => {
   const api_host = import.meta.env.VITE_API_HOST as string;
+  const frontMatterVals = (
+    (import.meta.env.VITE_FRONTMATTER_VALS as string | undefined) ??
+    "link,tag_set,file_name"
+  ).split(",");
+  const searchURL = import.meta.env.VITE_SEARCH_URL as string;
 
   const [expanded, setExpanded] = createSignal(props.initialExpanded ?? false);
   const [userVote, setUserVote] = createSignal(0);
@@ -108,53 +114,6 @@ const ScoreCard = (props: ScoreCardProps) => {
     });
   };
 
-  function base64UrlToDownloadFile(base64Url: string, fileName: string) {
-    // Decode base64 URL encoded string
-    const base64Data = atob(base64Url.replace(/-/g, "+").replace(/_/g, "/"));
-
-    // Convert binary string to ArrayBuffer
-    const buffer = new ArrayBuffer(base64Data.length);
-    const array = new Uint8Array(buffer);
-    for (let i = 0; i < base64Data.length; i++) {
-      array[i] = base64Data.charCodeAt(i);
-    }
-
-    // Create Blob from ArrayBuffer
-    const blob = new Blob([buffer], { type: "application/octet-stream" });
-
-    // Create URL object
-    const url = URL.createObjectURL(blob);
-
-    // Create <a> element
-    const link = document.createElement("a");
-    link.href = url;
-    link.download = fileName;
-
-    // Programmatically click the link to trigger the file download
-    link.click();
-
-    // Clean up URL object
-    URL.revokeObjectURL(url);
-  }
-
-  const downloadFile = (e: Event) => {
-    e.stopPropagation();
-    e.preventDefault();
-    void fetch(`${api_host}/file/${props.card.file_id ?? ""}`, {
-      method: "GET",
-      credentials: "include",
-    }).then((response) => {
-      if (response.status === 401) {
-        return;
-      }
-
-      void response.json().then((data) => {
-        const file = data as FileDTO;
-        base64UrlToDownloadFile(file.base64url_content, file.file_name);
-      });
-    });
-  };
-
   const copyCard = () => {
     navigator.clipboard
       .write([
@@ -182,12 +141,10 @@ const ScoreCard = (props: ScoreCardProps) => {
     <div class="flex w-full flex-col items-center rounded-md bg-neutral-100 p-2 dark:!bg-neutral-800 lg:ml-2">
       <div class="flex w-full flex-col space-y-2">
         <div class="flex h-fit items-center space-x-1">
-          <Show when={!props.card.private}>
-            <Tooltip
-              body={<FiGlobe class="h-5 w-5 text-green-500" />}
-              tooltipText="Publicly visible"
-            />
-          </Show>
+          <Tooltip
+            body={<FiGlobe class="h-5 w-5 text-green-500" />}
+            tooltipText="Publicly visible"
+          />
           <div class="flex-1" />
           <Show when={!copied()}>
             <button class="h-fit" onClick={() => copyCard()}>
@@ -197,10 +154,7 @@ const ScoreCard = (props: ScoreCardProps) => {
           <Show when={copied()}>
             <FiCheck class="text-green-500" />
           </Show>
-          <a
-            title="Open"
-            href={`https://search.arguflow.ai/card/${props.card.id}`}
-          >
+          <a title="Open" href={`${searchURL}/card/${props.card.id}`}>
             <VsFileSymlinkFile class="h-5 w-5 fill-current" />
           </a>
         </div>
@@ -245,97 +199,77 @@ const ScoreCard = (props: ScoreCardProps) => {
             </Show>
           </div>
           <div class="flex w-full flex-col">
-            <Show when={props.card.link}>
-              <a
-                class="dark:text-turquoise-400 line-clamp-1 w-fit break-all text-magenta-500 underline"
-                target="_blank"
-                href={props.card.link ?? ""}
-              >
-                {props.card.link}
-              </a>
-            </Show>
-            <Show when={props.card.tag_set}>
-              <div class="flex space-x-2">
-                <span class="font-semibold text-neutral-800 dark:text-neutral-200">
-                  OC_Path:{" "}
-                </span>
-                <span class="line-clamp-1 break-all">
-                  {props.card.tag_set?.split("/").slice(0, -1).join("/")}
-                </span>
-              </div>
-            </Show>
-            <Show when={props.card.tag_set ?? props.card.file_name}>
-              <div class="flex space-x-2">
-                <span class="font-semibold text-neutral-800 dark:text-neutral-200">
-                  Brief:{" "}
-                </span>
-                <Show when={props.card.file_name}>
-                  <a
-                    class="dark:text-turquoise-400 line-clamp-1 cursor-pointer break-all text-magenta-500 underline"
-                    target="_blank"
-                    onClick={(e) => downloadFile(e)}
+            <For each={frontMatterVals}>
+              {(frontMatterVal) => (
+                <>
+                  <Show when={props.card.link && frontMatterVal == "link"}>
+                    <a
+                      class="dark:text-turquoise-400 line-clamp-1 w-fit break-all text-magenta-400 underline"
+                      target="_blank"
+                      href={props.card.link ?? ""}
+                    >
+                      {props.card.link}
+                    </a>
+                  </Show>
+                  <Show
+                    when={
+                      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                      frontMatterVal !== "link" &&
+                      props.card.metadata &&
+                      indirectHasOwnProperty(
+                        props.card.metadata,
+                        frontMatterVal,
+                      ) &&
+                      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
+                      (props.card.metadata as any)[frontMatterVal]
+                    }
                   >
-                    {props.card.file_name}
-                  </a>
-                </Show>
-              </div>
-            </Show>
-            <div class="grid w-fit auto-cols-min grid-cols-[1fr,3fr] gap-x-2 text-neutral-800 dark:text-neutral-200">
-              <Show when={props.score != 0}>
-                <span class="font-semibold">Similarity: </span>
-                <span>{props.score.toPrecision(3)}</span>
-              </Show>
-              <Show when={props.card.author}>
-                <span class="font-semibold">Author: </span>
-                <a
-                  href={`https://search.arguflow.ai/user/${
-                    props.card.author?.id ?? ""
-                  }`}
-                  class="line-clamp-1 break-all underline"
-                >
-                  {props.card.author?.username ?? props.card.author?.email}
-                </a>
-              </Show>
-              <span class="font-semibold">Created: </span>
-              <span>
-                {getLocalTime(props.card.created_at).toLocaleDateString()}
-              </span>
-            </div>
+                    <div class="flex space-x-2">
+                      <span class="font-semibold text-neutral-800 dark:text-neutral-200">
+                        {frontMatterVal}:{" "}
+                      </span>
+                      <span class="line-clamp-1 break-all">
+                        {props.card.metadata &&
+                          indirectHasOwnProperty(
+                            props.card.metadata,
+                            frontMatterVal,
+                          ) &&
+                          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
+                          (props.card.metadata as any)[frontMatterVal]}
+                      </span>
+                    </div>
+                  </Show>
+                </>
+              )}
+            </For>
           </div>
         </div>
       </div>
       <div class="mb-1 h-1 w-full border-b border-neutral-300 dark:border-neutral-600" />
-      <Show when={props.card.card_html == null}>
-        <p
-          classList={{
-            "line-clamp-4 gradient-mask-b-0": !expanded(),
-          }}
-        >
-          {props.card.content.toString()}
-        </p>
-      </Show>
-      <Show when={props.card.card_html != null}>
-        <div
-          classList={{
-            "line-clamp-4 gradient-mask-b-0": !expanded(),
-            "text-ellipsis max-w-[100%] break-words space-y-5 leading-normal":
-              true,
-          }}
-          // eslint-disable-next-line solid/no-innerhtml, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-          innerHTML={sanitizeHtml(
-            props.card.card_html !== undefined
-              ? props.card.card_html
-                  .replaceAll("line-height", "lh")
-                  .replace("\n", " ")
-                  .replace(`<br>`, " ")
-                  .replace(`\\n`, " ")
-              : "",
-            sanitzerOptions,
-          )}
-        />
-      </Show>
+      <div
+        classList={{
+          "line-clamp-4 gradient-mask-b-0": !expanded(),
+          "text-ellipsis max-w-[100%] break-words space-y-5 leading-normal":
+            true,
+        }}
+        // eslint-disable-next-line solid/no-innerhtml, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+        innerHTML={sanitizeHtml(
+          props.card.card_html !== undefined
+            ? props.card.card_html
+                .replaceAll("line-height", "lh")
+                .replace("\n", " ")
+                .replace(`<br>`, " ")
+                .replace(`\\n`, " ")
+            : "",
+          sanitzerOptions,
+        )}
+      />
       <button
-        class="font-semibold"
+        classList={{
+          "ml-2 font-semibold": true,
+          "text-neutral-300 dark:text-neutral-600": !props.showExpand,
+        }}
+        disabled={!props.showExpand}
         onClick={() => setExpanded((prev) => !prev)}
       >
         {expanded() ? (
