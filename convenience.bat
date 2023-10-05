a45f884a5c4a9918bfcee65d1a4b5d072c2a6967
poke5352 <poke5352@gmail.com>
2023-10-04 18:16:19 -0700

    feature: add convenience batch script for Windows

---
+++ convenience.bat
@@ -0,0 +1,77 @@
+@echo off
+
+if "%1"=="q" (
+    call :reset_qdrant_database
+)
+
+if "%1"=="p" (
+    call :setup_python_environment
+)
+
+if "%1"=="3" (
+    call :reset_s3_service
+)
+
+if "%1"=="s" (
+    call :reset_script_redis
+)
+
+if "%1"=="l" (
+    call :start_local_services
+)
+
+if "%1"=="" (
+    echo "Usage: ./convenience.bat [q|p|3|s|l]"
+    echo "q - reset the Qdrant database"
+    echo "p - set up the Python environment"
+    echo "3 - reset the S3 service"
+    echo "s - reset the script Redis database"
+    echo "l - start local services"
+)
+
+EXIT /B %ERRORLEVEL%
+
+rem Function to reset the Qdrant database
+:reset_qdrant_database
+echo "Resetting the Qdrant database..."
+docker "compose" "stop" "qdrant-database"
+docker "compose" "rm" "-f" "qdrant-database"
+docker "volume" "rm" "arguflow_qdrant_data"
+docker "compose" "up" "-d" "qdrant-database"
+diesel "db" "reset"
+EXIT /B 0
+
+rem Function to reset the s3 serivce
+:reset_s3_service
+echo "Resetting the S3 service..."
+docker "compose" "stop" "s3"
+docker "compose" "rm" "-f" "s3"
+docker "volume" "rm" "vault_s3-data"
+docker "compose" "up" "-d" "s3"
+EXIT /B 0
+
+rem Function to set up the Python environment
+:setup_python_environment
+echo "Setting up the Python environment..."
+virtualenv "venv"
+source "venv\bin\activate"
+pip "install" "-r" "%CD%\server-python\requirements.txt"
+EXIT /B 0
+
+rem Function to reset the script database
+:reset_script_redis
+echo "Resetting the script Redis database..."
+docker "compose" "stop" "script-redis"
+docker "compose" "rm" "-f" "script-redis"
+docker "volume" "rm" "vault_script-redis-data"
+docker "compose" "up" "-d" "script-redis"
+EXIT /B 0
+
+:start_local_services
+echo "Starting local services..."
+docker "compose" "up" "-d" "db"
+docker "compose" "up" "-d" "redis"
+docker "compose" "up" "-d" "qdrant-database"
+docker "compose" "up" "-d" "s3"
+docker "compose" "up" "-d" "s3-client"
+EXIT /B 0
