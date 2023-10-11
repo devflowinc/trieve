b4799ccb48b0c291847afef51f38a001dea89d05
rohit-singh-bisht <thersbisht@gmail.com>
2023-10-11 23:25:41 +0530

    bugfix: when you are not auth'ed and get a 401 on the single card recommendations, it should show

--- search/src/components/CollectionPage.tsx
+++ search/src/components/CollectionPage.tsx
@@ -357,6 +357,10 @@ export const CollectionPage = (props: CollectionPageProps) => {
           setRecommendedCards(new_recommendations);
         });
       }
+      if (response.status == 401) {
+        setShowNeedLoginModal(true);
+        setLoadingRecommendations(false);
+      }
     });
   };
 
@@ -769,8 +773,8 @@ export const CollectionPage = (props: CollectionPageProps) => {
             <div class="min-w-[250px] sm:min-w-[300px]">
               <BiRegularXCircle class="mx-auto h-8 w-8 fill-current  !text-red-500" />
               <div class="mb-4 text-center text-xl font-bold">
-                Login or register to bookmark cards, vote, or view private
-                themes
+                Login or register to bookmark cards, vote, get recommend cards
+                or view private themes
               </div>
               <div class="mx-auto flex w-fit flex-col space-y-3">
                 <a
