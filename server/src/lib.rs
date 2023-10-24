5201756d868a9f378bd5c729be5481d5126472c5
Nick K <nick@arguflow.gg>
2023-10-23 20:36:10 -0500

    bugfix: make payload size limit 1gb

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -122,7 +122,8 @@ pub async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         App::new()
-            .app_data(PayloadConfig::new(2500000000000))
+            .app_data(PayloadConfig::new(134200000))
+            .app_data( web::JsonConfig::default().limit(134200000))
             .app_data(web::Data::new(pool.clone()))
             .wrap(
                 IdentityMiddleware::builder()
