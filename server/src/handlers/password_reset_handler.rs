9500a4f5eb1de9415bc41fc4e33315e56a253544
Nick K <nick@arguflow.gg>
2023-11-01 16:20:39 -0500

    feature: add OpenAPI spec for auth and password endpoints

--- server/src/handlers/password_reset_handler.rs
+++ server/src/handlers/password_reset_handler.rs
@@ -4,19 +4,31 @@ use crate::errors::DefaultError;
 use crate::operators::password_reset_operator::{reset_user_password, send_password_reset_email};
 use actix_web::{web, HttpRequest, HttpResponse};
 use serde::Deserialize;
+use utoipa::ToSchema;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, ToSchema)]
 pub struct PasswordResetData {
     pub password_reset_id: String,
     pub password: String,
     pub password_confirmation: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, ToSchema)]
 pub struct PasswordResetEmailData {
     pub email: String,
 }
 
+#[utoipa::path(
+    post,
+    path = "/password",
+    context_path = "/api",
+    tag = "password",
+    request_body(content = PasswordResetData, description = "JSON request payload to reset password", content_type = "application/json"),
+    responses(
+        (status = 204, description = "Confirmation that the password was reset"),
+        (status = 400, description = "Password either too short or did not match confirmation password", body = [DefaultError]),
+    )
+)]
 pub async fn reset_user_password_handler(
     password_reset_data: web::Json<PasswordResetData>,
     pool: web::Data<Pool>,
@@ -48,6 +60,20 @@ pub async fn reset_user_password_handler(
     }
 }
 
+#[utoipa::path(
+    get,
+    path = "/password/{email}",
+    context_path = "/api",
+    tag = "password",
+    request_body(content = PasswordResetEmailData, description = "JSON request payload to send email with password reset link", content_type = "application/json"),
+    responses(
+        (status = 204, description = "Confirmation that the password reset email was sent"),
+        (status = 400, description = "Email was invalid or sendgrid had an error", body = [DefaultError]),
+    ),
+    params(
+        ("email" = string, description = "Email of the user to send the password reset email to"),
+    )
+)]
 pub async fn send_password_reset_email_handler(
     request: HttpRequest,
     password_reset_email_data: web::Path<PasswordResetEmailData>,
