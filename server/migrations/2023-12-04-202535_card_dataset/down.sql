19b80e96029d2cc7c2b9a7cf9339bc40090a7769
fastfist <fdenzell@gmail.com>
2023-12-05 15:00:20 -0800

    wip: trying to change database migration to make dataset a TABLE

--- server/migrations/2023-12-04-202535_card_dataset/down.sql
+++ server/migrations/2023-12-04-202535_card_dataset/down.sql
@@ -1,3 +1,13 @@
+DROP TABLE IF EXISTS dataset;
+
 -- This file should undo anything in `up.sql`
 ALTER TABLE card_metadata
+DROP COLUMN IF EXISTS dataset;
+
+-- This file should undo anything in `up.sql`
+ALTER TABLE card_collection
+DROP COLUMN IF EXISTS dataset;
+
+-- This file should undo anything in `up.sql`
+ALTER TABLE card_collection_bookmarks
 DROP COLUMN IF EXISTS dataset;
--- server/migrations/2023-12-04-202535_card_dataset/up.sql
+++ server/migrations/2023-12-04-202535_card_dataset/up.sql
@@ -1,3 +1,40 @@
--- Your SQL goes here
+CREATE TABLE dataset (
+  id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+  name TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO dataset (name) VALUES ('DEFAULT');
+
+CREATE OR REPLACE FUNCTION set_default_dataset_id()
+RETURNS TRIGGER AS $$
+BEGIN
+  IF NEW.dataset_id IS NULL THEN
+    SELECT id INTO NEW.dataset_id FROM dataset WHERE name = 'DEFAULT';
+  END IF;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER set_default_dataset_id_trigger
+BEFORE INSERT ON card_metadata
+FOR EACH ROW EXECUTE FUNCTION set_default_dataset_id();
+
+CREATE TRIGGER set_default_dataset_id_trigger
+BEFORE INSERT ON card_collection
+FOR EACH ROW EXECUTE FUNCTION set_default_dataset_id();
+
+CREATE TRIGGER set_default_dataset_id_trigger
+BEFORE INSERT ON card_collection_bookmarks
+FOR EACH ROW EXECUTE FUNCTION set_default_dataset_id();
+
 ALTER TABLE card_metadata
-ADD COLUMN dataset TEXT NOT NULL default 'DEFAULT';
+ADD COLUMN dataset_id UUID NOT NULL;
+
+ALTER TABLE card_collection
+ADD COLUMN dataset_id UUID NOT NULL;
+
+ALTER TABLE card_collection_bookmarks
+ADD COLUMN dataset_id UUID NOT NULL;
