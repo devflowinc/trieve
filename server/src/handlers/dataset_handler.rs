8cc8b28158ba77fe73d18710f9153157526e38cc
fastfist <fdenzell@gmail.com>
2023-12-04 18:48:53 -0600

    feature: add dataset to tantivy index operator

--- server/src/handlers/dataset_handler.rs
+++ server/src/handlers/dataset_handler.rs
@@ -1,10 +1,16 @@
-use std::future::{ready, Ready};
+use std::{
+    future::{ready, Ready},
+    sync::RwLock,
+};
 
 use actix_web::{web, FromRequest, HttpResponse};
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
-use crate::{errors::ServiceError, operators::qdrant_operator};
+use crate::{
+    errors::ServiceError,
+    operators::{qdrant_operator, tantivy_operator::TantivyIndexMap},
+};
 
 use super::auth_handler::LoggedUser;
 
@@ -21,8 +27,21 @@ impl FromRequest for Dataset {
         req: &actix_web::HttpRequest,
         _payload: &mut actix_web::dev::Payload,
     ) -> Self::Future {
+        log::error!(
+            "Dataset from request, headers: {:?}",
+            req.headers().get("AF-Dataset")
+        );
         match req.headers().get("AF-Dataset") {
             Some(dataset_header) => match dataset_header.to_str() {
+                Ok(dataset) if dataset != "" && dataset != "undefined" => {
+                    log::error!("Dataset is {}", dataset);
+                    ready(Ok(Dataset {
+                        name: dataset.to_string(),
+                    }))
+                }
+                Ok(dataset) if dataset.eq("") || dataset.eq("undefined") => ready(Ok(Dataset {
+                    name: "DEFAULT".to_string(),
+                })),
                 Ok(dataset) => ready(Ok(Dataset {
                     name: dataset.to_string(),
                 })),
@@ -30,7 +49,9 @@ impl FromRequest for Dataset {
                     "Dataset must be ASCII".to_string(),
                 ))),
             },
-            None => ready(Ok(Dataset { name: "DEFAULT".to_string() })),
+            None => ready(Ok(Dataset {
+                name: "DEFAULT".to_string(),
+            })),
         }
     }
 }
@@ -53,13 +74,32 @@ pub struct CreateDatasetRequest {
 )]
 pub async fn create_dataset(
     data: web::Json<CreateDatasetRequest>,
+    tantivy_index_map: web::Data<RwLock<TantivyIndexMap>>,
     user: LoggedUser,
 ) -> Result<HttpResponse, ServiceError> {
+    log::info!("Creating dataset {:?}", data.dataset);
     let admin_email = std::env::var("ADMIN_USER_EMAIL").unwrap_or("".to_string());
     if admin_email != user.email {
         return Err(ServiceError::Forbidden);
     }
 
+    tantivy_index_map
+        .write()
+        .map_err(|err| {
+            ServiceError::BadRequest(format!(
+                "Failed to lock tantivy index: {:?}",
+                err.to_string()
+            ))
+        })?
+        .create_index(Some(&data.dataset))
+        .map_err(|err| {
+            ServiceError::BadRequest(format!(
+                "Failed to create tantivy index: {:?}",
+                err.to_string()
+            ))
+        })?;
+
+    log::info!("Creating dataset {:?}", data.dataset);
     qdrant_operator::create_new_qdrant_collection_query(data.dataset.clone()).await?;
     Ok(HttpResponse::NoContent().finish())
 }
