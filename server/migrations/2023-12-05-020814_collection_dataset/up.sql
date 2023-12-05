def4873e4864dd1969a9a6a31220deef5ad26add
fastfist <fdenzell@gmail.com>
2023-12-05 15:00:20 -0800

    wip: trying to change database migration to make dataset a TABLE

--- server/migrations/2023-12-05-020814_collection_dataset/up.sql
+++
@@ -1,6 +0,0 @@
--- Your SQL goes here
-ALTER TABLE card_collection
-ADD COLUMN dataset TEXT NOT NULL default 'DEFAULT';
-
-ALTER TABLE card_collection_bookmarks
-ADD COLUMN dataset TEXT NOT NULL default 'DEFAULT';
