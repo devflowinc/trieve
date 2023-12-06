8f1face257773220fe75f5c11fa485efac31a860
Nick K <nick@arguflow.gg>
2023-12-05 20:27:44 -0800

    cleanup: remove stripe items and referral codes, feature: add organization_id to invitations and users

---
+++ server/migrations/2023-12-06-034507_drop-user-plans-table/down.sql
@@ -0,0 +1,13 @@
+-- This file should undo anything in `up.sql`
+-- Your SQL goes here
+CREATE TABLE user_plans (
+    id UUID NOT NULL UNIQUE PRIMARY KEY,
+    stripe_customer_id TEXT NOT NULL UNIQUE,
+    stripe_subscription_id TEXT NOT NULL UNIQUE,
+    plan TEXT NOT NULL,
+    status TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+
+CREATE INDEX idx_user_plans_stripe_customer_id ON user_plans (stripe_customer_id);
