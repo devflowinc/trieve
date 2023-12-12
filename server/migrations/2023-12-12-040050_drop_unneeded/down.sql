761f0fda7defad3c82d8362cbf3d245bd0384738
Dens Sumesh <dens.sumesh79@gmail.com>
2023-12-11 22:34:31 -0700

    feature: CRUD ops for datasets and dataset access guard

--- server/migrations/2023-12-12-040050_drop_unneeded/down.sql
+++ server/migrations/2023-12-12-040050_drop_unneeded/down.sql
@@ -17,3 +17,15 @@ CREATE TABLE password_resets (
 
 
 ALTER TABLE card_metadata ADD COLUMN IF NOT EXISTS card_metadata_tsvector tsvector;
+
+CREATE FUNCTION IF NOT EXISTS update_tsvector() RETURNS TRIGGER AS $$
+BEGIN
+    NEW.card_metadata_tsvector := to_tsvector(NEW.content);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER IF NOT EXISTS update_tsvector_trigger
+BEFORE INSERT ON card_metadata
+FOR EACH ROW
+EXECUTE FUNCTION update_tsvector();
