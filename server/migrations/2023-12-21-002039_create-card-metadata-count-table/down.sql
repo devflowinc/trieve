2bd5017283bf19df9c34839b4b73fcd2d78d5e99
Nick K <nick@arguflow.gg>
2023-12-21 17:34:39 -0600

    bugfix: add down migration to card_metadata_counts

--- server/migrations/2023-12-21-002039_create-card-metadata-count-table/down.sql
+++ server/migrations/2023-12-21-002039_create-card-metadata-count-table/down.sql
@@ -1,1 +1,6 @@
 -- This file should undo anything in `up.sql`
+DROP TRIGGER card_metadata_count_trigger ON card_metadata;
+
+DROP FUNCTION update_card_metadata_count();
+
+DROP TABLE card_metadata_counts;
