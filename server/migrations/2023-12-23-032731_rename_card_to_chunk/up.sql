1fd555ef718186733f7599da0cd2b7fea1331a91
Nick K <nick@arguflow.gg>
2023-12-23 16:42:44 -0600

    bugfix: fix migration script for card to chunk rename

--- server/migrations/2023-12-23-032731_rename_card_to_chunk/up.sql
+++ server/migrations/2023-12-23-032731_rename_card_to_chunk/up.sql
@@ -3,26 +3,36 @@ ALTER TABLE card_collection
 RENAME TO chunk_collection;
 
 ALTER TABLE card_collection_bookmarks
-RENAME COLUMN card_collection_id TO chunk_collection_id
+RENAME COLUMN card_metadata_id TO chunk_metadata_id;
+
+ALTER TABLE card_collection_bookmarks
 RENAME TO chunk_collection_bookmarks;
 
 ALTER TABLE card_collisions
-RENAME COLUMN card_id TO chunk_id
+RENAME COLUMN card_id TO chunk_id;
+
+ALTER TABLE card_collisions
 RENAME TO chunk_collisions;
 
 ALTER TABLE card_files
-RENAME COLUMN card_id TO chunk_id
+RENAME COLUMN card_id TO chunk_id;
+
+ALTER TABLE card_files
 RENAME TO chunk_files;
 
 ALTER TABLE card_metadata
-RENAME COLUMN card_html TO chunk_html
+RENAME COLUMN card_html TO chunk_html;
+
+ALTER TABLE card_metadata
 RENAME TO chunk_metadata;
 
 ALTER TABLE card_metadata_counts
 RENAME TO chunk_metadata_counts;
 
 ALTER TABLE cut_cards
-RENAME COLUMN cut_card_content TO cut_chunk_content
+RENAME COLUMN cut_card_content TO cut_chunk_content;
+
+ALTER TABLE cut_cards
 RENAME TO cut_chunks;
 
 ALTER TABLE stripe_plans
