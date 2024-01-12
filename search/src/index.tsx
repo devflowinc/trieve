ef5cf3e4c6913ffa6c28646a042291780a920d8e
Nick K <nick@arguflow.gg>
2024-01-11 18:20:18 -0800

    feature: re-establish all routes and get running with Vite

--- search/src/index.tsx
+++ search/src/index.tsx
@@ -1,12 +1,37 @@
 /* @refresh reload */
 import "./index.css";
 import { render } from "solid-js/web";
-import { Router } from "@solidjs/router";
+import { Route, Router } from "@solidjs/router";
+import { Home } from "./pages/Home";
+import { Search } from "./pages/Search";
+import { Upload } from "./pages/Upload";
+import { CreateChunk } from "./pages/CreateChunk";
+import { EditChunk } from "./pages/chunk/edit/EditChunk";
+import { ViewChunk } from "./pages/chunk/ViewChunk";
+import { ViewCollection } from "./pages/collection/ViewCollection";
+import { Settings } from "./pages/user/Settings";
+import { ViewUser } from "./pages/user/ViewUser";
 
 const root = document.getElementById("root");
 
 render(
-  () => <Router />,
+  () => (
+    <Router>
+      <Route path="/" component={Home} />
+      <Route path="/search" component={Search} />
+      <Route path="/upload" component={Upload} />
+      <Route path="/create" component={CreateChunk} />
+      <Route path="/chunk">
+        <Route path="/edit/:id" component={EditChunk} />
+        <Route path="/:id" component={ViewChunk} />
+      </Route>
+      <Route path="/collection/:id" component={ViewCollection} />
+      <Route path="/user">
+        <Route path="/settings" component={Settings} />
+        <Route path="/:id" component={ViewUser} />
+      </Route>
+    </Router>
+  ),
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   root!,
 );
