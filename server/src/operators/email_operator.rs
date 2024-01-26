fcd06a36b85a87f59cbd8a72a4f42d7f065731a1
Nick K <nick@arguflow.gg>
2024-01-25 23:16:52 -0800

    feature: use SMTP server instead of sendgrid

--- server/src/operators/email_operator.rs
+++ server/src/operators/email_operator.rs
@@ -1,39 +1,40 @@
 use crate::{errors::DefaultError, get_env};
-use sendgrid::v3::{Content, Email, Message, Personalization, Sender};
+use lettre::message::header::ContentType;
+use lettre::transport::smtp::authentication::Credentials;
+use lettre::{Message, SmtpTransport, Transport};
 
-#[allow(dead_code)]
-pub async fn send_health_check_error(email: String, error: String) -> Result<(), DefaultError> {
-    let sg_email_content = format!(
-        "WARNING health check is down. <br/>
-        Error message: <br/>
-         <code>{}</code>",
-        error
-    );
-    let sg_email_personalization = Personalization::new(Email::new(email));
-    let email_address = Email::new(get_env!(
-        "SENDGRID_EMAIL_ADDRESS",
-        "SENDGRID_EMAIL_ADDRESS should be set"
-    ));
-    let sg_email = Message::new(email_address)
-        .set_subject("WARNING WARNING WARNING production is down WARNING WARING WARNING")
-        .add_content(
-            Content::new()
-                .set_content_type("text/html")
-                .set_value(sg_email_content),
-        )
-        .add_personalization(sg_email_personalization);
+pub fn get_smtp_creds() -> Credentials {
+    let smtp_username = get_env!("SMTP_USERNAME", "SMTP_USERNAME should be set");
+    let smtp_password = get_env!("SMTP_PASSWORD", "SMTP_PASSWORD should be set");
 
-    send_email(sg_email).await
+    Credentials::new(smtp_username.to_owned(), smtp_password.to_owned())
 }
 
-pub async fn send_email(sg_email: Message) -> Result<(), DefaultError> {
-    let sg_api_key = get_env!("SENDGRID_API_KEY", "SENDGRID_API_KEY should be set").into();
-    let sg_sender = Sender::new(sg_api_key);
-    let sg_response = sg_sender.send(&sg_email).await;
-    match sg_response {
+pub async fn send_email(html_email_body: String, to_address: String) -> Result<(), DefaultError> {
+    let smtp_relay = get_env!("SMTP_RELAY", "SMTP_RELAY should be set");
+    let smtp_email_address = get_env!("SMTP_EMAIL_ADDRESS", "SMTP_EMAIL_ADDRESS should be set");
+
+    let smtp_creds = get_smtp_creds();
+    let mailer = SmtpTransport::relay(smtp_relay)
+        .expect("Failed to create mailer")
+        .credentials(smtp_creds)
+        .build();
+
+    let email = Message::builder()
+        .from(smtp_email_address.parse().expect("Invalid email address"))
+        .to(to_address.parse().expect("Invalid email address"))
+        .subject("Trieve Sign Up Invitation")
+        .header(ContentType::TEXT_HTML)
+        .body(html_email_body)
+        .expect("Failed to create email");
+
+    match mailer.send(&email) {
         Ok(_) => Ok(()),
-        Err(_e) => Err(DefaultError {
-            message: "Error sending email.",
-        }),
+        Err(e) => {
+            log::error!("Error sending email: {:?}", e);
+            Err(DefaultError {
+                message: "Error sending email.",
+            })
+        }
     }
 }
