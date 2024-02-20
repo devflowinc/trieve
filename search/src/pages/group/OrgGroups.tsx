8f8c1e9a3d8a21a79175f6bc5121e299e9e6b565
Nick K <nick@arguflow.gg>
2024-02-19 18:24:58 -0800

    lint: apply yarn lint to search

--- search/src/pages/group/OrgGroups.tsx
+++ search/src/pages/group/OrgGroups.tsx
@@ -1,39 +1,35 @@
-import {createSignal } from 'solid-js';
-import { SearchLayout } from '../../layouts/SearchLayout';
-import { Footer } from '../../components/Footer';
-import { GroupUserPageView } from '../../components/OrgGroupPageView';
-import { useStore } from '@nanostores/solid';
-import { currentUser } from '../../stores/userStore';
-import { ConfirmModal } from '../../components/Atoms/ConfirmModal';
-
-
+import { createSignal } from "solid-js";
+import { SearchLayout } from "../../layouts/SearchLayout";
+import { Footer } from "../../components/Footer";
+import { GroupUserPageView } from "../../components/OrgGroupPageView";
+import { useStore } from "@nanostores/solid";
+import { currentUser } from "../../stores/userStore";
+import { ConfirmModal } from "../../components/Atoms/ConfirmModal";
 
 export const OrgGroups = () => {
-    // Define your component logic here
-    const $currentUser = useStore(currentUser);
-    const [showConfirmGroupDeleteModal, setShowConfirmGroupDeleteModal] = createSignal(false);
-    const [onGroupDelete, setOnGroupDelete] = createSignal(() => { });
+  // Define your component logic here
+  const $currentUser = useStore(currentUser);
+  const [showConfirmGroupDeleteModal, setShowConfirmGroupDeleteModal] =
+    createSignal(false);
+  const [onGroupDelete, setOnGroupDelete] = createSignal(() => {});
 
-
-    return (
-        <SearchLayout>
-            <div class="mb-4 mt-4 flex  flex-col overflow-hidden  pt-4 text-xl mx-[10rem]">
-                <GroupUserPageView
-                    loggedUser={$currentUser()}
-                    setOnDelete={setOnGroupDelete}
-                    setShowConfirmModal={setShowConfirmGroupDeleteModal}
-                />
-                <ConfirmModal
-                    showConfirmModal={showConfirmGroupDeleteModal}
-                    setShowConfirmModal={setShowConfirmGroupDeleteModal}
-                    onConfirm={onGroupDelete}
-                    message={"Are you sure you want to delete this group?"}
-                />
-            </div>
-            <div class="flex-1" />
-            <Footer />
-
-        </SearchLayout>
-    );
+  return (
+    <SearchLayout>
+      <div class="mx-[10rem] mb-4 mt-4  flex flex-col  overflow-hidden pt-4 text-xl">
+        <GroupUserPageView
+          loggedUser={$currentUser()}
+          setOnDelete={setOnGroupDelete}
+          setShowConfirmModal={setShowConfirmGroupDeleteModal}
+        />
+        <ConfirmModal
+          showConfirmModal={showConfirmGroupDeleteModal}
+          setShowConfirmModal={setShowConfirmGroupDeleteModal}
+          onConfirm={onGroupDelete}
+          message={"Are you sure you want to delete this group?"}
+        />
+      </div>
+      <div class="flex-1" />
+      <Footer />
+    </SearchLayout>
+  );
 };
-
