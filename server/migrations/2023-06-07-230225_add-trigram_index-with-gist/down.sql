6e2febafd8a2fb085383c11ac2c1cfe8880c7321
Nick K <nick@arguflow.gg>
2024-03-09 14:55:29 -0800

    cleanup: remove pg-trgm migration for yugabyte compliance

--- server/migrations/2023-06-07-230225_add-trigram_index-with-gist/down.sql
+++ server/migrations/2023-06-07-230225_add-trigram_index-with-gist/down.sql
@@ -1,3 +1,3 @@
 -- This file should undo anything in `up.sql`
-DROP INDEX IF EXISTS idx_gist;
-DROP EXTENSION IF EXISTS pg_trgm;
+-- DROP INDEX IF EXISTS idx_gist;
+-- DROP EXTENSION IF EXISTS pg_trgm;
