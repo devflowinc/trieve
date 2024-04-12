48b86a5401856fc8145d0b72f540ca37703ae0d3
Dens Sumesh <dens.sumesh79@gmail.com>
2024-04-12 10:55:45 -0700

    feature: file microservice

--- server/src/operators/file_operator.rs
+++ server/src/operators/file_operator.rs
@@ -1,31 +1,26 @@
+use super::chunk_operator::{create_chunk_metadata, get_row_count_for_dataset_id_query};
 use super::event_operator::create_event_query;
 use super::group_operator::{create_group_from_file_query, create_group_query};
 use super::parse_operator::{coarse_doc_chunker, convert_html_to_text};
 use crate::data::models::FileDTO;
-use crate::data::models::RedisPool;
+use crate::data::models::{ChunkGroup, FileDataDTO};
 use crate::data::models::{
     ChunkMetadata, Dataset, DatasetAndOrgWithSubAndPlan, EventType, ServerDatasetConfiguration,
 };
-use crate::handlers::auth_handler::AdminOnly;
-use crate::handlers::chunk_handler::{ChunkData, CreateSingleChunkData, SingleQueuedChunkResponse};
+use crate::handlers::chunk_handler::ChunkData;
 use crate::operators::chunk_operator::delete_chunk_metadata_query;
-use crate::{data::models::ChunkGroup, handlers::chunk_handler::ReturnQueuedChunk};
 use crate::{data::models::Event, get_env};
 use crate::{
     data::models::{File, Pool},
     errors::ServiceError,
-    handlers::{
-        auth_handler::LoggedUser,
-        chunk_handler::{create_chunk, CreateChunkData},
-        file_handler::UploadFileResult,
-    },
 };
-use actix_web::{body::MessageBody, web};
+use actix_web::web;
 use diesel::dsl::sql;
 use diesel::prelude::*;
 use diesel::sql_types::BigInt;
 use diesel_async::scoped_futures::ScopedFutureExt;
 use diesel_async::{AsyncConnection, RunQueryDsl};
+use redis::aio::MultiplexedConnection;
 use s3::{creds::Credentials, Bucket, Region};
 
 #[tracing::instrument]
@@ -67,16 +62,11 @@ pub fn get_aws_bucket() -> Result<Bucket, ServiceError> {
     Ok(aws_bucket)
 }
 
-#[allow(clippy::too_many_arguments)]
 #[tracing::instrument(skip(pool))]
 pub async fn create_file_query(
     file_id: uuid::Uuid,
-    file_name: &str,
     file_size: i64,
-    tag_set: Option<String>,
-    metadata: Option<serde_json::Value>,
-    link: Option<String>,
-    time_stamp: Option<String>,
+    upload_file_data: FileDataDTO,
     dataset_id: uuid::Uuid,
     pool: web::Data<Pool>,
 ) -> Result<File, ServiceError> {
@@ -89,12 +79,12 @@ pub async fn create_file_query(
 
     let new_file = File::from_details(
         Some(file_id),
-        file_name,
+        &upload_file_data.file_name,
         file_size,
-        tag_set,
-        metadata,
-        link,
-        time_stamp,
+        upload_file_data.tag_set.map(|tag_set| tag_set.join(",")),
+        upload_file_data.metadata,
+        upload_file_data.link,
+        upload_file_data.time_stamp,
         dataset_id,
     );
 
@@ -108,186 +98,27 @@ pub async fn create_file_query(
 }
 
 #[allow(clippy::too_many_arguments)]
-#[tracing::instrument(skip(pool, redis_pool))]
-pub async fn convert_doc_to_html_query(
-    file_name: String,
-    file_data: Vec<u8>,
-    tag_set: Option<String>,
-    description: Option<String>,
-    link: Option<String>,
-    metadata: Option<serde_json::Value>,
-    create_chunks: Option<bool>,
-    time_stamp: Option<String>,
-    group_tracking_id: Option<String>,
-    user: LoggedUser,
-    dataset_org_plan_sub: DatasetAndOrgWithSubAndPlan,
-    pool: web::Data<Pool>,
-    redis_pool: web::Data<RedisPool>,
-) -> Result<UploadFileResult, ServiceError> {
-    let file_id = uuid::Uuid::new_v4();
-    let file_id_query_clone = file_id;
-    let file_name1 = file_name.clone();
-    let file_data1 = file_data.clone();
-    let tag_set1 = tag_set.clone();
-    let dataset_org_plan_sub1 = dataset_org_plan_sub.clone();
-
-    tokio::spawn(async move {
-        let tika_url = std::env::var("TIKA_URL")
-            .expect("TIKA_URL must be set")
-            .to_string();
-
-        let tika_client = reqwest::Client::new();
-        let tika_response = tika_client
-            .put(&format!("{}/tika", tika_url))
-            .header("Accept", "text/html")
-            .body(file_data.clone())
-            .send()
-            .await
-            .map_err(|err| {
-                log::error!("Could not send file to tika {:?}", err);
-                ServiceError::BadRequest("Could not send file to tika".to_string())
-            })?;
-
-        let tike_html_converted_file_bytes = tika_response
-            .bytes()
-            .await
-            .map_err(|err| {
-                log::error!("Could not get tika response bytes {:?}", err);
-                ServiceError::BadRequest("Could not get tika response bytes".to_string())
-            })?
-            .to_vec();
-        let html_content = String::from_utf8_lossy(&tike_html_converted_file_bytes).to_string();
-
-        // get file metadata from tika
-        let tika_metadata_response = tika_client
-            .put(&format!("{}/meta", tika_url))
-            .header("Accept", "application/json")
-            .body(file_data.clone())
-            .send()
-            .await
-            .map_err(|err| {
-                log::error!("Could not send file to tika {:?}", err);
-                ServiceError::BadRequest("Could not send file to tika".to_string())
-            })?;
-
-        let mut tika_metadata_response_json: serde_json::Value =
-            tika_metadata_response.json().await.map_err(|err| {
-                log::error!("Could not get tika metadata response json {:?}", err);
-                ServiceError::BadRequest("Could not get tika metadata response json".to_string())
-            })?;
-
-        if let Some(metadata) = metadata {
-            match metadata.as_object() {
-                Some(metadata) => {
-                    for (key, value) in metadata {
-                        tika_metadata_response_json[key] = value.clone();
-                    }
-                }
-                _ => {
-                    log::error!("Could not convert metadata to object {:?}", metadata);
-                }
-            }
-        }
-
-        let file_size_mb = (file_data.len() as f64 / 1024.0 / 1024.0).round() as i64;
-
-        let created_file = create_file_query(
-            file_id_query_clone,
-            &file_name,
-            file_size_mb,
-            tag_set.clone(),
-            Some(tika_metadata_response_json.clone()),
-            link.clone(),
-            time_stamp.clone(),
-            dataset_org_plan_sub1.dataset.id,
-            pool.clone(),
-        )
-        .await?;
-
-        let bucket = get_aws_bucket()?;
-        bucket
-            .put_object(created_file.id.to_string(), file_data.as_slice())
-            .await
-            .map_err(|e| {
-                log::error!("Could not upload file to S3 {:?}", e);
-                ServiceError::BadRequest("Could not upload file to S3".to_string())
-            })?;
-
-        if create_chunks.is_some_and(|create_chunks_bool| !create_chunks_bool) {
-            return Ok::<(), ServiceError>(());
-        }
-
-        let resp = create_chunks_with_handler(
-            tag_set,
-            file_name,
-            created_file.id,
-            description,
-            Some(tika_metadata_response_json.clone()),
-            time_stamp,
-            link.clone(),
-            user,
-            html_content,
-            group_tracking_id,
-            dataset_org_plan_sub1,
-            pool,
-            redis_pool,
-        )
-        .await;
-
-        if resp.is_err() {
-            log::error!("Create chunks with handler failed {:?}", resp);
-        }
-
-        Ok(())
-    });
-
-    Ok(UploadFileResult {
-        file_metadata: File::from_details(
-            Some(file_id),
-            &file_name1,
-            file_data1.len().try_into().unwrap(),
-            tag_set1,
-            None,
-            None,
-            None,
-            dataset_org_plan_sub.dataset.id,
-        ),
-    })
-}
-
-#[allow(clippy::too_many_arguments)]
-#[tracing::instrument(skip(pool, redis_pool))]
+#[tracing::instrument(skip(pool, redis_conn))]
 pub async fn create_chunks_with_handler(
-    tag_set: Option<String>,
-    file_name: String,
     created_file_id: uuid::Uuid,
-    description: Option<String>,
-    metadata: Option<serde_json::Value>,
-    time_stamp: Option<String>,
-    link: Option<String>,
-    user: LoggedUser,
+    upload_file_data: FileDataDTO,
     html_content: String,
-    group_tracking_id: Option<String>,
     dataset_org_plan_sub: DatasetAndOrgWithSubAndPlan,
     pool: web::Data<Pool>,
-    redis_pool: web::Data<RedisPool>,
+    mut redis_conn: MultiplexedConnection,
 ) -> Result<(), ServiceError> {
     let file_text = convert_html_to_text(&html_content);
     let chunk_htmls = coarse_doc_chunker(file_text);
 
-    let mut chunk_ids: Vec<uuid::Uuid> = [].to_vec();
+    let mut chunks: Vec<ChunkData> = [].to_vec();
 
-    let split_tag_set: Option<Vec<String>> =
-        tag_set.map(|tag_set| tag_set.split(',').map(|x| x.to_string()).collect());
-
-    let name = format!("Group for file {}", file_name);
-    let converted_description = convert_html_to_text(&description.unwrap_or("".to_string()));
+    let name = format!("Group for file {}", upload_file_data.file_name);
 
     let chunk_group = ChunkGroup::from_details(
         name.clone(),
-        Some(converted_description),
+        upload_file_data.description.clone(),
         dataset_org_plan_sub.dataset.id,
-        group_tracking_id.clone(),
+        upload_file_data.group_tracking_id.clone(),
         None,
         None,
     );
@@ -311,68 +142,73 @@ pub async fn create_chunks_with_handler(
     for (i, chunk_html) in chunk_htmls.iter().enumerate() {
         let create_chunk_data = ChunkData {
             chunk_html: Some(chunk_html.clone()),
-            link: link.clone(),
-            tag_set: split_tag_set.clone(),
+            link: upload_file_data.link.clone(),
+            tag_set: upload_file_data.tag_set.clone(),
             file_id: Some(created_file_id),
-            metadata: metadata.clone(),
+            metadata: upload_file_data.metadata.clone(),
             group_ids: Some(vec![group_id]),
             group_tracking_ids: None,
-            tracking_id: group_tracking_id
+            tracking_id: upload_file_data
+                .group_tracking_id
                 .clone()
                 .map(|tracking_id| format!("{}|{}", tracking_id, i.to_string())),
             upsert_by_tracking_id: None,
-            time_stamp: time_stamp.clone(),
+            time_stamp: upload_file_data.time_stamp.clone(),
             chunk_vector: None,
             weight: None,
             split_avg: None,
         };
-        let web_json_create_chunk_data = web::Json(CreateChunkData::Single(CreateSingleChunkData(
-            create_chunk_data,
-        )));
-
-        match create_chunk(
-            web_json_create_chunk_data,
-            pool.clone(),
-            AdminOnly(user.clone()),
-            dataset_org_plan_sub.clone(),
-            redis_pool.clone(),
-        )
-        .await
-        {
-            Ok(response) => {
-                if response.status().is_success() {
-                    let queued_chunk: ReturnQueuedChunk = serde_json::from_slice(
-                        response.into_body().try_into_bytes().unwrap().as_ref(),
-                    )
-                    .map_err(|_err| {
-                        ServiceError::BadRequest(
-                            "Error creating chunk metadata's for file".to_string(),
-                        )
-                    })?;
-                    match queued_chunk {
-                        ReturnQueuedChunk::Single(SingleQueuedChunkResponse {
-                            chunk_metadata,
-                            pos_in_queue: _,
-                        }) => chunk_ids.push(chunk_metadata.id),
-                        _ => unreachable!("Only uploaded 1 chunk but multiple chunks returned"),
-                    }
-                }
-            }
-            Err(error) => {
-                log::error!("Error creating chunk: {:?}", error.to_string());
-            }
-        }
+        chunks.push(create_chunk_data);
     }
+
+    let chunk_count =
+        get_row_count_for_dataset_id_query(dataset_org_plan_sub.dataset.id, pool.clone()).await?;
+
+    if chunk_count + chunks.len()
+        > dataset_org_plan_sub
+            .organization
+            .plan
+            .unwrap_or_default()
+            .chunk_count as usize
+    {
+        return Err(ServiceError::BadRequest(
+            "Chunk count exceeds plan limit".to_string(),
+        ));
+    }
+
+    let server_dataset_configuration =
+        ServerDatasetConfiguration::from_json(dataset_org_plan_sub.dataset.server_configuration);
+
+    let ingestion_messages = create_chunk_metadata(
+        chunks,
+        dataset_org_plan_sub.dataset.id,
+        server_dataset_configuration,
+        pool.clone(),
+    )
+    .await?;
+
+    let serialized_messages: Vec<String> = ingestion_messages
+        .0
+        .iter()
+        .filter_map(|msg| serde_json::to_string(&msg).ok())
+        .collect();
+
+    redis::cmd("rpush")
+        .arg("ingestion")
+        .arg(&serialized_messages)
+        .query_async(&mut redis_conn)
+        .await
+        .map_err(|err| ServiceError::BadRequest(err.to_string()))?;
 
     create_event_query(
         Event::from_details(
             dataset_org_plan_sub.dataset.id,
             EventType::FileUploaded {
-                group_id,
+                file_id: created_file_id,
                 file_name: name,
             },
         ),
-        pool,
+        pool.clone(),
     )
     .await
     .map_err(|_| ServiceError::BadRequest("Thread error creating notification".to_string()))?;
