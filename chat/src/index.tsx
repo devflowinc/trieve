7b99a3bcc26080f06a9d42d1c41970639c819f42
Nick K <nick@arguflow.gg>
2024-04-12 08:36:00 -0700

    feature: add sentry tracking to search chat and dashboard

--- chat/src/index.tsx
+++ chat/src/index.tsx
@@ -1,8 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* @refresh reload */
 import "./index.css";
 import { render } from "solid-js/web";
+import * as Sentry from "@sentry/browser";
+import { DEV } from "solid-js";
 import { Chat } from "./pages/chat";
 import { UserContextWrapper } from "./components/contexts/UserContext";
+
+if (!DEV) {
+  Sentry.init({
+    dsn: `${import.meta.env.VITE_SENTRY_CHAT_DSN as string}`,
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration(),
+    ],
+
+    tracesSampleRate: 1.0,
+
+    tracePropagationTargets: ["localhost", /^https:\/\/trieve\.ai\/api/],
+
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
+}
 
 const root = document.getElementById("root");
 
