6092f48951344239d7b0f64fd1f418c4da9e8a34
Dens Sumesh <dens.sumesh79@gmail.com>
2024-05-10 10:01:19 -0700

    fmt: cargo fmt

--- server/src/operators/qdrant_operator.rs
+++ server/src/operators/qdrant_operator.rs
@@ -12,9 +12,9 @@ use qdrant_client::{
         group_id::Kind, point_id::PointIdOptions, quantization_config::Quantization,
         BinaryQuantization, CountPoints, CreateCollection, Distance, FieldType, Filter,
         HnswConfigDiff, PointId, PointStruct, QuantizationConfig, RecommendPointGroups,
-        RecommendPoints, RecommendStrategy, SearchPointGroups, SearchPoints,
-        SparseIndexConfig, SparseVectorConfig, SparseVectorParams, Value, Vector, VectorParams,
-        VectorParamsMap, VectorsConfig,
+        RecommendPoints, RecommendStrategy, SearchPointGroups, SearchPoints, SparseIndexConfig,
+        SparseVectorConfig, SparseVectorParams, Value, Vector, VectorParams, VectorParamsMap,
+        VectorsConfig,
     },
 };
 use serde::{Deserialize, Serialize};
