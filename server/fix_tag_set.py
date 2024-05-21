dc1b7b133fab2fd8d58a343efd9a515c2a62a5f0
Dens Sumesh <dens.sumesh79@gmail.com>
2024-05-21 10:14:08 -0700

    feature: added pagination

--- server/fix_tag_set.py
+++ server/fix_tag_set.py
@@ -1,6 +1,7 @@
 import psycopg2
 import os
 import dotenv
+import uuid
 
 dotenv.load_dotenv()
 # Connect to the PostgreSQL database
@@ -19,24 +20,39 @@ for table in tables:
     # Execute the query to retrieve the tag_set column
     import psycopg2.extensions
 
-    cur.execute(f"SELECT tag_set FROM {table}")
+    lastBusiness_id = uuid.UUID(int=0)
 
-    # Fetch all rows from the result set
-    rows = cur.fetchall()
+    while True:
+        # Execute the query to retrieve the tag_set column
+        cur.execute(
+            f"SELECT * FROM {table} WHERE id > (%s)::uuid ORDER BY id LIMIT 1000",
+            (str(lastBusiness_id),),
+        )
 
-    # Convert the tag_set field from a comma-separated string to an array within PostgreSQL
-    for row in rows:
-        tag_set = row[0]
-        if tag_set:
-            tag_set_array = tag_set.split(",")
-            tag_set = tag_set.replace("'", "\\'")
-            cur.execute(
-                f"UPDATE {table} SET tag_set_array = %s WHERE tag_set = (E'%s')::text",
-                (
-                    tag_set_array,
-                    psycopg2.extensions.AsIs(tag_set),
-                ),
-            )
+        # Fetch the first 10000 rows from the result set
+        rows = cur.fetchmany(1000)
+
+        if not rows:
+            break
+
+        # Convert the tag_set field from a comma-separated string to an array within PostgreSQL
+        for row in rows:
+            tag_set = row[5]
+            if tag_set:
+                tag_set_array = tag_set.split(",")
+                tag_set = tag_set.replace("'", "\\'")
+                cur.execute(
+                    f"UPDATE {table} SET tag_set_array = %s WHERE tag_set = (E'%s')::text",
+                    (
+                        tag_set_array,
+                        psycopg2.extensions.AsIs(tag_set),
+                    ),
+                )
+
+            # Fetch the next 10000 rows from the result set
+
+        lastRecord = rows[-1]
+        lastBusiness_id = lastRecord[0]
 
 # Commit the changes to the database
 conn.commit()
