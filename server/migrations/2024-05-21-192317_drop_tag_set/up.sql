c20e9cf50fdd09353c185b369d3184a008bc6321
skeptrune <nicholas.khami@gmail.com>
2024-05-23 10:50:06 -0700

    Revert "feature: added gin index to tag_set"
    This reverts commit 0f8c97b519cbb9940a9f766182d53ae59d450be9.

--- server/migrations/2024-05-21-192317_drop_tag_set/up.sql
+++ server/migrations/2024-05-21-192317_drop_tag_set/up.sql
@@ -5,7 +5,3 @@ ALTER TABLE chunk_group DROP COLUMN tag_set;
 ALTER TABLE chunk_group RENAME COLUMN tag_set_array TO tag_set;
 ALTER TABLE files DROP COLUMN tag_set;
 ALTER TABLE files RENAME COLUMN tag_set_array TO tag_set;
-
-CREATE INDEX idx_chunk_metadata_tag_set ON chunk_metadata USING gin (tag_set);
-CREATE INDEX idx_chunk_group_tag_set ON chunk_group USING gin (tag_set);
-CREATE INDEX idx_files_tag_set ON files USING gin (tag_set);
