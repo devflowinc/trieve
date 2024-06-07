4916299b533f412055a06488eebf15cb0c4646b2
skeptrune <nick.k@trieve.ai>
2024-06-07 12:34:07 -0700

    bugfix: handle rebalance_chunks: false correctly by having a small extra group at end instead of 1 per split

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -133,7 +133,7 @@ impl Modify for SecurityAddon {
             name = "BSL",
             url = "https://github.com/devflowinc/trieve/blob/main/LICENSE.txt",
         ),
-        version = "0.10.2",
+        version = "0.10.3",
     ),
     servers(
         (url = "https://api.trieve.ai",
