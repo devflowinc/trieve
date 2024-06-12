57a32125995867443466c46f3e14955d66d18c55
Drew Harris <harrisd@smu.edu>
2024-06-12 14:18:16 -0500

    feat: source scroll fade indicator

--- chat/src/components/Atoms/AfMessage.tsx
+++ chat/src/components/Atoms/AfMessage.tsx
@@ -206,58 +206,6 @@ export const AfMessage = (props: AfMessageProps) => {
             </Show>
           </div>
         </Show>
-        {/* <Show when={editing()}> */}
-        {/*   <div */}
-        {/*     classList={{ */}
-        {/*       "dark:text-white md:px-6 w-full px-4 py-4 flex items-start": true, */}
-        {/*       "bg-neutral-200 dark:bg-zinc-700": props.role === "assistant", */}
-        {/*       "bg-neutral-50 dark:bg-zinc-800": props.role === "user", */}
-        {/*     }} */}
-        {/*   > */}
-        {/*     <form class="w-full"> */}
-        {/*       <textarea */}
-        {/*         id="new-message-content-textarea" */}
-        {/*         class="max-h-[180px] w-full resize-none whitespace-pre-wrap rounded bg-neutral-100 bg-transparent p-2 py-1 scrollbar-thin scrollbar-track-neutral-200 scrollbar-thumb-neutral-400 scrollbar-track-rounded-md scrollbar-thumb-rounded-md focus:outline-none dark:bg-neutral-700 dark:text-white dark:scrollbar-track-neutral-700 dark:scrollbar-thumb-neutral-600" */}
-        {/*         placeholder="Write a question or prompt for the assistant..." */}
-        {/*         value={editingMessageContent()} */}
-        {/*         onInput={(e) => resizeTextarea(e.target)} */}
-        {/*         onKeyDown={(e) => { */}
-        {/*           if (e.key === "Enter") { */}
-        {/*             e.preventDefault(); */}
-        {/*             props.onEdit(editingMessageContent()); */}
-        {/*             setEditedContent(editingMessageContent()); */}
-        {/*             setEditing(false); */}
-        {/*           } */}
-        {/*         }} */}
-        {/*         rows="1" */}
-        {/*       /> */}
-        {/*       <div class="mt-2 flex flex-row justify-center space-x-2 text-sm"> */}
-        {/*         <button */}
-        {/*           type="submit" */}
-        {/*           class="rounded bg-purple-500 px-2 py-1 text-white" */}
-        {/*           onClick={(e) => { */}
-        {/*             e.preventDefault(); */}
-        {/*             props.onEdit(editingMessageContent()); */}
-        {/*             setEditedContent(editingMessageContent()); */}
-        {/*             setEditing(false); */}
-        {/*           }} */}
-        {/*         > */}
-        {/*           Save & Submit */}
-        {/*         </button> */}
-        {/*         <button */}
-        {/*           type="button" */}
-        {/*           class="rounded border border-neutral-500 px-2 py-1" */}
-        {/*           onClick={(e) => { */}
-        {/*             e.preventDefault(); */}
-        {/*             setEditing(false); */}
-        {/*           }} */}
-        {/*         > */}
-        {/*           Cancel */}
-        {/*         </button> */}
-        {/*       </div> */}
-        {/*     </form> */}
-        {/*   </div> */}
-        {/* </Show> */}
         <div>
           <Show when={metadata() && metadata().length > 0}>
             <div
@@ -280,6 +228,7 @@ export const AfMessage = (props: AfMessageProps) => {
                   />
                 )}
               </For>
+              <div class="sticky bottom-0 h-[40px] w-full bg-gradient-to-t from-neutral-100 to-transparent dark:from-neutral-900" />
             </div>
           </Show>
         </div>
