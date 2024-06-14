c593b03d90e64c147937f10e70fda473bb0c09cc
Drew Harris <harrisd@smu.edu>
2024-06-14 10:52:22 -0500

    feat: add fill example data checkbox to new dataset modal

---
+++ dashboard/src/api/createDataset.ts
@@ -0,0 +1,38 @@
+import {
+  Dataset,
+  DefaultError,
+  ServerEnvsConfiguration,
+} from "../types/apiTypes";
+
+const api_host = import.meta.env.VITE_API_HOST as unknown as string;
+
+export const createNewDataset = async ({
+  name,
+  organizationId,
+  serverConfig,
+}: {
+  name: string;
+  organizationId: string;
+  serverConfig: ServerEnvsConfiguration;
+}) => {
+  const response = await fetch(`${api_host}/dataset`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+      "TR-Organization": organizationId,
+    },
+    body: JSON.stringify({
+      dataset_name: name,
+      organization_id: organizationId,
+      server_configuration: serverConfig,
+      client_configuration: "{}",
+    }),
+  });
+  if (response.ok) {
+    return (await response.json()) as unknown as Dataset;
+  } else {
+    const error = (await response.json()) as DefaultError;
+    throw new Error(error.message);
+  }
+};
