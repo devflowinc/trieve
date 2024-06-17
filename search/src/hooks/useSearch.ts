1a1c0feaae5978178c814fe56d750e8991437da5
Drew Harris <harrisd@smu.edu>
2024-06-17 14:47:14 -0500

    feat: active search scoreThreshold and extendResults

--- search/src/hooks/useSearch.ts
+++ search/src/hooks/useSearch.ts
@@ -3,6 +3,8 @@ import { createStore } from "solid-js/store";
 export const useSearch = () => {
   const [state, setSearch] = createStore({
     query: "", // TODO: Debounce
+    searchType: "",
+    scoreThreshold: 0.0,
   });
 
   return {
