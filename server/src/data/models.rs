d9d08908386125a69f2912f2088749f7970132ca
Dens Sumesh <dens.sumesh79@gmail.com>
2024-06-18 04:58:38 -0700

    feature: logging middleware

--- server/src/data/models.rs
+++ server/src/data/models.rs
@@ -2486,8 +2486,7 @@ impl QdrantPayload {
                 .payload
                 .get("time_stamp")
                 .cloned()
-                .map(|x| x.as_integer())
-                .flatten(),
+                .and_then(|x| x.as_integer()),
             dataset_id: point
                 .payload
                 .get("dataset_id")
@@ -2507,14 +2506,12 @@ impl QdrantPayload {
                 .payload
                 .get("location")
                 .cloned()
-                .map(|value| serde_json::from_value(value.into()).ok())
-                .flatten(),
+                .and_then(|value| serde_json::from_value(value.into()).ok()),
             num_value: point
                 .payload
                 .get("num_value")
                 .cloned()
-                .map(|x| x.as_double())
-                .flatten(),
+                .and_then(|x| x.as_double()),
         }
     }
 }
@@ -2539,16 +2536,14 @@ impl From<RetrievedPoint> for QdrantPayload {
                 .payload
                 .get("time_stamp")
                 .cloned()
-                .map(|x| x.as_integer())
-                .flatten(),
+                .and_then(|x| x.as_integer()),
             dataset_id: point
                 .payload
                 .get("dataset_id")
                 .cloned()
                 .unwrap_or_default()
                 .as_str()
-                .map(|s| uuid::Uuid::parse_str(s).ok())
-                .flatten()
+                .and_then(|s| uuid::Uuid::parse_str(s).ok())
                 .unwrap_or_default(),
             group_ids: point.payload.get("group_ids").cloned().map(|x| {
                 x.as_list()
@@ -2567,15 +2562,13 @@ impl From<RetrievedPoint> for QdrantPayload {
                 .payload
                 .get("location")
                 .cloned()
-                .map(|value| serde_json::from_value(value.into()).ok())
-                .flatten()
+                .and_then(|value| serde_json::from_value(value.into()).ok())
                 .unwrap_or_default(),
             num_value: point
                 .payload
                 .get("num_value")
                 .cloned()
-                .map(|x| x.as_double())
-                .flatten(),
+                .and_then(|x| x.as_double()),
         }
     }
 }
