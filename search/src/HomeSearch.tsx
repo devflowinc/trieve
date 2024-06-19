7cdf941e9d945d6e051a44216777ff60a095fe7c
Drew Harris <harrisd@smu.edu>
2024-06-19 14:34:34 -0500

    styling: max width the same as previous search page

--- search/src/HomeSearch.tsx
+++ search/src/HomeSearch.tsx
@@ -11,7 +11,7 @@ export const HomeSearch = () => {
     <div class="flex min-h-screen flex-col bg-white dark:bg-shark-800 dark:text-white">
       <Navbar />
       <div class="space-y mt-12 flex w-full flex-col items-center">
-        <div class="w-full max-w-7xl px-4 sm:px-8 md:px-20">
+        <div class="w-full max-w-[1216px] px-4 sm:px-8 md:px-20">
           <SearchForm search={search} />
           <ResultsPage search={search} />
         </div>
