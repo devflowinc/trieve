0664148f053925461fb67ba0a1492c44bc4be563
Drew Harris <harrisd@smu.edu>
2024-06-19 14:32:04 -0500

    feat: remove HomeNavbar

--- search/src/HomeSearch.tsx
+++ search/src/HomeSearch.tsx
@@ -1,15 +1,15 @@
-import { HomeNavbar } from "./components/Atoms/HomeNavbar";
 import SearchForm from "./components/SearchForm";
 import { Footer } from "./components/Footer";
 import { useSearch } from "./hooks/useSearch";
 import ResultsPage from "./components/ResultsPage";
+import { Navbar } from "./components/Atoms/Navbar";
 
 export const HomeSearch = () => {
   const search = useSearch();
 
   return (
     <div class="flex min-h-screen flex-col bg-white dark:bg-shark-800 dark:text-white">
-      <HomeNavbar />
+      <Navbar />
       <div class="space-y mt-12 flex w-full flex-col items-center">
         <div class="w-full max-w-7xl px-4 sm:px-8 md:px-20">
           <SearchForm search={search} />
