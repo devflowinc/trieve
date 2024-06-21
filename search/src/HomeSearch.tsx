e9a7c9f617b1286f0d606f26161cd30431648bcf
Drew Harris <harrisd@smu.edu>
2024-06-21 09:31:58 -0500

    cleanup: remove debug from home search page

--- search/src/HomeSearch.tsx
+++ search/src/HomeSearch.tsx
@@ -3,18 +3,13 @@ import { Footer } from "./components/Footer";
 import { useSearch } from "./hooks/useSearch";
 import ResultsPage from "./components/ResultsPage";
 import { Navbar } from "./components/Atoms/Navbar";
-import { useSearchParams } from "@solidjs/router";
 
 export const HomeSearch = () => {
   const search = useSearch();
-  const [searchParams] = useSearchParams();
 
   return (
     <div class="flex min-h-screen flex-col bg-white dark:bg-shark-800 dark:text-white">
       <Navbar />
-      {JSON.stringify(searchParams, undefined, 4)}
-      <hr />
-      {JSON.stringify(search.state, undefined, 4)}
       <div class="space-y mx-auto mt-8 flex w-full max-w-7xl flex-col items-center">
         <div class="mx-auto w-full max-w-[calc(100%-32px)] px-4 min-[360px]:max-w-[calc(100%-64px)] sm:px-8 md:px-20">
           <SearchForm search={search} />
