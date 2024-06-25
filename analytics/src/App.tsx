ea3b8d9130713cd4491cc37bd9ea3ea10f6f67b0
Drew Harris <harrisd@smu.edu>
2024-06-25 09:32:57 -0700

    feat: create solid vite app with tailwind and router

---
+++ analytics/src/App.tsx
@@ -0,0 +1,24 @@
+import { createSignal } from "solid-js";
+
+function App() {
+  const [count, setCount] = createSignal(0);
+
+  return (
+    <>
+      <h1 class="bg-red-500">Vite + Solid</h1>
+      <div class="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count()}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p class="read-the-docs">
+        Click on the Vite and Solid logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
