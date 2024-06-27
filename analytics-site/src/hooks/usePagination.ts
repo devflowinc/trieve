45e00cccd2fd759c192c52ccc68fa3e7039e0b55
Drew Harris <harrisd@smu.edu>
2024-06-27 13:59:46 -0700

    feat: pagination hook

---
+++ analytics-site/src/hooks/usePagination.ts
@@ -0,0 +1,33 @@
+import { createMemo, createSignal } from "solid-js";
+
+export const usePagination = () => {
+  const [page, setPage] = createSignal(1);
+  const [maxPageDiscovered, setMaxPageDiscovered] = createSignal<number | null>(
+    null,
+  );
+
+  const nextPage = () => {
+    setPage((prev) => prev + 1);
+  };
+
+  const prevPage = () => {
+    if (page() > 1) {
+      setPage((prev) => prev - 1);
+    }
+  };
+
+  const canGoNext = createMemo(() => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const maxPage = maxPageDiscovered();
+    return maxPage === null || page() < maxPage;
+  });
+
+  return {
+    page,
+    nextPage,
+    prevPage,
+    canGoNext,
+    setMaxPageDiscovered,
+    maxPageDiscovered,
+  };
+};
