98486b2f3588dc7f965cc1308a2d945273055ef3
Dens Sumesh <dens.sumesh79@gmail.com>
2024-06-27 12:15:25 -0700

    feature: events in clickhouse

--- server/src/operators/event_operator.rs
+++ server/src/operators/event_operator.rs
@@ -1,10 +1,8 @@
 use crate::{
-    data::models::{Event, Pool},
+    data::models::{ClickhouseEvent, Event},
     errors::ServiceError,
 };
 use actix_web::web;
-use diesel::prelude::*;
-use diesel_async::RunQueryDsl;
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
@@ -16,17 +14,14 @@ pub async fn create_event_query(
     if std::env::var("USE_ANALYTICS")
         .unwrap_or("false".to_string())
         .parse()
-        .unwrap_or(false) { 
-        let query = format!(
-            "INSERT INTO dataset_events (id, dataset_id, event_type, event_data, created_at) VALUES ('{}', '{}', '{}', '{}', now())",
-            event.id,
-            event.dataset_id,
-            event.event_type,
-            event.event_data    
-        );
-
+        .unwrap_or(false)
+    {
         client
-            .query(&query)
+            .query("INSERT INTO trieve.dataset_events (id, dataset_id, event_type, event_data, created_at) VALUES (?, ?, ?, ?, now())")
+            .bind(event.id)
+            .bind(event.dataset_id)
+            .bind(event.event_type)
+            .bind(event.event_data)
             .execute()
             .await
             .map_err(|err| {
@@ -42,53 +37,84 @@ pub struct EventReturn {
     pub events: Vec<Event>,
     pub page_count: i32,
 }
-#[tracing::instrument(skip(pool))]
+#[tracing::instrument(skip(clickhouse_client))]
 pub async fn get_events_query(
     dataset_id: uuid::Uuid,
     page: i64,
     page_size: i64,
     event_types: Vec<String>,
-    pool: web::Data<Pool>,
+    clickhouse_client: web::Data<clickhouse::Client>,
 ) -> Result<EventReturn, ServiceError> {
-    use crate::data::schema::dataset_event_counts::dsl as dataset_event_counts_columns;
-    use crate::data::schema::events::dsl as events_columns;
-    let mut conn = pool.get().await.unwrap();
+    let query = format!(
+        "
+        SELECT
+            id,
+            dataset_id,
+            event_type,
+            event_data,
+            created_at
+        FROM trieve.dataset_events
+        WHERE dataset_id = '{dataset_id}' AND event_type IN ({event_types})
+        ORDER BY created_at DESC
+        LIMIT {limit} OFFSET {offset}
+        ",
+        dataset_id = dataset_id.to_string(),
+        event_types = event_types
+            .iter()
+            .map(|x| format!("'{}'", x))
+            .collect::<Vec<String>>()
+            .join(","),
+        limit = page_size,
+        offset = (page - 1) * page_size,
+    );
 
-    let events_and_count = events_columns::events
-        .left_join(
-            dataset_event_counts_columns::dataset_event_counts
-                .on(events_columns::dataset_id.eq(dataset_event_counts_columns::dataset_uuid)),
-        )
-        .filter(events_columns::dataset_id.eq(dataset_id))
-        .filter(
-            events_columns::event_type.eq_any(
-                event_types
-                    .iter()
-                    .map(|event_type| event_type.as_str())
-                    .collect::<Vec<&str>>(),
-            ),
-        )
-        .select((
-            Event::as_select(),
-            crate::data::schema::dataset_event_counts::dsl::notification_count.nullable(),
-        ))
-        .order(events_columns::created_at.desc())
-        .limit(page_size)
-        .offset((page - 1) * page_size)
-        .load::<(Event, Option<i32>)>(&mut conn)
+    let events_and_count: Vec<ClickhouseEvent> = clickhouse_client
+        .query(&query)
+        .fetch_all()
         .await
-        .map_err(|_| ServiceError::BadRequest("Failed to get events".to_string()))?;
+        .map_err(|err| {
+            log::error!("Failed to get events {:?}", err);
+            ServiceError::BadRequest("Failed to get events".to_string())
+        })?;
 
-    let events: Vec<Event> = events_and_count
-        .iter()
-        .map(|(event, _)| event.clone())
+    let events = events_and_count
+        .into_iter()
+        .map(|event| event.into())
         .collect();
-    let event_count: i32 = events_and_count
-        .get(0)
-        .map(|(_, count)| count.unwrap_or(0))
-        .unwrap_or(0);
+
+    let count_query = format!(
+        "
+        SELECT count(*) / {page_size}
+        FROM trieve.dataset_events
+        WHERE dataset_id = '{dataset_id}' AND event_type IN ({event_types})
+        ",
+        dataset_id = dataset_id.to_string(),
+        event_types = event_types
+            .iter()
+            .map(|x| format!("'{}'", x))
+            .collect::<Vec<String>>()
+            .join(","),
+        page_size = page_size,
+    );
+
+    let count: i32 = clickhouse_client
+        .query(&count_query)
+        .fetch()
+        .map_err(|err| {
+            log::error!("Failed to get events count {:?}", err);
+            ServiceError::BadRequest("Failed to get events count".to_string())
+        })?
+        .next()
+        .await
+        .map_err(|err| {
+            log::error!("Failed to get events count {:?}", err);
+            ServiceError::BadRequest("Failed to get events count".to_string())
+        })?
+        .unwrap_or(0.0_f64)
+        .ceil() as i32;
+
     Ok(EventReturn {
         events,
-        page_count: (event_count as f64 / 10.0).ceil() as i32,
+        page_count: count,
     })
 }
