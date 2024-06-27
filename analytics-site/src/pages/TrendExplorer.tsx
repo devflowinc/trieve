be55a1a4b65a0b8342e247da88fc7deeaa6d0bad
Drew Harris <harrisd@smu.edu>
2024-06-27 12:19:30 -0700

    feat: showing queries in sidebar

--- analytics-site/src/pages/TrendExplorer.tsx
+++ analytics-site/src/pages/TrendExplorer.tsx
@@ -1,8 +1,9 @@
 import { createQuery } from "@tanstack/solid-query";
-import { TrendExplorerCanvas } from "../components/trend-explorer/TrendExplorerCanvas";
 import { DatasetContext } from "../layouts/TopBarLayout";
-import { getTrendsBubbles } from "../api/trends";
-import { Show, useContext } from "solid-js";
+import { getQueriesForTopic, getTrendsBubbles } from "../api/trends";
+import { createSignal, For, Show, useContext } from "solid-js";
+import { TrendExplorerCanvas } from "../components/trend-explorer/TrendExplorerCanvas";
+import { SearchQueryEvent } from "shared/types";
 
 export const TrendExplorer = () => {
   const dataset = useContext(DatasetContext);
@@ -14,12 +15,53 @@ export const TrendExplorer = () => {
     },
   }));
 
+  const [selectedTopicId, setSelectedTopicId] = createSignal<string | null>(
+    null,
+  );
+
+  const selectedTopicQuery = createQuery(() => ({
+    queryKey: ["selected-topic", selectedTopicId()],
+    queryFn: async () => {
+      const selectedTopic = selectedTopicId();
+      if (selectedTopic === null) {
+        return [];
+      }
+      return getQueriesForTopic(dataset().dataset.id, selectedTopic);
+    },
+    enabled() {
+      return selectedTopicId() !== null;
+    },
+  }));
+
   return (
     <div class="grid grow grid-cols-[300px_1fr]">
-      <div class="border-r border-r-neutral-400 bg-neutral-200">Sidebar</div>
+      <div class="border-r border-r-neutral-400 bg-neutral-200 p-2">
+        <div class="flex flex-col gap-2">
+          <For each={selectedTopicQuery?.data}>
+            {(query) => <QueryCard searchEvent={query} />}
+          </For>
+        </div>
+      </div>
       <Show when={trendsQuery?.data}>
-        {(trends) => <TrendExplorerCanvas topics={trends()} />}
+        {(trends) => (
+          <TrendExplorerCanvas
+            onSelectTopic={(topic) => setSelectedTopicId(topic)}
+            topics={trends()}
+          />
+        )}
       </Show>
+    </div>
+  );
+};
+
+interface QueryCardProps {
+  searchEvent: SearchQueryEvent;
+}
+const QueryCard = (props: QueryCardProps) => {
+  return (
+    <div class="bg-white p-3">
+      <div>{props.searchEvent.query}</div>
+      <div>Score: {props.searchEvent.top_score}</div>
     </div>
   );
 };
