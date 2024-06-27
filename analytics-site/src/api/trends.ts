8968c43440b1ba2974b7ec92dbd4e82183529147
Drew Harris <harrisd@smu.edu>
2024-06-27 12:19:30 -0700

    feat: showing queries in sidebar

--- analytics-site/src/api/trends.ts
+++ analytics-site/src/api/trends.ts
@@ -1,4 +1,4 @@
-import { SearchClusterTopics } from "shared/types";
+import { SearchClusterTopics, SearchQueryEvent } from "shared/types";
 import { apiHost } from "../utils/apiHost";
 
 export const getTrendsBubbles = async (
@@ -17,5 +17,29 @@ export const getTrendsBubbles = async (
   }
 
   const data = (await response.json()) as unknown as SearchClusterTopics[];
+  return data;
+};
+
+export const getQueriesForTopic = async (
+  datasetId: string,
+  clusterId: string,
+): Promise<SearchQueryEvent[]> => {
+  const response = await fetch(
+    `${apiHost}/analytics/${datasetId}/${clusterId}/1`,
+    {
+      credentials: "include",
+      headers: {
+        "TR-Dataset": datasetId,
+        "Content-Type": "application/json",
+      },
+    },
+  );
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch trends bubbles: ${response.statusText}`);
+  }
+
+  const data = (await response.json()) as unknown as SearchQueryEvent[];
+  console.log("GOT DATA", data);
   return data;
 };
