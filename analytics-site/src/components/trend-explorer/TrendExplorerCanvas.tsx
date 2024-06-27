68788e599235343f51e1f7423fee9d28eca71ecf
Drew Harris <harrisd@smu.edu>
2024-06-27 11:59:27 -0700

    fix: better canvas

--- analytics-site/src/components/trend-explorer/TrendExplorerCanvas.tsx
+++ analytics-site/src/components/trend-explorer/TrendExplorerCanvas.tsx
@@ -1,4 +1,4 @@
-import { createEffect, createSignal, onMount } from "solid-js";
+import { createEffect, createSignal, on, onMount } from "solid-js";
 import {
   Composite,
   Engine,
@@ -21,8 +21,13 @@ const getColorFromDensity = (density: number) => {
   return `rgb(${color}, ${color}, ${color})`;
 };
 
+const centeredRandom = (factor: number) => {
+  return Math.random() * factor - factor / 2;
+};
+
 export const TrendExplorerCanvas = (props: TrendExplorerCanvas) => {
   const [canvasElement, setCanvasElement] = createSignal<HTMLCanvasElement>();
+  const [render, setRender] = createSignal<Render | null>(null);
 
   const [containerSize, setContainerSize] = createStore({
     width: 700,
@@ -42,6 +47,7 @@ export const TrendExplorerCanvas = (props: TrendExplorerCanvas) => {
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       observer.observe(canvasElement()!);
     }
+
     return () => observer.disconnect();
   });
 
@@ -51,6 +57,24 @@ export const TrendExplorerCanvas = (props: TrendExplorerCanvas) => {
     },
   });
 
+  const runner = Runner.create();
+
+  createEffect(
+    on(
+      () => containerSize.width,
+      () => {
+        // Set the render options to the size of the container
+        const localRender = render();
+        if (localRender === null) {
+          return;
+        }
+        // Update the canvas size
+        localRender.canvas.width = containerSize.width;
+        localRender.canvas.height = containerSize.height;
+      },
+    ),
+  );
+
   createEffect(() => {
     console.log("updating");
     const render = Render.create({
@@ -58,7 +82,6 @@ export const TrendExplorerCanvas = (props: TrendExplorerCanvas) => {
       engine: engine,
       options: {
         background: "#f5f5f5",
-        showIds: true,
         height: 800,
         width: 700,
         wireframes: false,
@@ -67,46 +90,52 @@ export const TrendExplorerCanvas = (props: TrendExplorerCanvas) => {
 
     const circles = props.topics.map((topic) => {
       const circle = Bodies.circle(
-        containerSize.width / 2,
-        containerSize.height / 2,
+        centeredRandom(3),
+        centeredRandom(3),
         1 * topic.density,
       );
       // @ts-expect-error just debugging
       circle.id = topic.topic;
-      // Make the circle gray
       circle.render.fillStyle = getColorFromDensity(topic.avg_score);
-      // Add a border
       circle.render.strokeStyle = "#333";
       circle.render.lineWidth = 1;
+      circle.timeScale = 0.2;
+      circle.friction = 0.9999;
+      circle.density = 0.9999;
+
       return circle;
     });
 
     Composite.add(engine.world, [...circles]);
 
-    Render.run(render);
-
-    const runner = Runner.create();
-
-    Events.on(runner, "beforeTick", () => {
+    const response = Events.on(runner, "beforeTick", () => {
       // Pull the circles towards the center
       circles.forEach((circle) => {
         const x = circle.position.x;
         const y = circle.position.y;
-        let dx = unwrap(containerSize).width / 2 - x;
-        dx += Math.random() * 8 - 1;
-        const dy = unwrap(containerSize).height / 2 - y;
-        const angle = Math.atan2(dy, dx);
-        const force = 0.001 * circle.density;
-        const fx = Math.cos(angle) * force;
-        const fy = Math.sin(angle) * force;
+        const fx = -0.0005 * x * 0.5;
+        const fy = -0.0005 * y * 0.5;
+
         Body.applyForce(circle, { x: x, y: y }, { x: fx, y: fy });
       });
     });
+
+    // center the camera on (0, 0)
+    setRender(render);
+
+    // console
+    Render.lookAt(render, {
+      min: { x: -containerSize.width / 2, y: -containerSize.height / 2 },
+      max: { x: containerSize.width / 2, y: containerSize.height / 2 },
+    });
+
+    Render.run(render);
 
     Runner.run(runner, engine);
 
     return () => {
       console.log("cleaning up");
+      response();
       Render.stop(render);
       Runner.stop(runner);
       Engine.clear(engine);
