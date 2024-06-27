eedcda2046d4e13297647198b8c5fcb6a14fe33d
Drew Harris <harrisd@smu.edu>
2024-06-27 12:23:49 -0700

    fix: eslint

--- analytics-site/src/components/shared/Select.tsx
+++ analytics-site/src/components/shared/Select.tsx
@@ -1,9 +1,7 @@
-import { splitProps } from "solid-js";
 import { JSX } from "solid-js/jsx-runtime";
 
 interface SelectProps extends JSX.InputHTMLAttributes<HTMLSelectElement> {}
 
 export const Select = (props: SelectProps) => {
-  const [options] = splitProps(props, ["options"]);
   return <select class="rounded border border-neutral-300 p-1" {...props} />;
 };
