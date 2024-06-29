1d44eb92085129eba1f305312010be539e0428b0
Vidyoot Senthil <vidyoots@gmail.com>
2024-06-28 23:56:33 -0500

    cleanup: removing ClientDatasetConfiguration from server

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -330,7 +330,6 @@ impl Modify for SecurityAddon {
             data::models::DatasetUsageCount,
             data::models::DatasetDTO,
             data::models::DatasetUsageCount,
-            data::models::ClientDatasetConfiguration,
             data::models::StripePlan,
             data::models::SlimChunkMetadata,
             data::models::RangeCondition,
