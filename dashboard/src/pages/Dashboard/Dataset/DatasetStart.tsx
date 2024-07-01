5961d0452e112c51f72d9f33bd9bea000f83aae7
skeptrune <nick.k@trieve.ai>
2024-07-01 15:11:06 -0700

    content: update doc page links to new docs + clarify info descriptions

--- dashboard/src/pages/Dashboard/Dataset/DatasetStart.tsx
+++ dashboard/src/pages/Dashboard/Dataset/DatasetStart.tsx
@@ -338,7 +338,7 @@ export const DatasetStart = () => {
               Initial Request Examples
             </h2>
             <div class="flex flex-col space-y-4">
-              <p>1. Add searchable data</p>
+              <p>1. Add a searchable chunk</p>
               <div class="flex w-fit space-x-4 rounded-md border border-blue-600/20 bg-blue-50 px-4 py-4">
                 <div class="flex">
                   <div class="flex-shrink-0">
@@ -347,20 +347,19 @@ export const DatasetStart = () => {
                   </div>
                   <div class="ml-3">
                     <h3 class="text-sm font-semibold text-blue-800">
-                      This example only uses 3 of 10 potential request
-                      parameters
+                      Create a chunk
                     </h3>
                     <div class="mt-2 text-sm text-blue-700">
                       <p>
                         Read our{" "}
                         <a
-                          href="https://redoc.trieve.ai/redoc#tag/chunk/operation/create_chunk"
+                          href="https://docs.trieve.ai/api-reference/chunk/create-or-upsert-chunk-or-chunks"
                           class="underline"
                         >
-                          OpenAPI docs for chunks
+                          API reference for creating chunks
                         </a>{" "}
-                        to see how to add metadata for filtering, a timestamp
-                        for recency biasing, tags, and more.
+                        to see how to add tags and prices for filtering,
+                        timestamps for recency biasing, and more.
                       </p>
                     </div>
                   </div>
@@ -378,19 +377,19 @@ export const DatasetStart = () => {
                   </div>
                   <div class="ml-3">
                     <h3 class="text-sm font-semibold text-blue-800">
-                      This example only uses 3 of 9 potential request parameters
+                      Search chunks
                     </h3>
                     <div class="mt-2 text-sm text-blue-700">
                       <p>
                         Read our{" "}
                         <a
-                          href="https://redoc.trieve.ai/redoc#tag/chunk/operation/search_chunk"
+                          href="https://docs.trieve.ai/api-reference/chunk/search"
                           class="underline"
                         >
-                          OpenAPI docs for search
+                          API reference for searching chunks
                         </a>{" "}
-                        to see how to add filters, manually weight semantic vs.
-                        full-text importance, bias for recency, and more.
+                        to see how to add filters, set highlight parameters,
+                        bias for recency, and more.
                       </p>
                     </div>
                   </div>
