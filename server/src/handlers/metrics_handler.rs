3d8e63efe71856f50f302538d280a371dc216e2a
Drew Harris <harrisd@smu.edu>
2024-07-01 16:56:36 -0500

    feat: add all queues to metrics endpoint

--- server/src/handlers/metrics_handler.rs
+++ server/src/handlers/metrics_handler.rs
@@ -1,9 +1,13 @@
 use actix_web::{web, HttpResponse};
 
-use crate::metrics::Metrics;
+use crate::{data::models::RedisPool, metrics::Metrics};
 
-pub async fn get_metrics(metrics: web::Data<Metrics>) -> Result<HttpResponse, actix_web::Error> {
+pub async fn get_metrics(
+    metrics: web::Data<Metrics>,
+    redis_pool: web::Data<RedisPool>,
+) -> Result<HttpResponse, actix_web::Error> {
+    let _ = metrics.update_queue_gauges(redis_pool).await?;
+
     let reponse = metrics.get_response();
-    // Set the proper content-type for prometheus
     Ok(HttpResponse::Ok().content_type("text/plain").body(reponse))
 }
