919c2c047f310d0ef3dcc6d4a43547ac12671d1b
cdxker <fdenzell@gmail.com>
2024-07-01 10:37:10 -0700

    feature: cleanup Dockerfiles

--- docker/mc/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mc config host add myminio http://${S3_URI} ${MINIO_ROOT_USER} ${MINIO_ROOT_PASSWORD};
-mc alias set myminio http://${S3_URI} ${MINIO_ROOT_USER} ${MINIO_ROOT_PASSWORD};
-mc admin user add myminio ${S3_ACCESS_KEY} ${S3_SECRET_KEY};
-mc admin policy attach myminio readwrite --user ${S3_ACCESS_KEY};
-mc mb myminio/${S3_BUCKET};
-exit $?;
