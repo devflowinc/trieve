2559f8d16b870467f4ac496e89d2d92b29846526
Drew Harris <harrisd@smu.edu>
2024-07-01 10:32:48 -0500

    feat: copy code snippet to clipboard

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,27 +1,9 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
-import md from "vite-plugin-solid-markdown";
-import remarkGfm from "remark-gfm";
-import rehypePrettyCode from "rehype-pretty-code";
 import runtimeEnv from "vite-plugin-runtime-env";
 
 export default defineConfig({
   plugins: [
-    md({
-      wrapperClasses: "prose prose-sm m-auto text-left",
-      rehypePlugins: [
-        [
-          rehypePrettyCode,
-          {
-            theme: {
-              dark: "one-dark-pro",
-              light: "light-plus",
-            },
-          },
-        ],
-      ],
-      remarkPlugins: [remarkGfm],
-    }),
     solid({
       extensions: [".mdx", ".md"],
     }),
