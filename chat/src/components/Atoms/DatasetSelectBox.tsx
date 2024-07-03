1c6b1dc7e7beae6fa957bceac830e92ff6c26b50
aaryanpunia <aaryan57231punia@gmail.com>
2024-07-03 09:06:17 -0700

    wip: chat search input box losing focus automatically

--- chat/src/components/Atoms/DatasetSelectBox.tsx
+++ chat/src/components/Atoms/DatasetSelectBox.tsx
@@ -1,5 +1,13 @@
 import { FaSolidCheck } from "solid-icons/fa";
-import { Show, For, useContext } from "solid-js";
+import {
+  Show,
+  For,
+  useContext,
+  createSignal,
+  createEffect,
+  Switch,
+  Match,
+} from "solid-js";
 import {
   Menu,
   MenuItem,
@@ -8,50 +16,74 @@ import {
   PopoverPanel,
 } from "terracotta";
 import { UserContext } from "../contexts/UserContext";
+import { DatasetAndUsageDTO } from "../../utils/apiTypes";
+import createFuzzySearch from "@nozbe/microfuzz";
+import { FiChevronDown, FiChevronUp } from "solid-icons/fi";
 
 export const DatasetSelectBox = () => {
   const userContext = useContext(UserContext);
+  const $datasetsAndUsages = userContext.datasetsAndUsages;
+  const $currentDataset = userContext.currentDataset;
+
+  const [datasetSearchQuery, setDatasetSearchQuery] = createSignal("");
+
+  const [datasetSearchResults, setDatasetSearchResults] = createSignal<
+    DatasetAndUsageDTO[]
+  >([]);
+  createEffect(() => {
+    const datasetListOrEmpty = userContext.datasetsAndUsages?.() ?? [];
+    if (datasetSearchQuery() === "") {
+      setDatasetSearchResults(datasetListOrEmpty);
+    } else {
+      const fuzzy = createFuzzySearch(datasetListOrEmpty, {
+        getText: (item: DatasetAndUsageDTO) => {
+          return [item.dataset.name];
+        },
+      });
+      const results = fuzzy(datasetSearchQuery() ?? "");
+      setDatasetSearchResults(results.map((result) => result.item));
+    }
+  });
 
   return (
-    <Show when={userContext.datasetsAndUsages?.()?.length != 0}>
+    <Show when={$datasetsAndUsages?.().length != 0}>
       <Popover defaultOpen={false} class="relative">
         {({ isOpen, setState }) => (
           <>
             <PopoverButton
               aria-label="Toggle filters"
               type="button"
-              class="flex items-center space-x-1 pb-1 text-sm"
+              class="flex min-w-fit items-center space-x-1 pb-1 text-sm"
             >
-              <span class="line-clamp-1 text-left text-sm">
-                {userContext.currentDataset?.()?.dataset?.name}
+              <span class="line-clamp-1 min-w-fit text-left text-sm">
+                {$currentDataset?.()?.dataset.name}
               </span>
-              <svg
-                fill="currentColor"
-                stroke-width="0"
-                style={{ overflow: "visible", color: "currentColor" }}
-                viewBox="0 0 16 16"
-                class="h-3.5 w-3.5"
-                height="1em"
-                width="1em"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M2 5.56L2.413 5h11.194l.393.54L8.373 11h-.827L2 5.56z" />
-              </svg>
+              <Switch>
+                <Match when={isOpen()}>
+                  <FiChevronUp class="h-3.5 w-3.5" />
+                </Match>
+                <Match when={!isOpen()}>
+                  <FiChevronDown class="h-3.5 w-3.5" />
+                </Match>
+              </Switch>
             </PopoverButton>
             <Show when={isOpen()}>
               <PopoverPanel
                 unmount={false}
-                class="absolute bottom-5 left-0 z-20 mt-2 h-fit w-[180px] rounded-md border bg-white p-1 dark:bg-neutral-800"
+                class="absolute bottom-5 left-0 z-10 mt-2 h-fit w-[180px] rounded-md border bg-white p-1 dark:bg-neutral-800"
               >
                 <Menu class="mx-1 space-y-0.5">
-                  <For each={userContext.datasetsAndUsages?.()}>
+                  <input
+                    placeholder="Search datasets..."
+                    class="mb-2 flex w-full items-center justify-between rounded bg-neutral-300 p-1 text-sm text-black outline-none dark:bg-neutral-700 dark:hover:text-white dark:focus:text-white"
+                    onInput={(e) => {
+                      setDatasetSearchQuery(e.target.value);
+                    }}
+                    value={datasetSearchQuery()}
+                  />
+
+                  <For each={datasetSearchResults()}>
                     {(datasetItem) => {
-                      const onClick = (e: Event) => {
-                        e.preventDefault();
-                        e.stopPropagation();
-                        userContext.setCurrentDataset(datasetItem);
-                        setState(false);
-                      };
                       return (
                         <MenuItem
                           as="button"
@@ -60,19 +92,28 @@ export const DatasetSelectBox = () => {
                               true,
                             "bg-neutral-300 dark:bg-neutral-700":
                               datasetItem.dataset.id ===
-                              userContext.currentDataset?.()?.dataset.id,
+                              $currentDataset?.()?.dataset.id,
                           }}
-                          onClick={onClick}
+                          onClick={(e: Event) => {
+                            e.preventDefault();
+                            e.stopPropagation();
+                            userContext.setCurrentDataset(datasetItem);
+                            setState(false);
+                          }}
                         >
                           <div class="break-all px-1 text-left text-sm">
                             {datasetItem.dataset.name}
                           </div>
-                          {datasetItem.dataset.id ==
-                            userContext.currentDataset?.()?.dataset.id && (
+                          <Show
+                            when={
+                              datasetItem.dataset.id ==
+                              $currentDataset?.()?.dataset.id
+                            }
+                          >
                             <span>
                               <FaSolidCheck class="text-sm" />
                             </span>
-                          )}
+                          </Show>
                         </MenuItem>
                       );
                     }}
