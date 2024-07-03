6bc19bcbcab02e2016338fefabc3852fbce98b04
Drew Harris <harrisd@smu.edu>
2024-07-03 17:07:04 -0500

    fix: clicking off select box

--- shared/ui/SelectBox.tsx
+++ shared/ui/SelectBox.tsx
@@ -2,6 +2,7 @@ import { FaSolidCheck } from "solid-icons/fa";
 import { TbSelector } from "solid-icons/tb";
 import { createSignal, For, JSX, Show } from "solid-js";
 import {
+  DisclosureStateChild,
   Listbox,
   ListboxButton,
   ListboxOption,
@@ -34,49 +35,53 @@ export const Select = <T,>(props: SelectProps<T>) => {
           {props.display(props.selected)}
           <TbSelector />
         </ListboxButton>
-        <Show when={open()}>
-          <div class="relative w-full">
-            <ListboxOptions
-              unmount={false}
-              class="absolute z-40 shadow mt-1 max-h-60 w-full overflow-auto rounded-md bg-white text-base outline outline-1 outline-gray-300 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
-            >
-              <For each={props.options}>
-                {(option): JSX.Element => (
-                  <ListboxOption
-                    class="group min-w-full w-[max-content] rounded-md focus:outline-none"
-                    value={option}
-                  >
-                    {({ isSelected }): JSX.Element => (
-                      <div
-                        classList={{
-                          "bg-magenta-100 text-magenta-900": isSelected(),
-                          "text-gray-900": !isSelected(),
-                          "group-hover:bg-magenta-50 whitespace-nowrap flex p-2 justify-between px-2 items-center gap-2 group-hover:text-magenta-900 relative cursor-default select-none ":
-                            true,
-                        }}
-                        onClick={() => {
-                          props.onSelected(option);
-                          setOpen(false);
-                        }}
+        <DisclosureStateChild>
+          {({ isOpen }): JSX.Element => (
+            <Show when={isOpen()}>
+              <div class="relative w-full">
+                <ListboxOptions
+                  unmount={false}
+                  class="absolute z-40 shadow mt-1 max-h-60 w-full overflow-auto rounded-md bg-white text-base outline outline-1 outline-gray-300 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+                >
+                  <For each={props.options}>
+                    {(option): JSX.Element => (
+                      <ListboxOption
+                        class="group min-w-full w-[max-content] rounded-md focus:outline-none"
+                        value={option}
                       >
-                        <span>{props.display(option)}</span>
-                        {isSelected() ? (
-                          <span
+                        {({ isSelected }): JSX.Element => (
+                          <div
                             classList={{
-                              "": true,
+                              "bg-magenta-100 text-magenta-900": isSelected(),
+                              "text-gray-900": !isSelected(),
+                              "group-hover:bg-magenta-50 whitespace-nowrap flex p-2 justify-between px-2 items-center gap-2 group-hover:text-magenta-900 relative cursor-default select-none ":
+                                true,
+                            }}
+                            onClick={() => {
+                              props.onSelected(option);
+                              setOpen(false);
                             }}
                           >
-                            <FaSolidCheck />
-                          </span>
-                        ) : null}
-                      </div>
+                            <span>{props.display(option)}</span>
+                            {isSelected() ? (
+                              <span
+                                classList={{
+                                  "": true,
+                                }}
+                              >
+                                <FaSolidCheck />
+                              </span>
+                            ) : null}
+                          </div>
+                        )}
+                      </ListboxOption>
                     )}
-                  </ListboxOption>
-                )}
-              </For>
-            </ListboxOptions>
-          </div>
-        </Show>
+                  </For>
+                </ListboxOptions>
+              </div>
+            </Show>
+          )}
+        </DisclosureStateChild>
       </Listbox>
     </>
   );
