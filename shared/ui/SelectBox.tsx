a63bb39350b400e7805bb77f5d9452db0439bac6
Drew Harris <harrisd@smu.edu>
2024-07-03 16:03:23 -0500

    feat: add label to selectbox

--- shared/ui/SelectBox.tsx
+++ shared/ui/SelectBox.tsx
@@ -14,66 +14,70 @@ interface SelectProps<T> {
   selected: T;
   onSelected: (option: T) => void;
   class?: string;
+  label?: JSX.Element;
 }
 
 export const Select = <T,>(props: SelectProps<T>) => {
   const [open, setOpen] = createSignal(false);
   return (
-    <Listbox
-      class={`bg-neutral-200/70 min-w-[100px] relative border rounded border-neutral-300 ${props.class}`}
-      value={props.selected}
-      defaultOpen={false}
-    >
-      <ListboxButton
-        class="flex py-1 text-sm px-3 w-full justify-between gap-2 items-center"
-        onClick={() => setOpen(!open())}
+    <>
+      <Show when={props.label}>{(label) => label()}</Show>
+      <Listbox
+        class={`bg-neutral-200/70 min-w-[100px] relative border rounded border-neutral-300 ${props.class}`}
+        value={props.selected}
+        defaultOpen={false}
       >
-        {props.display(props.selected)}
-        <TbSelector />
-      </ListboxButton>
-      <Show when={open()}>
-        <div class="relative w-full">
-          <ListboxOptions
-            unmount={false}
-            class="absolute z-40 shadow mt-1 max-h-60 w-full overflow-auto rounded-md bg-white text-base outline outline-1 outline-gray-300 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
-          >
-            <For each={props.options}>
-              {(option): JSX.Element => (
-                <ListboxOption
-                  class="group min-w-full w-[max-content] rounded-md focus:outline-none"
-                  value={option}
-                >
-                  {({ isActive, isSelected }): JSX.Element => (
-                    <div
-                      classList={{
-                        "bg-magenta-100 text-magenta-900": isSelected(),
-                        "text-gray-900": !isSelected(),
-                        "group-hover:bg-magenta-50 whitespace-nowrap flex p-2 justify-between px-2 items-center gap-2 group-hover:text-magenta-900 relative cursor-default select-none ":
-                          true,
-                      }}
-                      onClick={() => {
-                        props.onSelected(option);
-                        setOpen(false);
-                      }}
-                    >
-                      <span>{props.display(option)}</span>
-                      {isSelected() ? (
-                        <span
-                          classList={{
-                            "": true,
-                          }}
-                        >
-                          <FaSolidCheck />
-                        </span>
-                      ) : null}
-                    </div>
-                  )}
-                </ListboxOption>
-              )}
-            </For>
-          </ListboxOptions>
-        </div>
-      </Show>
-    </Listbox>
+        <ListboxButton
+          class="flex py-1 text-sm px-3 w-full justify-between gap-2 items-center"
+          onClick={() => setOpen(!open())}
+        >
+          {props.display(props.selected)}
+          <TbSelector />
+        </ListboxButton>
+        <Show when={open()}>
+          <div class="relative w-full">
+            <ListboxOptions
+              unmount={false}
+              class="absolute z-40 shadow mt-1 max-h-60 w-full overflow-auto rounded-md bg-white text-base outline outline-1 outline-gray-300 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+            >
+              <For each={props.options}>
+                {(option): JSX.Element => (
+                  <ListboxOption
+                    class="group min-w-full w-[max-content] rounded-md focus:outline-none"
+                    value={option}
+                  >
+                    {({ isActive, isSelected }): JSX.Element => (
+                      <div
+                        classList={{
+                          "bg-magenta-100 text-magenta-900": isSelected(),
+                          "text-gray-900": !isSelected(),
+                          "group-hover:bg-magenta-50 whitespace-nowrap flex p-2 justify-between px-2 items-center gap-2 group-hover:text-magenta-900 relative cursor-default select-none ":
+                            true,
+                        }}
+                        onClick={() => {
+                          props.onSelected(option);
+                          setOpen(false);
+                        }}
+                      >
+                        <span>{props.display(option)}</span>
+                        {isSelected() ? (
+                          <span
+                            classList={{
+                              "": true,
+                            }}
+                          >
+                            <FaSolidCheck />
+                          </span>
+                        ) : null}
+                      </div>
+                    )}
+                  </ListboxOption>
+                )}
+              </For>
+            </ListboxOptions>
+          </div>
+        </Show>
+      </Listbox>
+    </>
   );
 };
