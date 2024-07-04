f87de244b1ccb808aff3da662caefb6418d32c57
cdxker <fdenzell@gmail.com>
2024-07-04 01:23:35 -0700

    bugfix: metrics handler support for pgbulk queue, fix bearer auth bug

--- server/src/handlers/metrics_handler.rs
+++ server/src/handlers/metrics_handler.rs
@@ -13,36 +13,52 @@ pub struct Metrics {
     pub delete_processing_gauge: Gauge,
     pub ingest_processing_gauge: Gauge,
     pub group_update_processing_gauge: Gauge,
+    pub pgbulk_queue_gauge: Gauge,
+    pub pgbulk_processing_gauge: Gauge,
 }
 
 impl Metrics {
     pub fn new() -> Result<Self, Error> {
         let registry = Registry::new();
 
-        let ingest_queue_gauge = Gauge::new("ingest_queue", "number of items in the ingest queue")?;
+        let ingest_queue_gauge =
+            Gauge::new("tr_ingest_queue", "number of items in the ingest queue")?;
         registry.register(Box::new(ingest_queue_gauge.clone()))?;
 
-        let delete_queue_gauge = Gauge::new("delete_queue", "number of items in the delete queue")?;
+        let delete_queue_gauge =
+            Gauge::new("tr_delete_queue", "number of items in the delete queue")?;
         registry.register(Box::new(delete_queue_gauge.clone()))?;
 
         let file_queue_gauge =
-            Gauge::new("file_queue", "number of items in the file ingest queue")?;
+            Gauge::new("tr_file_queue", "number of items in the file ingest queue")?;
         registry.register(Box::new(file_queue_gauge.clone()))?;
 
-        let update_queue_gauge =
-            Gauge::new("group_update_queue", "number of items in the update queue")?;
+        let update_queue_gauge = Gauge::new(
+            "tr_group_update_queue",
+            "number of items in the update queue",
+        )?;
         registry.register(Box::new(update_queue_gauge.clone()))?;
 
+        let pgbulk_queue_gauge =
+            Gauge::new("tr_pg_bulk_queue", "number of items in the pg bulk queue")?;
+        registry.register(Box::new(pgbulk_queue_gauge.clone()))?;
+
+        let pgbulk_processing_gauge = Gauge::new(
+            "tr_pg_bulk_processing",
+            "number of items being bulk processed",
+        )?;
+        registry.register(Box::new(pgbulk_processing_gauge.clone()))?;
+
         let file_processing_gauge =
-            Gauge::new("file_processing", "number of files being processed")?;
+            Gauge::new("tr_file_processing", "number of files being processed")?;
         registry.register(Box::new(file_processing_gauge.clone()))?;
 
         let delete_processing_gauge =
-            Gauge::new("delete_processing", "number of files being deleted")?;
+            Gauge::new("tr_delete_processing", "number of files being deleted")?;
         registry.register(Box::new(delete_processing_gauge.clone()))?;
 
         let ingest_processing_gauge =
-            Gauge::new("ingest_processing", "number of chunks being ingested")?;
+            Gauge::new("tr_ingest_processing", "number of chunks being ingested")?;
         registry.register(Box::new(ingest_processing_gauge.clone()))?;
 
         let group_update_processing_gauge = Gauge::new(
@@ -54,6 +70,8 @@ impl Metrics {
         Ok(Metrics {
             registry,
             ingest_queue_gauge,
+            pgbulk_queue_gauge,
+            pgbulk_processing_gauge,
             file_queue_gauge,
             delete_queue_gauge,
             update_queue_gauge,
@@ -82,7 +100,9 @@ impl Metrics {
             processing,
             group_update_queue,
             group_update_processing,
-        ): (i32, i32, i32, i32, i32, i32, i32, i32) = redis::pipe()
+            pg_bulk_queue,
+            pg_bulk_processing,
+        ): (i32, i32, i32, i32, i32, i32, i32, i32, i32, i32) = redis::pipe()
             .cmd("LLEN")
             .arg("ingestion")
             .cmd("LLEN")
@@ -99,6 +119,10 @@ impl Metrics {
             .arg("group_update_queue")
             .cmd("LLEN")
             .arg("group_update_processing")
+            .cmd("LLEN")
+            .arg("bulk_pg_queue")
+            .cmd("LLEN")
+            .arg("bulk_pg_processing")
             .query_async(&mut *redis_conn)
             .await
             .map_err(|err| ServiceError::InternalServerError(err.to_string()))?;
@@ -113,6 +137,8 @@ impl Metrics {
         self.update_queue_gauge.set(group_update_queue as f64);
         self.group_update_processing_gauge
             .set(group_update_processing as f64);
+        self.pgbulk_queue_gauge.set(pg_bulk_queue as f64);
+        self.pgbulk_processing_gauge.set(pg_bulk_processing as f64);
 
         Ok(())
     }
@@ -132,13 +158,13 @@ fn check_x_api_access(req: &actix_web::HttpRequest) -> bool {
     let auth_api_key = req.headers().get("Authorization");
 
     if let Ok(admin_key) = admin_key {
-        if let Some(api_key) = x_api_key {
-            return api_key.to_str().unwrap_or_default() == admin_key.as_str();
+        if let Some(api_key_provided) = x_api_key {
+            return api_key_provided.to_str().unwrap_or_default() == admin_key.as_str();
         }
 
-        if let Some(api_key) = auth_api_key {
-            return format!("Bearer: {}", api_key.to_str().unwrap_or_default()).as_str()
-                == admin_key.as_str();
+        if let Some(api_key_provided) = auth_api_key {
+            return format!("Bearer {}", admin_key.as_str()).as_str()
+                == api_key_provided.to_str().unwrap_or_default();
         }
     }
 
