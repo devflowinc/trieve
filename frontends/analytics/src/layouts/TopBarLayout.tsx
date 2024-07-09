c506b92f02f2a0501d5a4ab10a1dcd3a572d8b09
Drew Harris <harrisd@smu.edu>
2024-07-09 16:36:48 -0500

    styling: fix bottom of sidebar

--- frontends/analytics/src/layouts/TopBarLayout.tsx
+++ frontends/analytics/src/layouts/TopBarLayout.tsx
@@ -78,7 +78,7 @@ export const TopBarLayout: ParentComponent = (props) => {
   };
 
   return (
-    <div class="grid min-h-screen grid-cols-[300px_1fr] bg-neutral-100">
+    <div class="grid min-h-screen grid-cols-[300px_1fr] items-start bg-neutral-100">
       <Sidebar
         datasetOptions={datasetsQuery.data || []}
         selectedDataset={selectedDataset()}
