e897760f2fbec27113bd44c47c0494fb6f950a7d
Drew Harris <harrisd@smu.edu>
2024-07-09 14:24:59 -0500

    styling: add user info on bottom

--- frontends/analytics/src/components/Sidebar.tsx
+++ frontends/analytics/src/components/Sidebar.tsx
@@ -5,6 +5,9 @@ import { DatasetAndUsage } from "shared/types";
 import { usePathname } from "../hooks/usePathname";
 import { useBetterNav } from "../utils/useBetterNav";
 import { Select } from "shared/ui";
+import { AiOutlineUser } from "solid-icons/ai";
+import { apiHost } from "../utils/apiHost";
+import { IoLogOutOutline } from "solid-icons/io";
 interface NavbarProps {
   datasetOptions: DatasetAndUsage[];
   selectedDataset: DatasetAndUsage | null;
@@ -29,13 +32,14 @@ export const Sidebar = (props: NavbarProps) => {
   const navigate = useBetterNav();
 
   return (
-    <div class="justify-between border border-r-neutral-300 bg-neutral-50 p-2 px-4 pr-8">
+    <div class="relative flex min-h-screen flex-col justify-start border border-r-neutral-300 bg-neutral-50 p-2 px-4 pr-8">
       <div class="items-center gap-3">
         <img
           class="h-12 w-12 cursor-pointer"
           src="https://cdn.trieve.ai/trieve-logo.png"
           alt="Logo"
         />
+        <div class="h-4" />
         <Select
           label={<div class="text-sm opacity-60">Organization</div>}
           class="min-w-[150px]"
@@ -63,7 +67,7 @@ export const Sidebar = (props: NavbarProps) => {
           )}
         </Show>
       </div>
-      <div class="items-center gap-4">
+      <div class="items-center gap-4 pt-4">
         <For each={navbarRoutes}>
           {(link) => {
             return (
@@ -83,6 +87,33 @@ export const Sidebar = (props: NavbarProps) => {
             );
           }}
         </For>
+      </div>
+      <div class="absolute bottom-0 left-0 right-0 flex flex-col items-start justify-self-end border-t px-4 py-4">
+        <div class="flex items-center gap-2">
+          <p>{userContext?.user().email}</p>
+          <AiOutlineUser class="h-4 w-4" />
+        </div>
+        <button
+          class="flex items-center gap-2 hover:text-fuchsia-800"
+          onClick={() => {
+            void fetch(`${apiHost}/auth?redirect_uri=${window.origin}`, {
+              method: "DELETE",
+              credentials: "include",
+            }).then((res) => {
+              res
+                .json()
+                .then((res) => {
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+                  window.location.href = res.logout_url;
+                })
+                .catch(() => {
+                  console.log("error");
+                });
+            });
+          }}
+        >
+          Log Out <IoLogOutOutline class="inline-block h-4 w-4" />
+        </button>
       </div>
     </div>
   );
