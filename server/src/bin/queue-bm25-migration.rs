5dd074ca23d28f6ad06d1a22408c6b3cdc40801a
cdxker <fdenzell@gmail.com>
2024-07-17 10:59:32 -0700

    observerability: add timestamp logs back to workers

--- server/src/bin/queue-bm25-migration.rs
+++ server/src/bin/queue-bm25-migration.rs
@@ -1,3 +1,4 @@
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Layer};
 use trieve_server::{
     data::models::{MigratePointMessage, MigrationMode},
     errors::ServiceError,
@@ -9,6 +10,14 @@ use trieve_server::{
 #[tokio::main]
 async fn main() -> Result<(), ServiceError> {
     dotenvy::dotenv().ok();
+    tracing_subscriber::Registry::default()
+        .with(
+            tracing_subscriber::fmt::layer().with_filter(
+                EnvFilter::from_default_env()
+                    .add_directive(tracing_subscriber::filter::LevelFilter::INFO.into()),
+            ),
+        )
+        .init();
 
     let redis_url = get_env!("REDIS_URL", "REDIS_URL is not set");
     let redis_connections: u32 = std::env::var("REDIS_CONNECTIONS")
@@ -40,11 +49,11 @@ async fn main() -> Result<(), ServiceError> {
         .collect();
 
     for collection in collections {
-        println!("queue'ing collection: {:?}", collection);
+        log::info!("queue'ing collection: {:?}", collection);
 
         let mut offset = Some(uuid::Uuid::nil().to_string());
 
-        while let Some(cur_offset) = offset {
+        while let Some(cur_offset) = offset.clone() {
             let (qdrant_point_ids, new_offset) = scroll_qdrant_collection_ids(
                 collection.clone(),
                 Some(cur_offset.to_string()),
@@ -58,7 +67,7 @@ async fn main() -> Result<(), ServiceError> {
                 .expect("Failed to connect to redis");
 
             let message = serde_json::to_string(&MigratePointMessage {
-                qdrant_point_ids,
+                qdrant_point_ids: qdrant_point_ids.clone(),
                 from_collection: collection.clone(),
                 to_collection: format!("{}_bm25", collection),
                 mode: MigrationMode::BM25 {
@@ -78,6 +87,12 @@ async fn main() -> Result<(), ServiceError> {
                     ServiceError::BadRequest("Failed to send message to redis".to_string())
                 })?;
 
+            log::info!(
+                "Migrated {:?} points between {:?} and {:?}",
+                qdrant_point_ids.len(),
+                offset.clone(),
+                new_offset.clone()
+            );
             offset = new_offset;
         }
     }
