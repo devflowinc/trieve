3af7d573ccc72e2b6cd5f6829fd830be360676b4
Vidyoot Senthil <vidyoots@gmail.com>
2024-07-29 12:49:29 -0500

    feature: add group, create, upload links to dashboard

--- frontends/dashboard/src/pages/Home.tsx
+++ frontends/dashboard/src/pages/Home.tsx
@@ -1,40 +1,13 @@
 import { createEffect, useContext } from "solid-js";
-import { createToast } from "../components/ShowToasts";
 import { UserContext } from "../contexts/UserContext";
 
 export const Home = () => {
-  const api_host: string = import.meta.env.VITE_API_HOST as string;
-
   const userContext = useContext(UserContext);
 
-  const login = () => {
-    fetch(`${api_host}/auth/me`, {
-      credentials: "include",
-    })
-      .then((res) => {
-        if (res.status === 401) {
-          window.location.href = `${api_host}/auth?redirect_uri=${window.origin}`;
-          return;
-        }
-        return res.json();
-      })
-      .then(() => {
-        window.location.href = `${
-          window.origin
-        }/dashboard/${userContext.selectedOrganizationId?.()}`;
-      })
-      .catch((err) => {
-        console.error(err);
-        createToast({
-          title: "Error",
-          type: "error",
-          message: "Error logging in",
-        });
-      });
-  };
-
   createEffect(() => {
-    login();
+    window.location.href = `${
+      window.origin
+    }/dashboard/${userContext.selectedOrganizationId?.()}`;
   });
   return <div />;
 };
