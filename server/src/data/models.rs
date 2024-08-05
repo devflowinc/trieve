8fbd9670b71a22b5305160d7080d9caaa4625dee
Nick K <nick@arguflow.gg>
2024-08-05 01:23:19 -0700

    cleanup: properly respect max num for highlight array

--- server/src/data/models.rs
+++ server/src/data/models.rs
@@ -4941,7 +4941,7 @@ pub struct HighlightOptions {
     pub highlight_max_length: Option<u32>,
     /// Set highlight_max_num to control the maximum number of highlights per chunk. If not specified, this defaults to 3. It may be less than 3 if no snippets score above the highlight_threshold.
     pub highlight_max_num: Option<u32>,
-    /// Set highlight_window to a number to control the amount of words that are returned around the matched phrases. If not specified, this defaults to 0. This is useful for when you want to show more context around the matched words. When specified, window/2 whitespace separated words are added before and after each highlight in the response's highlights array. If an extended highlight overlaps with another highlight, the overlapping words are only included once.
+    /// Set highlight_window to a number to control the amount of words that are returned around the matched phrases. If not specified, this defaults to 0. This is useful for when you want to show more context around the matched words. When specified, window/2 whitespace separated words are added before and after each highlight in the response's highlights array. If an extended highlight overlaps with another highlight, the overlapping words are only included once. This parameter can be overriden to respect the highlight_max_length param.
     pub highlight_window: Option<u32>,
 }
 
