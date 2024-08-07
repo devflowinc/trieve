1428d699fb4ece93ade848b3cb1ee67918e98fd4
Dens Sumesh <dens.sumesh79@gmail.com>
2024-08-06 21:08:41 -0700

    cleanup: use replacing merge table

--- server/ch_migrations/1721414235_last_row_processed_for_collapse/up.sql
+++ server/ch_migrations/1721414235_last_row_processed_for_collapse/up.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS last_collapsed_dataset
 (
-	id UUID,
-	last_collapsed DateTime,
+    id UUID,
+    last_collapsed DateTime,
     dataset_id UUID,
     created_at DateTime
-) ENGINE = MergeTree()
-ORDER BY (dataset_id, created_at)
-PARTITION BY
-    (toYYYYMM(created_at),
-    dataset_id)
+)
+ENGINE = ReplacingMergeTree(created_at)
+ORDER BY (dataset_id)
+PARTITION BY (toYYYYMM(created_at), dataset_id)
 TTL created_at + INTERVAL 30 DAY;
+
 
