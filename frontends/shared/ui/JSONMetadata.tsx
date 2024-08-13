865de3b293e91baaa6e5a29fa86b4cf5116f07ce
Drew Harris <harrisd@smu.edu>
2024-08-13 13:15:10 -0500

    styling: switch to bracket style

--- frontends/shared/ui/JSONMetadata.tsx
+++ frontends/shared/ui/JSONMetadata.tsx
@@ -36,9 +36,8 @@ export const JSONMetadata = (props: JSONMetadataProps) => {
     <div
       class={cn(
         "flex flex-col items-start rounded-md",
-        "outline-magenta-200 outline outline-[0.5px]",
+        // "outline-magenta-200 outline outline-[0.5px]",
         props.class,
-        props.isAlternate ? "bg-magenta-100" : "bg-magenta-50",
       )}
     >
       <Show when={props.copyJSONButton}>
@@ -115,12 +114,20 @@ const JSONMetadaRow = (props: JSONMetadaRowProps) => {
           // "background-color": value()[0] === "inline" ? undefined : "#fde0ff",
         }}
         class={cn(
-          "flex px-2 my-0.5 rounded-md",
+          "flex px-2 rounded-md",
           // props.isAlternate ? "bg-magenta-100" : "bg-magenta-50",
         )}
       >
-        <div class="font-medium">{props.key}: </div>
+        <div class="font-medium">
+          {props.key}:
+          <Show when={value()[0] === "block"}>
+            <span>{" {"}</span>
+          </Show>
+        </div>
         <div> {value()[1]}</div>
+        <Show when={value()[0] === "block"}>
+          <span class="font-medium">{" }"}</span>
+        </Show>
       </div>
     </Show>
   );
