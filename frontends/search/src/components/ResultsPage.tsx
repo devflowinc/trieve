5d260e8779671d16417d4d3d364e02849cb57587
Drew Harris <harrisd@smu.edu>
2024-08-14 22:12:01 -0500

    feat: add server timings view

--- frontends/search/src/components/ResultsPage.tsx
+++ frontends/search/src/components/ResultsPage.tsx
@@ -46,12 +46,28 @@ import {
 import { downloadFile } from "../utils/downloadFile";
 import ScoreChunk from "./ScoreChunk";
 import { FiEye } from "solid-icons/fi";
+import { ServerTimings } from "./ServerTimings";
 
 export interface ResultsPageProps {
   search: SearchStore;
   rateQuery: Accessor<boolean>;
   setRatingQuery: Setter<boolean>;
 }
+
+export type ServerTiming = {
+  name: string;
+  duration: number;
+};
+
+const parseServerTimings = (labels: string[]): ServerTiming[] => {
+  return labels.map((label) => {
+    const [name, rawDuration] = label.split(";");
+    return {
+      name,
+      duration: parseInt(rawDuration.substring(4)),
+    };
+  });
+};
 
 const ResultsPage = (props: ResultsPageProps) => {
   const apiHost = import.meta.env.VITE_API_HOST as string;
@@ -87,6 +103,9 @@ const ResultsPage = (props: ResultsPageProps) => {
     rating: 5,
     note: "",
   });
+
+  const [serverTimings, setServerTimings] = createSignal<ServerTiming[]>([]);
+  const [showServerTimings, setShowServerTimings] = createSignal(false);
 
   const fetchChunkCollections = () => {
     if (!$currentUser?.()) return;
@@ -190,6 +209,33 @@ const ResultsPage = (props: ResultsPageProps) => {
       return null;
     }
   });
+
+  const ShowServerTimings = () => {
+    return (
+      <div class="w-full self-start">
+        <div class="flex items-center space-x-2 self-start">
+          <label class="flex items-center space-x-2">
+            <input
+              type="checkbox"
+              class="text-primary-600 focus:ring-primary-500 h-3 w-3 rounded border-gray-300"
+              checked={showServerTimings()}
+              onChange={(e) => {
+                setShowServerTimings(e.target.checked);
+              }}
+            />
+            <span class="text-sm font-medium text-gray-900 dark:text-white">
+              Show server timings
+            </span>
+          </label>
+        </div>
+        <div class="w-full">
+          <Show when={showServerTimings()}>
+            <ServerTimings timings={serverTimings()} />
+          </Show>
+        </div>
+      </div>
+    );
+  };
 
   createEffect(
     on([() => props.search.debounced.version, dataset, page], () => {
@@ -325,6 +371,18 @@ const ResultsPage = (props: ResultsPageProps) => {
             if (resultingChunks.length === 0) {
               setNoResults(true);
             }
+
+            // Handle server timing
+            const serverTiming = response.headers.get("Server-Timing");
+            if (serverTiming) {
+              const metrics = serverTiming.split(",");
+              try {
+                setServerTimings(parseServerTimings(metrics));
+              } catch {
+                setServerTimings([]);
+                console.error("Failed to parse server timing");
+              }
+            }
           });
         } else {
           void response
@@ -378,7 +436,7 @@ const ResultsPage = (props: ResultsPageProps) => {
           </FullScreenModal>
         </Portal>
       </Show>
-      <div class="mt-12 flex w-full flex-col items-center space-y-4">
+      <div class="flex w-full flex-col items-center gap-4 pt-12">
         <Switch>
           <Match when={loading()}>
             <div
@@ -397,6 +455,7 @@ const ResultsPage = (props: ResultsPageProps) => {
             </div>
           </Match>
           <Match when={!loading() && groupResultChunks().length == 0}>
+            <ShowServerTimings />
             <div class="flex w-full max-w-screen-2xl flex-col space-y-4">
               <For each={resultChunks()}>
                 {(chunk) => (
@@ -496,6 +555,7 @@ const ResultsPage = (props: ResultsPageProps) => {
             </div>
           </Match>
           <Match when={!loading() && groupResultChunks().length > 0}>
+            <ShowServerTimings />
             <For each={groupResultChunks()}>
               {(groupResult) => {
                 const [groupExpanded, setGroupExpanded] = createSignal(true);
@@ -505,7 +565,7 @@ const ResultsPage = (props: ResultsPageProps) => {
                 };
 
                 return (
-                  <div class="flex w-full max-w-screen-2xl flex-col space-y-4">
+                  <div class="flex w-full max-w-screen-2xl flex-col gap-4">
                     <div
                       onClick={toggle}
                       classList={{
---
+++ frontends/search/src/components/ServerTimings.tsx
@@ -0,0 +1,77 @@
+import { createMemo, createSignal, For } from "solid-js";
+import { ServerTiming } from "./ResultsPage";
+
+interface ServerTimingsProps {
+  timings: ServerTiming[];
+}
+export const ServerTimings = (props: ServerTimingsProps) => {
+  const [timingsBox, setTimingsBox] = createSignal<HTMLDivElement>();
+  const totalTime = createMemo(() => {
+    return props.timings.reduce((acc, t) => acc + t.duration, 0);
+  });
+
+  const labels = createMemo(() => {
+    return props.timings.map((t) => t.name);
+  });
+
+  const banners = createMemo(() => {
+    console.log(timingsBox());
+    const availableWidth =
+      document.getElementById("timingsBox")?.clientWidth ?? 0;
+    console.log(availableWidth);
+    if (availableWidth === 0) return [];
+
+    const bannerComponents = [];
+    let usedWidth = 0;
+
+    for (let i = 0; i < props.timings.length; i++) {
+      const timing = props.timings[i];
+      const width = (timing.duration / totalTime()) * availableWidth;
+      bannerComponents.push(
+        <div
+          class={`grid w-full place-items-center bg-magenta-700`}
+          style={{
+            width: `${width}px`,
+            height: "24px",
+            "margin-left": `${usedWidth}px`,
+          }}
+        >
+          <div class="h-full w-full pt-[2px] text-center align-middle text-xs">
+            {timing.duration}ms
+          </div>
+        </div>,
+      );
+      usedWidth += width;
+    }
+
+    return bannerComponents;
+  });
+
+  return (
+    <div class="py-2">
+      <div class="pl-2 font-medium">Total Time: {totalTime()}ms</div>
+      <div class="flex">
+        <div class="shrink px-2 pr-4">
+          <For each={labels()}>
+            {(name) => (
+              <div>
+                <div class="absolute w-[calc(100%-50px)] border border-neutral-800/80" />
+                {name}
+              </div>
+            )}
+          </For>
+          <div class="absolute w-[calc(100%-50px)] border border-neutral-800/80" />
+        </div>
+        <div class="mr-8 grow">
+          <div
+            id="timingsBox"
+            ref={setTimingsBox}
+            class="grow border-l border-l-neutral-800/80"
+          >
+            <For each={banners()}>{(component) => component}</For>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
