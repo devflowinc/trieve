6da020aa3ae4f8efa886b2ac7dcceb006da16341
Drew Harris <harrisd@smu.edu>
2024-08-14 22:29:39 -0500

    styling: use full container width

--- frontends/search/src/components/ServerTimings.tsx
+++ frontends/search/src/components/ServerTimings.tsx
@@ -6,25 +6,39 @@ interface ServerTimingsProps {
 }
 export const ServerTimings = (props: ServerTimingsProps) => {
   const [timingsBox, setTimingsBox] = createSignal<HTMLDivElement>();
+  const [fullContainer, setFullContainer] = createSignal<HTMLDivElement>();
   const totalTime = createMemo(() => {
     return props.timings.reduce((acc, t) => acc + t.duration, 0);
   });
 
   const [availableWidth, setAvailableWidth] = createSignal(0);
 
-  onMount(() => {
-    onMount(() => {
-      const observer = new ResizeObserver((entries) => {
-        const entry = entries[0];
-        setAvailableWidth(entry.contentRect.width);
-      });
-      if (timingsBox() !== undefined) {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        observer.observe(timingsBox()!);
-      }
+  const [fullContainerWidth, setFullContainerWidth] = createSignal(0);
 
-      return () => observer.disconnect();
+  onMount(() => {
+    const observer = new ResizeObserver((entries) => {
+      console.log("IT CHANGED");
+      const entry = entries[0];
+      setAvailableWidth(entry.contentRect.width);
     });
+    if (timingsBox() !== undefined) {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      observer.observe(timingsBox()!);
+    }
+
+    const fullContainerObserver = new ResizeObserver((entries) => {
+      const entry = entries[0];
+      setFullContainerWidth(entry.contentRect.width);
+    });
+    if (fullContainer() !== undefined) {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      fullContainerObserver.observe(fullContainer()!);
+    }
+
+    return () => {
+      observer.disconnect();
+      fullContainerObserver.disconnect();
+    };
   });
 
   const labels = createMemo(() => {
@@ -32,8 +46,7 @@ export const ServerTimings = (props: ServerTimingsProps) => {
   });
 
   const banners = createMemo(() => {
-    console.log(timingsBox());
-    console.log(availableWidth);
+    console.log("RECOMPUTING WITH AVAILABLE WIDTH", availableWidth());
     if (availableWidth() === 0) return [];
 
     const bannerComponents = [];
@@ -47,6 +60,8 @@ export const ServerTimings = (props: ServerTimingsProps) => {
           class={`grid w-full place-items-center bg-magenta-700`}
           style={{
             width: `${width}px`,
+            "min-width": `${width}px`,
+            "max-width": `${width}px`,
             height: "24px",
             "margin-left": `${usedWidth}px`,
           }}
@@ -63,19 +78,29 @@ export const ServerTimings = (props: ServerTimingsProps) => {
   });
 
   return (
-    <div class="py-2">
+    <div ref={setFullContainer} class="py-2">
       <div class="text-md pl-2 font-semibold">Total Time: {totalTime()}ms</div>
       <div class="flex">
         <div class="shrink px-2 pr-4">
           <For each={labels()}>
             {(name) => (
               <>
-                <div class="absolute w-[calc(100%-50px)] border border-neutral-800/80" />
+                <div
+                  style={{
+                    "min-width": `${fullContainerWidth() - 18}px`,
+                  }}
+                  class="absolute border border-neutral-800/80"
+                />
                 <div class="pl-2">{name}</div>
               </>
             )}
           </For>
-          <div class="absolute w-[calc(100%-50px)] border border-neutral-800/80" />
+          <div
+            style={{
+              "min-width": `${fullContainerWidth() - 18}px`,
+            }}
+            class="absolute border border-neutral-800/80"
+          />
         </div>
         <div class="mr-8 grow">
           <div
