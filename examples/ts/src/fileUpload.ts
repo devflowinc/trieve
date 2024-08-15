f19ef10dfd68e97a5208156cd84c228c00b1a49c
Drew Harris <harrisd@smu.edu>
2024-08-15 17:05:25 -0500

    fix: use pdf for file upload

--- examples/ts/src/fileUpload.ts
+++ examples/ts/src/fileUpload.ts
@@ -2,16 +2,19 @@ import fs from "fs";
 import { EXAMPLE_DATASET_ID, trieve } from "./trieve";
 
 const main = async () => {
-  const file = fs.readFileSync("./src/uploadme.txt");
+  const file = fs.readFileSync("./src/uploadme.pdf");
 
-  const fileString = file.toString("base64");
-
-  console.log(fileString);
+  console.log(file.toString("base64"));
+  const fileEncoded = file
+    .toString("base64")
+    .replace(/\+/g, "-") // Convert '+' to '-'
+    .replace(/\//g, "_") // Convert '/' to '_'
+    .replace(/=+$/, ""); // Remove ending '='
 
   const response = await trieve.fetch("/api/file", "post", {
     data: {
-      base64_file: fileString,
-      file_name: "uploadmeagain.txt",
+      base64_file: fileEncoded,
+      file_name: "uploadme.pdf",
       group_tracking_id: "file-upload-group",
     },
     datasetId: EXAMPLE_DATASET_ID,
