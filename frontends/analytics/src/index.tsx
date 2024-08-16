f11a15f02fa34b7f45609de1c7b756ab9cb6c728
cdxker <fdenzell@gmail.com>
2024-08-16 15:08:00 -0700

    feature: add analytics to sentry

--- frontends/analytics/src/index.tsx
+++ frontends/analytics/src/index.tsx
@@ -6,6 +6,8 @@ import "./index.css";
 import { RouteDefinition, Router } from "@solidjs/router";
 import { SearchAnalyticsPage } from "./pages/SearchAnalyticsPage";
 import { UserAuthContextProvider } from "./contexts/UserAuthContext";
+import * as Sentry from "@sentry/browser";
+import { DEV } from "solid-js";
 import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
 import { TrendExplorer } from "./pages/TrendExplorer";
 import { Chart, registerables } from "chart.js";
@@ -18,6 +20,23 @@ import { RAGTablePage } from "./pages/tablePages/RAGTablePage";
 import { SingleQueryPage } from "./pages/SingleQueryPage";
 
 const queryClient = new QueryClient();
+
+if (!DEV) {
+  Sentry.init({
+    dsn: `${import.meta.env.VITE_SENTRY_ANALYTICS_DSN as string}`,
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration(),
+    ],
+
+    tracesSampleRate: 1.0,
+
+    tracePropagationTargets: ["localhost", /^https:\/\/trieve\.ai\/api/],
+
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
+}
 
 Chart.register(...registerables);
 
