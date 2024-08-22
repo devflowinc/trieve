d41891933a90c3b3f457c228acbcc5e96d2aa50b
skeptrune <nick.k@trieve.ai>
2024-08-22 15:35:37 -0700

    cleanup: remove examples post ts sdk refactor

--- examples/ts/src/trieve.ts
+++
@@ -1,9 +0,0 @@
-import { Trieve } from "trieve-fetch-client";
-
-export const trieve = new Trieve({
-  apiKey: "admin",
-  baseUrl: "http://localhost:8090",
-  debug: false,
-});
-
-export const EXAMPLE_DATASET_ID = "6e15c9ff-037b-4559-ad25-bbb17aaf51d2";
--- frontends/analytics/src/api/trieve.ts
+++
@@ -1,5 +0,0 @@
-import { TrieveFetchClient } from "trieve-ts-sdk";
-
-export const trieve = new TrieveFetchClient({
-  baseUrl: import.meta.env.VITE_API_HOST as string,
-});
