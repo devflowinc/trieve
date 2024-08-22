7ec5af9e5490c5ceb1ea28da26d0c2ea10efd351
skeptrune <nick.k@trieve.ai>
2024-08-22 12:42:21 -0700

    lint: fix eslint/unbound-method issues in JSX callbacks

--- frontends/search/src/components/SearchForm.tsx
+++ frontends/search/src/components/SearchForm.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/unbound-method */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import { BiRegularSearch, BiRegularX } from "solid-icons/bi";
