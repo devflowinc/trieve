f152509038dd5a31372112a262ff66160bbef012
SaraVieira <hey@iamsaravieira.com>
2024-08-23 18:11:07 +0100

    add docs for analytics

--- clients/ts-sdk/src/functions/analytics/index.ts
+++ clients/ts-sdk/src/functions/analytics/index.ts
@@ -15,12 +15,23 @@ import {
 import { TrieveSDK } from "../../sdk";
 
 /**
- * Function that allows you to view the CTR analytics for a dataset:
+ * Function that allows you to view the CTR analytics for a dataset.
+ * 
  * Example:
  * ```js
  *const data = await trieve.getCTRAnalytics({
- *   type: "search_ctr_metrics",
- *});
+  filter: {
+    date_range: {
+      gt: "2021-01-01 00:00:00.000",
+      gte: "2021-01-01 00:00:00.000",
+      lt: "2021-01-01 00:00:00.000",
+      lte: "2021-01-01 00:00:00.000"
+    },
+    search_method: "fulltext",
+    search_type: "search"
+  },
+  type: "search_ctr_metrics"
+});
  * ```
  */
 export async function getCTRAnalytics(
@@ -40,6 +51,19 @@ export async function getCTRAnalytics(
   );
 }
 
+/**
+ * Function that allows you too send CTR data to the system.
+ * 
+ * Example:
+ * ```js
+ *const data = await trieve.sendCTRAnalytics({
+  clicked_chunk_id: "3c90c3cc-0d44-4b50-8888-8dd25736052a",
+  ctr_type: "search",
+  position: 123,
+  request_id: "3c90c3cc-0d44-4b50-8888-8dd25736052a"
+});
+ * ```
+ */
 export async function sendCTRAnalytics(
   /** @hidden */
   this: TrieveSDK,
@@ -57,6 +81,28 @@ export async function sendCTRAnalytics(
   );
 }
 
+/**
+ * Function that allows you to view the RAG analytics for a dataset.
+ * 
+ * Example:
+ * ```js
+ *const data = await trieve.getRagAnalytics({
+  filter: {
+    date_range: {
+      gt: "2021-01-01 00:00:00.000",
+      gte: "2021-01-01 00:00:00.000",
+      lt: "2021-01-01 00:00:00.000",
+      lte: "2021-01-01 00:00:00.000",
+    },
+    rag_type: "chosen_chunks",
+  },
+  page: 1,
+  sort_by: "created_at",
+  sort_order: "desc",
+  type: "rag_queries",
+});
+ * ```
+ */
 export async function getRagAnalytics(
   /** @hidden */
   this: TrieveSDK,
@@ -74,6 +120,27 @@ export async function getRagAnalytics(
   );
 }
 
+/**
+ * Function that allows you to view the recommendation analytics for a dataset.
+ * 
+ * Example:
+ * ```js
+ *const data = await trieve.getRecommendationAnalytics({
+  filter: {
+    date_range: {
+      gt: "2021-01-01 00:00:00.000",
+      gte: "2021-01-01 00:00:00.000",
+      lt: "2021-01-01 00:00:00.000",
+      lte: "2021-01-01 00:00:00.000",
+    },
+    recommendation_type: "Chunk",
+  },
+  page: 1,
+  threshold: 123,
+  type: "low_confidence_recommendations",
+});
+ * ```
+ */
 export async function getRecommendationAnalytics(
   /** @hidden */
   this: TrieveSDK,
@@ -91,6 +158,27 @@ export async function getRecommendationAnalytics(
   );
 }
 
+/**
+ * Function that allows you to view the search analytics for a dataset.
+ * 
+ * Example:
+ * ```js
+ *const data = await trieve.getSearchAnalytics({
+  filter: {
+    date_range: {
+      gt: "2021-01-01 00:00:00.000",
+      gte: "2021-01-01 00:00:00.000",
+      lt: "2021-01-01 00:00:00.000",
+      lte: "2021-01-01 00:00:00.000",
+    },
+    search_method: "fulltext",
+    search_type: "search",
+  },
+  granularity: "minute",
+  type: "latency_graph",
+});
+ * ```
+ */
 export async function getSearchAnalytics(
   /** @hidden */
   this: TrieveSDK,
@@ -108,6 +196,24 @@ export async function getSearchAnalytics(
   );
 }
 
+/**
+ * Function that allows you to view the cluster analytics for a dataset.
+ * 
+ * Example:
+ * ```js
+ *const data = await trieve.getClusterAnalytics({
+  filter: {
+    date_range: {
+      gt: "2021-01-01 00:00:00.000",
+      gte: "2021-01-01 00:00:00.000",
+      lt: "2021-01-01 00:00:00.000",
+      lte: "2021-01-01 00:00:00.000",
+    },
+  },
+  type: "cluster_topics",
+});
+ * ```
+ */
 export async function getClusterAnalytics(
   /** @hidden */
   this: TrieveSDK,
