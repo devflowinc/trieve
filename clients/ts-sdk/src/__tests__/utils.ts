a16935b01b8ae18bd1ba863ede4a184f6e03d492
SaraVieira <hey@iamsaravieira.com>
2024-08-23 13:39:09 +0100

    fix ts

--- clients/ts-sdk/src/__tests__/utils.ts
+++ clients/ts-sdk/src/__tests__/utils.ts
@@ -1,4 +1,4 @@
 import { test as baseTest } from "vitest";
 
 export const test = (name: string, fn: () => void) =>
-  baseTest(name, { retry: 3, timeout: 30_000 }, async () => fn());
+  baseTest(name, { retry: 3, timeout: 30_000 }, () => fn());
