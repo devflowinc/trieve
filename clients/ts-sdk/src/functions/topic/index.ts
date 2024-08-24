e6997d47d1a8ed294928af167f822c34c78e6b8e
SaraVieira <hey@iamsaravieira.com>
2024-08-24 11:12:37 +0100

    add docs to topics

--- clients/ts-sdk/src/functions/topic/index.ts
+++ clients/ts-sdk/src/functions/topic/index.ts
@@ -5,11 +5,24 @@
  */
 import {
   CreateTopicReqPayload,
+  DeleteTopicData2,
   GetAllTopicsForOwnerIdData,
   UpdateTopicReqPayload,
 } from "../../fetch-client";
 import { TrieveSDK } from "../../sdk";
 
+/**
+ * Create a new chat topic. Topics are attached to a owner_id’s and act as a coordinator for conversation message history of gen-AI chat sessions. Auth’ed user or api key must have an admin or owner role for the specified dataset’s organization.
+ * 
+ * Example:
+ * ```js
+ *const data = await trieve.createTopic({
+  first_user_message: "hello",
+  name: "Test",
+  owner_id: "3c90c3cc-1d76-27198-8888-8dd25736052a",
+});
+ * ```
+ */
 export async function createTopic(
   /** @hidden */
   this: TrieveSDK,
@@ -27,6 +40,17 @@ export async function createTopic(
   );
 }
 
+/**
+ * Update an existing chat topic. Currently, only the name of the topic can be updated. Auth’ed user or api key must have an admin or owner role for the specified dataset’s organization.
+ * 
+ * Example:
+ * ```js
+ *const data = await trieve.updateTopic({
+  topic_id: "3c90c3cc-1d76-27198-8888-8dd25736052a",
+  name: "NewName"
+});
+ * ```
+ */
 export async function updateTopic(
   /** @hidden */
   this: TrieveSDK,
@@ -44,6 +68,16 @@ export async function updateTopic(
   );
 }
 
+/**
+ * Get all topics belonging to an arbitary owner_id. This is useful for managing message history and chat sessions. It is common to use a browser fingerprint or your user’s id as the owner_id. Auth’ed user or api key must have an admin or owner role for the specified dataset’s organization.
+ * 
+ * Example:
+ * ```js
+ *const data = await trieve.getAllTopics({
+  ownerId: "3c90c3cc-1d76-27198-8888-8dd25736052a"
+});
+ * ```
+ */
 export async function getAllTopics(
   /** @hidden */
   this: TrieveSDK,
@@ -53,6 +87,33 @@ export async function getAllTopics(
   return await this.trieve.fetch(
     "/api/topic/owner/{owner_id}",
     "get",
+    {
+      ...data,
+      datasetId: this.datasetId,
+    },
+    signal
+  );
+}
+
+/**
+ * Delete an existing chat topic. When a topic is deleted, all associated chat messages are also deleted. Auth’ed user or api key must have an admin or owner role for the specified dataset’s organization.
+ * 
+ * Example:
+ * ```js
+ *const data = await trieve.deleteTopic({
+  topicId: "3c90c3cc-1d76-27198-8888-8dd25736052a"
+});
+ * ```
+ */
+export async function deleteTopic(
+  /** @hidden */
+  this: TrieveSDK,
+  data: Omit<DeleteTopicData2, "trDataset">,
+  signal?: AbortSignal
+) {
+  return await this.trieve.fetch(
+    "/api/topic/{topic_id}",
+    "delete",
     {
       ...data,
       datasetId: this.datasetId,
