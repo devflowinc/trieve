4d77e7e10cf76a74a1fb517c80c18c2a6f9c3b22
SaraVieira <hey@iamsaravieira.com>
2024-08-29 17:41:47 +0100

    cleanup: improve highlights display of search example

--- clients/search-component/example/src/App.tsx
+++ clients/search-component/example/src/App.tsx
@@ -1,40 +1,65 @@
 import { TrieveSearch, TrieveSDK, TrieveModalSearch } from "../../src/index";
 import "../../dist/app.css";
+import { useState } from "react";
+import { IconMoon, IconNext, IconPrevious, IconSun } from "./Icons";
 
 const trieve = new TrieveSDK({
   apiKey: "tr-l1IRx4Jw0iVICiFdf9NroFwmDWQ4CnEd",
   datasetId: "85bdeb65-44ec-4c9c-9d64-725601ad672a",
 });
 export default function App() {
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+  const [component, setComponent] = useState(0);
   return (
     <>
-      <div className="container mx-auto my-12 ">
-        <h2 className="font-bold text-center py-8">Search Modal Component </h2>
-        <div className="grid gap-8 grid-cols-2">
-          <div className="mb-8">
-            <h2>Search Dark Mode</h2>
-            <TrieveModalSearch trieve={trieve} theme="dark" />
-          </div>
-          <div>
-            <h2>Search Light Mode</h2>
-            <TrieveModalSearch trieve={trieve} />
-          </div>
+      <div
+        className={`p-12 flex flex-col items-center justify-center w-screen h-screen relative ${
+          theme === "dark" ? "bg-zinc-900 text-zinc-50" : ""
+        }`}
+      >
+        <div className="absolute top-6 right-6">
+          <ul>
+            <li>
+              <button
+                onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+              >
+                {theme === "light" ? <IconMoon /> : <IconSun />}
+              </button>
+            </li>
+          </ul>
         </div>
-      </div>
-      <div className="container mx-auto my-12 ">
-        <h2 className="font-bold text-center py-8">
-          Search Results Component{" "}
-        </h2>
-        <div className="grid gap-8 grid-cols-2">
-          <div className="mb-8">
-            <h2>Search Dark Mode</h2>
-            <TrieveSearch trieve={trieve} theme="dark" />
-          </div>
-          <div>
-            <h2>Search Light Mode</h2>
-            <TrieveSearch trieve={trieve} />
-          </div>
-        </div>
+        {component === 0 ? (
+          <>
+            <h2 className="font-bold text-center py-8">
+              Search Modal Component{" "}
+            </h2>
+
+            <TrieveModalSearch trieve={trieve} theme={theme} />
+          </>
+        ) : (
+          <>
+            <h2 className="font-bold text-center py-8">
+              Search Results Component
+            </h2>
+            <TrieveSearch trieve={trieve} theme={theme} />
+          </>
+        )}
+
+        <ul className="absolute top-1/2 -translate-y-1/2 w-full">
+          {component > 0 ? (
+            <li className="left-6 absolute">
+              <button onClick={() => setComponent(0)}>
+                <IconPrevious />
+              </button>
+            </li>
+          ) : (
+            <li className="right-6 absolute">
+              <button onClick={() => setComponent(1)}>
+                <IconNext />
+              </button>
+            </li>
+          )}
+        </ul>
       </div>
     </>
   );
