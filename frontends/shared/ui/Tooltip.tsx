d81405172c5e79976f879a84842b35a077e46f89
Drew Harris <harrisd@smu.edu>
2024-08-30 14:10:49 -0500

    styling: redesign sidebar

--- frontends/shared/ui/Tooltip.tsx
+++ frontends/shared/ui/Tooltip.tsx
@@ -4,9 +4,12 @@ import type { JSX } from "solid-js/jsx-runtime";
 export type TooltipDirection = "top" | "bottom" | "left" | "right";
 
 export interface TooltipProps {
-  body: JSX.Element;
+  body?: JSX.Element;
+  children?: JSX.Element;
   tooltipText: string;
   direction?: TooltipDirection;
+  tooltipClass?: string;
+  unsetWidth?: boolean;
 }
 
 export const Tooltip = (props: TooltipProps) => {
@@ -24,13 +27,15 @@ export const Tooltip = (props: TooltipProps) => {
           setShow(false);
         }}
       >
-        {props.body}
+        {props.children ? props.children : props.body}
       </div>
       <Show when={show()}>
         <div
+          class={props.tooltipClass}
           classList={{
-            "absolute z-10 inline-block w-[300px] rounded bg-[#fffee6] p-2 text-center shadow-lg dark:bg-[#fffee6] text-black text-wrap":
+            "absolute z-10 inline-block rounded bg-[#fffee6] p-2 text-center shadow-lg dark:bg-[#fffee6] text-black text-wrap":
               true,
+            "w-[300px]": !props.unsetWidth,
             "bottom-full left-1/2 -translate-x-1/2 translate-y-3":
               direction() === "top",
             "right-full top-1/2 -translate-y-1/2 -translate-x-3":
@@ -38,7 +43,7 @@ export const Tooltip = (props: TooltipProps) => {
             "left-full top-1/2 -translate-y-1/2 translate-x-3":
               direction() === "right",
             "top-full left-1/2 -translate-x-1/2 translate-y-3":
-              direction() === "bottom"
+              direction() === "bottom",
           }}
         >
           {props.tooltipText}
