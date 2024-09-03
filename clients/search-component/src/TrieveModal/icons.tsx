ed71bcc70689fa0df1654a5080346ba7e6a3aac4
SaraVieira <hey@iamsaravieira.com>
2024-09-03 23:12:09 +0100

    feature: add small icon animation

--- clients/search-component/src/TrieveModal/icons.tsx
+++ clients/search-component/src/TrieveModal/icons.tsx
@@ -145,3 +145,27 @@ export const BackIcon = () => (
     <path d="M15 6l-6 6l6 6" />
   </svg>
 );
+
+export const LoadingAIIcon = () => {
+  return (
+    <svg
+      width="23"
+      height="23"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z"
+        fill="currentColor"
+      >
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          dur="0.75s"
+          values="0 12 12;360 12 12"
+          repeatCount="indefinite"
+        />
+      </path>
+    </svg>
+  );
+};
