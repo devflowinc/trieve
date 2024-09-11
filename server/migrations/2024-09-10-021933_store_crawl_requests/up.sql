0b472c256df663a5483f92ff3c3fc6c5102258ab
Dens Sumesh <dens.sumesh79@gmail.com>
2024-09-10 18:19:29 -0700

    feature: set up scrape worker with endpoint to crawl

---
+++ server/migrations/2024-09-10-021933_store_crawl_requests/up.sql
@@ -0,0 +1,9 @@
+-- Your SQL goes here
+CREATE TABLE IF NOT EXISTS crawl_requests (
+    id UUID PRIMARY KEY,
+    url TEXT NOT NULL,
+    status TEXT NOT NULL,
+    scrape_id UUID NOT NULL,
+    dataset_id UUID NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
