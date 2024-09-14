419ff8efb5b1024fd7c680a549dc58246c3fd268
Drew Harris <harrisd@smu.edu>
2024-09-14 16:03:53 -0500

    feat: better sidebar

--- frontends/dashboard/src/components/Sidebar.tsx
+++ frontends/dashboard/src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { createMemo, For, useContext } from "solid-js";
+import { createMemo, Show, useContext } from "solid-js";
 import { JSX } from "solid-js";
 import { DatasetContext } from "../contexts/DatasetContext";
 import { A, useLocation } from "@solidjs/router";
@@ -6,32 +6,53 @@ import { AiOutlineLeft } from "solid-icons/ai";
 import { Spacer } from "./Spacer";
 import { Portal } from "solid-js/web";
 import { NavbarDatasetSelector } from "../layouts/NavbarDatasetSelector";
+import { FiExternalLink } from "solid-icons/fi";
+import { UserContext } from "../contexts/UserContext";
+import { IconTypes } from "solid-icons";
 
-type Link = {
-  label: string;
-  href: string;
-  isExternal: boolean;
-  icon?: JSX.Element;
-};
+const apiHost = import.meta.env.VITE_API_HOST as string;
+const analyticsUiURL = import.meta.env.VITE_ANALYTICS_UI_URL as string;
+const searchUiURL = import.meta.env.VITE_SEARCH_UI_URL as string;
+const chatUiURL = import.meta.env.VITE_CHAT_UI_URL as string;
 
 export const DashboardSidebar = () => {
   const { datasetId } = useContext(DatasetContext);
+  const userContext = useContext(UserContext);
   const pathname = useLocation();
 
-  const datasetLinks = createMemo<Link[]>(() => [
-    {
-      href: `/dataset/${datasetId}`,
-      label: "Overview",
-      isExternal: false,
-      // icon: <AiOutlineLeft size={12} />,
-    },
-    {
-      href: `/dataset/${datasetId}/keys`,
-      isExternal: false,
-      label: "Keys",
-      // icon: <AiOutlineLeft size={12} />,
-    },
-  ]);
+  const orgDatasetParams = createMemo(() => {
+    const orgId = userContext.selectedOrganization().id;
+    let params = "";
+    if (orgId) params += `?organization=${orgId}`;
+    if (orgId && datasetId) params += `&dataset=${datasetId}`;
+    return params;
+  });
+
+  const SectionLabel = (props: { children: string }) => (
+    <div class="border-b border-b-neutral-200 text-sm text-neutral-500">
+      {props.children}
+    </div>
+  );
+
+  const Link = (props: {
+    href: string;
+    label: JSX.Element;
+    isExternal: boolean;
+    icon?: IconTypes;
+  }) => (
+    <A
+      href={props.href}
+      class="flex items-center justify-between gap-2 rounded-md p-1 px-2 hover:underline"
+      classList={{
+        "bg-magenta-200/30": pathname.pathname === props.href,
+      }}
+    >
+      {props.label}
+      <Show when={props.isExternal}>
+        <FiExternalLink class="text-neutral-500" />
+      </Show>
+    </A>
+  );
 
   return (
     <>
@@ -50,21 +71,53 @@ export const DashboardSidebar = () => {
         </A>
         <Spacer h={9} withBorder />
         <div class="pt-4">
-          <div class="flex flex-col gap-2">
-            <For each={datasetLinks()}>
-              {(link) => (
-                <A
-                  href={link.href}
-                  class="flex items-center gap-2 rounded-md p-2 hover:underline"
-                  classList={{
-                    "bg-magenta-200/30": pathname.pathname === link.href,
-                  }}
-                >
-                  {link.icon}
-                  <div>{link.label}</div>
-                </A>
-              )}
-            </For>
+          <div class="gap flex flex-col">
+            <Link
+              href={`/dataset/${datasetId}`}
+              label="Overview"
+              isExternal={false}
+            />
+            <Link
+              href={`/dataset/${datasetId}/events`}
+              label={"Event Log"}
+              isExternal={false}
+            />
+          </div>
+          <div class="gap flex flex-col pt-4">
+            <SectionLabel>Dataset Settings</SectionLabel>
+            <Link
+              isExternal={false}
+              href={`/dataset/${datasetId}/keys`}
+              label="API Keys"
+            />
+            <Link
+              isExternal={false}
+              href={`/dataset/${datasetId}/llm-settings`}
+              label="LLM Settings"
+            />
+            <Link
+              isExternal={false}
+              href={`/dataset/${datasetId}/options`}
+              label="Dataset Options"
+            />
+            <Link
+              isExternal={false}
+              href={`/dataset/${datasetId}/manage`}
+              label="Manage Dataset"
+            />
+          </div>
+          <div class="gap flex flex-col pt-12">
+            <SectionLabel>Playgrounds</SectionLabel>
+            <Link
+              isExternal={true}
+              href={`${searchUiURL}${orgDatasetParams()}`}
+              label="Search"
+            />
+            <Link
+              isExternal={true}
+              href={`${chatUiURL}${orgDatasetParams()}`}
+              label="Chat"
+            />
           </div>
         </div>
       </div>
