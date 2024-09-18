ab08a91f9e4b8ef203136f6d6b15e3c06e666654
Drew Harris <harrisd@smu.edu>
2024-09-17 21:28:16 -0500

    styling: add padding to bottom of org homepage

--- frontends/dashboard/src/pages/orgs/OrganizationHomepage.tsx
+++ frontends/dashboard/src/pages/orgs/OrganizationHomepage.tsx
@@ -2,7 +2,7 @@ import { DatasetOverview } from "../../components/DatasetOverview";
 
 export const OrganizationHomepage = () => {
   return (
-    <div class="">
+    <div class="pb-8">
       <div class="h-1" />
       <DatasetOverview />
     </div>
