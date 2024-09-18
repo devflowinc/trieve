e8958fd0f5820042232faae53b596e74bc3471d5
Drew Harris <harrisd@smu.edu>
2024-09-17 19:57:03 -0500

    feat: better api keys table

--- frontends/dashboard/src/components/ApiKeys.tsx
+++ frontends/dashboard/src/components/ApiKeys.tsx
@@ -1,11 +1,4 @@
-import {
-  For,
-  Show,
-  createMemo,
-  createResource,
-  createSignal,
-  useContext,
-} from "solid-js";
+import { Show, createMemo, createSignal, useContext } from "solid-js";
 import { FaRegularTrashCan } from "solid-icons/fa";
 import { ApiKeyGenerateModal } from "./ApiKeyGenerateModal";
 import { UserContext } from "../contexts/UserContext";
@@ -15,10 +8,18 @@ import {
   fromI32ToUserRole,
 } from "shared/types";
 import { formatDate } from "../utils/formatters";
+import { createMutation, createQuery } from "@tanstack/solid-query";
+import { useTrieve } from "../hooks/useTrieve";
+import {
+  createColumnHelper,
+  createSolidTable,
+  getCoreRowModel,
+} from "@tanstack/solid-table";
+import { TanStackTable } from "shared/ui";
+
+const colHelp = createColumnHelper<ApiKeyRespBody>();
 
 export const ApiKeys = () => {
-  const api_host = import.meta.env.VITE_API_HOST as unknown as string;
-
   const userContext = useContext(UserContext);
 
   const [openModal, setOpenModal] = createSignal<boolean>(false);
@@ -34,40 +35,93 @@ export const ApiKeys = () => {
     );
   });
 
-  const [apiKeys, { refetch, mutate }] = createResource(
-    () => {
-      return fetch(`${api_host}/user/api_key`, {
-        method: "GET",
-        credentials: "include",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          return data as ApiKeyRespBody[];
-        });
+  const trieve = useTrieve();
+
+  const apiKeysQuery = createQuery(() => ({
+    queryKey: ["apiKeys", userContext.selectedOrg().id],
+    queryFn: () => {
+      return trieve.fetch<"eject">(`/api/user/api_key`, "get", {}) as Promise<
+        ApiKeyRespBody[]
+      >;
     },
-    { initialValue: [] },
-  );
+  }));
 
-  const deleteApiKey = (id: string) => {
-    mutate((prev) => {
-      return prev.filter((apiKey) => apiKey.id !== id);
-    });
+  const deleteApiKeyMutation = createMutation(() => ({
+    mutationFn: async (id: string) => {
+      return await trieve.fetch("/api/user/api_key/{api_key_id}", "delete", {
+        apiKeyId: id,
+      });
+    },
+    onSuccess() {
+      void apiKeysQuery.refetch();
+    },
+  }));
 
-    void fetch(`${api_host}/user/api_key/${id}`, {
-      method: "DELETE",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).then((resp) => {
-      if (resp.ok) {
-        void refetch();
-      }
+  const table = createMemo(() => {
+    if (!apiKeysQuery.data) {
+      return null;
+    }
+    const columns = [
+      colHelp.accessor("id", {
+        header: "ID",
+      }),
+      colHelp.accessor("role", {
+        header: "Perms",
+        cell: (info) => {
+          if (currentUserRole() > 0) {
+            return fromI32ToUserRole(info.getValue());
+          }
+          return fromI32ToApiKeyRole(info.getValue());
+        },
+      }),
+      colHelp.accessor("dataset_ids", {
+        header: "Datasets",
+        cell: (info) => {
+          return info.getValue()?.join(",");
+        },
+      }),
+      colHelp.accessor("organization_ids", {
+        header: "Organizations",
+        cell: (info) => {
+          return info.getValue()?.join(",");
+        },
+      }),
+      colHelp.accessor("created_at", {
+        header: "Created At",
+        cell: (info) => {
+          return formatDate(new Date(info.getValue()));
+        },
+      }),
+      colHelp.display({
+        header: " ",
+        cell: (info) => {
+          return (
+            <button
+              type="button"
+              class="inline-flex justify-center px-3 py-2 text-sm text-red-500"
+              onClick={(e) => {
+                e.preventDefault();
+                const result = window.confirm(
+                  "Are you sure you want to delete this API key?",
+                );
+                if (!result) {
+                  return;
+                }
+                deleteApiKeyMutation.mutate(info.row.original.id);
+              }}
+            >
+              <FaRegularTrashCan class="h-4 w-4" />
+            </button>
+          );
+        },
+      }),
+    ];
+    return createSolidTable({
+      columns: columns,
+      data: apiKeysQuery.data,
+      getCoreRowModel: getCoreRowModel(),
     });
-  };
+  });
 
   return (
     <>
@@ -87,130 +141,23 @@ export const ApiKeys = () => {
             Create New Key +
           </button>
         </div>
-        <Show when={apiKeys().length === 0}>
+        <Show when={apiKeysQuery.data?.length === 0}>
           <div class="rounded-md border-[0.5px] border-neutral-300 bg-white py-4 text-center text-sm text-gray-500 shadow-sm">
             No API Keys
           </div>
         </Show>
-        <Show when={apiKeys().length > 0}>
+        <Show when={(apiKeysQuery.data?.length || -1) > 0}>
           <div class="inline-block min-w-full overflow-hidden rounded-md border-[0.5px] border-neutral-300 bg-white align-middle shadow-sm">
-            <table class="min-w-full divide-y divide-gray-300">
-              <thead class="w-full min-w-full bg-neutral-100">
-                <tr>
-                  <th
-                    scope="col"
-                    class="py-3.5 pl-6 pr-3 text-left text-sm font-semibold"
-                  >
-                    Name
-                  </th>
-                  <th
-                    scope="col"
-                    class="py-3.5 pl-6 pr-3 text-left text-sm font-semibold"
-                  >
-                    Perms
-                  </th>
-                  <th
-                    scope="col"
-                    class="px-3 py-3.5 text-left text-sm font-semibold"
-                  >
-                    Datasets
-                  </th>
-                  <th
-                    scope="col"
-                    class="px-3 py-3.5 text-left text-sm font-semibold"
-                  >
-                    Organizations
-                  </th>
-                  <th
-                    scope="col"
-                    class="px-3 py-3.5 text-left text-sm font-semibold"
-                  >
-                    Created At
-                  </th>
-                  <th />
-                </tr>
-              </thead>
-              <tbody class="divide-y divide-neutral-200 bg-white">
-                <For each={apiKeys()}>
-                  {(apiKey) => (
-                    <tr>
-                      <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm text-gray-900 sm:pl-6 lg:pl-8">
-                        {apiKey.name}
-                      </td>
-                      <td class="px-3 py-3.5 text-left text-sm text-gray-900">
-                        {apiKey.role > 0
-                          ? fromI32ToUserRole(currentUserRole())
-                          : fromI32ToApiKeyRole(apiKey.role).toString()}
-                      </td>
-                      <td class="px-3 py-3.5 text-left text-sm text-gray-900">
-                        <Show when={apiKey.dataset_ids?.length}>[</Show>
-                        <For each={apiKey.dataset_ids}>
-                          {(dataset_id, index) => (
-                            <>
-                              <a
-                                class="text-fuchsia-600 hover:underline"
-                                href={`/dashboard/dataset/${dataset_id}/start`}
-                              >
-                                {dataset_id}
-                              </a>
-                              <Show
-                                when={
-                                  index() <
-                                  (apiKey.dataset_ids?.length ?? 0) - 1
-                                }
-                              >
-                                {", "}
-                              </Show>
-                            </>
-                          )}
-                        </For>
-                        <Show when={apiKey.dataset_ids?.length}>]</Show>
-                      </td>
-                      <td class="px-3 py-3.5 text-left text-sm text-gray-900">
-                        <Show when={apiKey.organization_ids?.length}>[</Show>
-                        <For each={apiKey.organization_ids}>
-                          {(org_id, index) => (
-                            <>
-                              <div>{org_id}</div>
-                              <Show
-                                when={
-                                  index() <
-                                  (apiKey.organization_ids?.length ?? 0) - 1
-                                }
-                              >
-                                {", "}
-                              </Show>
-                            </>
-                          )}
-                        </For>
-                        <Show when={apiKey.organization_ids?.length}>]</Show>
-                      </td>
-                      <td class="px-3 py-3.5 text-left text-sm text-gray-900">
-                        {formatDate(new Date(apiKey.created_at))}
-                      </td>
-                      <td class="px-3 py-3.5 text-center text-sm text-gray-900">
-                        <button
-                          class="text-red-500 hover:text-neutral-900"
-                          onClick={(e) => {
-                            e.preventDefault();
-                            confirm(
-                              "Are you sure you want to delete this key?",
-                            ) && deleteApiKey(apiKey.id);
-                          }}
-                        >
-                          <FaRegularTrashCan />
-                        </button>
-                      </td>
-                    </tr>
-                  )}
-                </For>
-              </tbody>
-            </table>
+            <Show when={table()}>
+              {(table) => <TanStackTable table={table()} />}
+            </Show>
           </div>
         </Show>
       </div>
       <ApiKeyGenerateModal
-        refetch={refetch}
+        onCreated={() => {
+          void apiKeysQuery.refetch();
+        }}
         openModal={openModal}
         closeModal={() => setOpenModal(false)}
       />
