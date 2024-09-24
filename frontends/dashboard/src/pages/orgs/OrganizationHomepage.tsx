d5985789e911112bd52694a49214e861fc3aa674
Drew Harris <harrisd@smu.edu>
2024-09-24 10:41:13 -0500

    feat: getting started links

--- frontends/dashboard/src/pages/orgs/OrganizationHomepage.tsx
+++ frontends/dashboard/src/pages/orgs/OrganizationHomepage.tsx
@@ -1,10 +1,13 @@
 import { DatasetOverview } from "../../components/DatasetOverview";
+import { GettingStartedDocsLinks } from "../../components/GettingStartedDocsLinks";
 
 export const OrganizationHomepage = () => {
   return (
     <div class="pb-8">
       <div class="h-1" />
       <DatasetOverview />
+      <div class="h-6" />
+      <GettingStartedDocsLinks />
     </div>
   );
 };
