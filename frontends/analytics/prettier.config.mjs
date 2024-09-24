d515340b9dc13775370c56f662f2b8b7e1aea962
Drew Harris <harrisd@smu.edu>
2024-09-24 11:07:53 -0500

    cleanup: remove analytics stuff

--- frontends/analytics/prettier.config.mjs
+++
@@ -1,1 +0,0 @@
-export default "../config/prettier-config.mjs";
--- frontends/analytics/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import solid from "vite-plugin-solid";
-import runtimeEnv from "vite-plugin-runtime-env";
-
-export default defineConfig({
-  plugins: [solid(), runtimeEnv()],
-});
