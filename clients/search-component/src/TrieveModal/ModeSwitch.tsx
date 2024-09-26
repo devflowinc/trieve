ea4a7bff4f93172adcfda4c9b8d16b34a6d81503
Sara Vieira <hey@iamsaravieira.com>
2024-09-26 16:53:22 +0100

    feat: create new toggle & delete custom item

---
+++ clients/search-component/src/TrieveModal/ModeSwitch.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import { useModalState } from "../utils/hooks/modal-context";
+
+export const ModeSwitch = () => {
+  const { mode, setMode } = useModalState();
+  return (
+    <div className="mode-switch-wrapper">
+      <div>
+        <button
+          className={mode === "search" ? "active" : ""}
+          onClick={() => setMode("search")}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+            <path d="M21 21l-6 -6" />
+          </svg>
+          Search
+        </button>
+        <button
+          className={mode === "chat" ? "active" : ""}
+          onClick={() => setMode("chat")}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M16 18a2 2 0 0 1 2 2a2 2 0 0 1 2 -2a2 2 0 0 1 -2 -2a2 2 0 0 1 -2 2zm0 -12a2 2 0 0 1 2 2a2 2 0 0 1 2 -2a2 2 0 0 1 -2 -2a2 2 0 0 1 -2 2zm-7 12a6 6 0 0 1 6 -6a6 6 0 0 1 -6 -6a6 6 0 0 1 -6 6a6 6 0 0 1 6 6z" />
+          </svg>
+          Ask AI
+        </button>
+      </div>
+    </div>
+  );
+};
