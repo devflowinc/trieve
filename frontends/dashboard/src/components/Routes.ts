eee2e05bbfa6a7ff4b0789c992a7926066286658
skeptrune <nick.k@trieve.ai>
2024-09-25 21:12:13 -0700

    feature: more convenient api route scoping

--- frontends/dashboard/src/components/Routes.ts
+++ frontends/dashboard/src/components/Routes.ts
@@ -1,19 +1,31 @@
 export type RouteScope = keyof typeof ApiRoutes;
 
 export const ApiRoutes = {
-  "api/analytics": [
+  "Search Routes": [
+    "POST /api/chunk/search",
+    "POST /api/chunk_group/search",
+    "POST /api/chunk/autocomplete",
+  ],
+  "Query Suggestions": ["POST /api/chunk/suggestions"],
+  "Count Chunks Above Threshold": ["POST /api/chunk/count"],
+  "Send Event Data": [
+    "PUT /api/analytics/ctr",
+    "PUT /api/analytics/search",
+    "PUT /api/analytics/events",
+  ],
+  "api/analytics/*": [
     "POST /api/analytics/rag",
     "POST /api/analytics/recommendations",
     "POST /api/analytics/search",
     "POST /api/analytics/search/cluster",
   ],
-  "api/auth": [
+  "api/auth/*": [
     "GET /api/auth",
     "DELETE /api/auth",
     "GET /api/auth/callback",
     "GET /api/auth/me",
   ],
-  "api/chunk": [
+  "api/chunk/*": [
     "POST /api/chunk",
     "PUT /api/chunk",
     "POST /api/chunk/autocomplete",
@@ -28,7 +40,7 @@ export const ApiRoutes = {
     "GET /api/chunk/{chunk_id}",
     "DELETE /api/chunk/{chunk_id}",
   ],
-  "api/chunk_group": [
+  "api/chunk_group/*": [
     "POST /api/chunk_group",
     "PUT /api/chunk_group",
     "POST /api/chunk_group/chunk/{group_id}",
@@ -46,8 +58,8 @@ export const ApiRoutes = {
     "DELETE /api/chunk_group/{group_id}",
     "GET /api/chunk_group/{group_id}/{page}",
   ],
-  "api/chunks": ["POST /api/chunks", "POST /api/chunks/tracking"],
-  "api/dataset": [
+  "api/chunks/*": ["POST /api/chunks", "POST /api/chunks/tracking"],
+  "api/dataset/*": [
     "POST /api/dataset",
     "PUT /api/dataset",
     "PUT /api/dataset/clear/{dataset_id}",
@@ -59,21 +71,22 @@ export const ApiRoutes = {
     "GET /api/dataset/{dataset_id}",
     "DELETE /api/dataset/{dataset_id}",
   ],
-  "api/events": ["POST /api/events"],
-  "api/file": [
+  "api/events/*": ["POST /api/events"],
+  "api/file/*": [
     "POST /api/file",
     "GET /api/file/{file_id}",
     "DELETE /api/file/{file_id}",
   ],
-  "api/health": ["GET /api/health"],
-  "api/invitation": ["POST /api/invitation"],
-  "api/message": [
+  "api/health/*": ["GET /api/health"],
+  "api/invitation/*": ["POST /api/invitation"],
+  "api/message/*": [
     "POST /api/message",
     "PUT /api/message",
     "DELETE /api/message",
+    "PATCH /api/message",
   ],
-  "api/messages": ["GET /api/messages/{messages_topic_id}"],
-  "api/organization": [
+  "api/messages/*": ["GET /api/messages/{messages_topic_id}"],
+  "api/organization/*": [
     "POST /api/organization",
     "PUT /api/organization",
     "POST /api/organization/update_dataset_configs",
@@ -82,7 +95,7 @@ export const ApiRoutes = {
     "GET /api/organization/{organization_id}",
     "DELETE /api/organization/{organization_id}",
   ],
-  "api/stripe": [
+  "api/stripe/*": [
     "POST /api/stripe/checkout/setup/{organization_id}",
     "GET /api/stripe/invoices/{organization_id}",
     "GET /api/stripe/payment_link/{plan_id}/{organization_id}",
@@ -90,13 +103,13 @@ export const ApiRoutes = {
     "DELETE /api/stripe/subscription/{subscription_id}",
     "PATCH /api/stripe/subscription_plan/{subscription_id}/{plan_id}",
   ],
-  "api/topic": [
+  "api/topic/*": [
     "POST /api/topic",
     "PUT /api/topic",
     "GET /api/topic/owner/{owner_id}",
     "DELETE /api/topic/{topic_id}",
   ],
-  "api/user": [
+  "api/user/*": [
     "PUT /api/user",
     "POST /api/user/api_key",
     "DELETE /api/user/api_key/{api_key_id}",
