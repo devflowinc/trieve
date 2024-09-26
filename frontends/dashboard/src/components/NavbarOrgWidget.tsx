6d1ea67b98f7783ba9b348c68306e331e2348280
Drew Harris <harrisd@smu.edu>
2024-09-26 11:36:32 -0500

    feat: better styling

--- frontends/dashboard/src/components/NavbarOrgWidget.tsx
+++ frontends/dashboard/src/components/NavbarOrgWidget.tsx
@@ -1,10 +1,12 @@
-import { createSignal, Show, useContext } from "solid-js";
+import { createSignal, For, Show, useContext } from "solid-js";
 import { UserContext } from "../contexts/UserContext";
-import { FiChevronDown, FiLogOut, FiPlus, FiUser } from "solid-icons/fi";
+import { FiLogOut, FiPlus, FiUsers } from "solid-icons/fi";
 import { Popover, PopoverButton, PopoverPanel } from "terracotta";
 import { IconTypes } from "solid-icons";
 import { OcPeople3 } from "solid-icons/oc";
 import NewOrgModal from "./CreateNewOrgModal";
+import { TbSelector } from "solid-icons/tb";
+import { cn } from "shared/utils";
 
 interface PopoutLinkProps {
   label: string;
@@ -34,13 +36,14 @@ export const NavbarOrgWidget = () => {
       <Popover class="relative" defaultOpen={false}>
         {({ isOpen }) => (
           <>
-            <PopoverButton class="flex items-center gap-2 rounded-md border border-neutral-200 bg-neutral-100 p-1 px-2 text-sm">
-              <FiUser class="text-neutral-500" />
-              <div>{userInfo.user().email}</div>
-              <FiChevronDown />
+            <PopoverButton class="flex items-center gap-2 rounded-md border border-neutral-300 p-1 px-2 text-sm">
+              <FiUsers class="text-neutral-500" />
+              <div>{userInfo.selectedOrg().name}</div>
+              <TbSelector />
             </PopoverButton>
             <Show when={isOpen()}>
-              <PopoverPanel class="absolute right-0 top-full z-10 mt-2 w-48 rounded-md border border-neutral-200 bg-white p-1 shadow-md">
+              <PopoverPanel class="absolute right-0 top-full z-10 mt-2 flex rounded-md border border-neutral-200 bg-white p-1 shadow-md">
+                <OrgSelector />
                 <div class="flex flex-col gap-2">
                   <PopoutLink
                     label="Switch Organization"
@@ -68,5 +71,44 @@ export const NavbarOrgWidget = () => {
         isOpen={createOrgModalOpen}
       />
     </>
+  );
+};
+
+const OrgSelector = () => {
+  const orgContext = useContext(UserContext);
+
+  return (
+    <div class="min-w-[200px] border-r border-r-neutral-300">
+      <div class="border-b border-b-neutral-200 pb-1 text-center font-medium">
+        Switch Organizations
+      </div>
+      <div class="w-full">
+        <For each={orgContext.user().orgs}>
+          {(org) => (
+            <button
+              onClick={() => {
+                orgContext.setSelectedOrg(org.id);
+              }}
+              class={cn(
+                "relative flex w-full cursor-pointer items-center gap-2 p-2 px-4 text-left last:border-b-transparent hover:bg-neutral-100",
+                org.id === orgContext.selectedOrg().id &&
+                  "bg-magenta-300 text-white hover:bg-magenta-300",
+              )}
+            >
+              <div class="absolute -left-1 bottom-0 top-0 w-1 bg-magenta-300" />
+              <FiUsers
+                size={12}
+                class={cn(
+                  "text-neutral-700",
+                  org.id === orgContext.selectedOrg().id && "text-white",
+                )}
+              />
+              <div class="text-sm font-medium">{org.name}</div>
+              {/* <div class="text-[9px] text-neutral-500">{org.id}</div> */}
+            </button>
+          )}
+        </For>
+      </div>
+    </div>
   );
 };
