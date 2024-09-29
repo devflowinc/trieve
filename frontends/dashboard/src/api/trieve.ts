ae3f8a65c68524f5018af22f00b81da5b8e572ac
cdxker <fdenzell@gmail.com>
2024-09-29 14:33:05 -0700

    bugfix: only put sdk client in debug mode for dev

--- frontends/dashboard/src/api/trieve.ts
+++ frontends/dashboard/src/api/trieve.ts
@@ -2,10 +2,11 @@ import { createContext } from "solid-js";
 import { TrieveFetchClient } from "trieve-ts-sdk";
 
 const apiHost = import.meta.env.VITE_API_HOST as string;
+const debug = import.meta.env.DEV;
 
 export const trieve = new TrieveFetchClient({
   baseUrl: apiHost.replace(/\/api$/, ""),
-  debug: true,
+  debug: debug,
 });
 
 export const ApiContext = createContext<TrieveFetchClient>(trieve);
