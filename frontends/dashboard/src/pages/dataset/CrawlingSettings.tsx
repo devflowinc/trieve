5575e052d70c92f566189c1fa283a8f9814859d1
Drew Harris <harrisd@smu.edu>
2024-10-01 21:44:21 -0500

    feat: add form controls

--- frontends/dashboard/src/pages/dataset/CrawlingSettings.tsx
+++ frontends/dashboard/src/pages/dataset/CrawlingSettings.tsx
@@ -1,8 +1,11 @@
 import { createQuery } from "@tanstack/solid-query";
-import { Show, useContext } from "solid-js";
+import { createSignal, Show, useContext } from "solid-js";
 import { DatasetContext } from "../../contexts/DatasetContext";
 import { useTrieve } from "../../hooks/useTrieve";
-import { CrawlOptions } from "trieve-ts-sdk";
+import { CrawlInterval, CrawlOptions } from "trieve-ts-sdk";
+import { createStore } from "solid-js/store";
+import { Select } from "shared/ui";
+import { toTitleCase } from "../../analytics/utils/titleCase";
 
 const defaultCrawlOptions: CrawlOptions = {
   boost_titles: false,
@@ -21,7 +24,7 @@ export const CrawlingSettings = () => {
   const trieve = useTrieve();
 
   const crawlSettingsQuery = createQuery(() => ({
-    queryKey: ["crawl-settings", datasetId],
+    queryKey: ["crawl-settings", datasetId()],
     queryFn: async () => {
       const result = await trieve.fetch(
         "/api/dataset/{dataset_id}/crawl_options",
@@ -36,8 +39,8 @@ export const CrawlingSettings = () => {
 
   return (
     <Show when={crawlSettingsQuery.isSuccess}>
-      <div>success</div>
       <RealCrawlingSettings
+        mode={crawlSettingsQuery.data ? "edit" : "create"}
         initialCrawlingSettings={crawlSettingsQuery.data || defaultCrawlOptions}
       />
     </Show>
@@ -46,12 +49,48 @@ export const CrawlingSettings = () => {
 
 interface RealCrawlingSettingsProps {
   initialCrawlingSettings: CrawlOptions;
+  mode: "edit" | "create";
 }
 
 const RealCrawlingSettings = (props: RealCrawlingSettingsProps) => {
+  const [options, setOptions] = createStore(props.initialCrawlingSettings);
+
   return (
-    <div>
-      <div>Crawling Options</div>
+    <div class="rounded border border-neutral-300 bg-white p-4 shadow">
+      <div class="text-lg">Crawl Options</div>
+
+      <div class="flex w-full items-stretch justify-between gap-4">
+        <div class="grow">
+          <label for="url" class="block">
+            Site URL
+          </label>
+          <input
+            name="url"
+            value={options.site_url || ""}
+            onInput={(e) => {
+              setOptions("site_url", e.currentTarget.value);
+            }}
+            class="block w-full rounded-md border border-neutral-300 px-3 py-1.5 shadow-sm placeholder:text-neutral-400 focus:outline-magenta-500 sm:text-sm sm:leading-6"
+          />
+        </div>
+        <div>
+          <Select
+            options={["daily", "weekly", "monthly"] as CrawlInterval[]}
+            display={(option) => toTitleCase(option)}
+            onSelected={(option) => {
+              setOptions("interval", option);
+            }}
+            class="p-1"
+            selected={options.interval || "daily"}
+            label="Crawl Interval"
+          />
+        </div>
+      </div>
+
+      <div>
+        <label for="">Max Pages</label>
+        <input type="number"></input>
+      </div>
     </div>
   );
 };
