ff655a5dc9a2e1869dd851ef2261589e43d38530
Drew Harris <harrisd@smu.edu>
2024-10-01 21:13:40 -0500

    fix: add handlers to lib.rs

--- frontends/dashboard/src/pages/dataset/CrawlingSettings.tsx
+++ frontends/dashboard/src/pages/dataset/CrawlingSettings.tsx
@@ -1,7 +1,20 @@
 import { createQuery } from "@tanstack/solid-query";
-import { useContext } from "solid-js";
+import { Show, useContext } from "solid-js";
 import { DatasetContext } from "../../contexts/DatasetContext";
 import { useTrieve } from "../../hooks/useTrieve";
+import { CrawlOptions } from "trieve-ts-sdk";
+
+const defaultCrawlOptions: CrawlOptions = {
+  boost_titles: false,
+  exclude_paths: [],
+  exclude_tags: [],
+  include_paths: [],
+  include_tags: [],
+  interval: "daily",
+  limit: 1000,
+  max_depth: 10,
+  site_url: "",
+};
 
 export const CrawlingSettings = () => {
   const datasetId = useContext(DatasetContext).datasetId;
@@ -22,8 +35,23 @@ export const CrawlingSettings = () => {
   }));
 
   return (
+    <Show when={crawlSettingsQuery.isSuccess}>
+      <div>success</div>
+      <RealCrawlingSettings
+        initialCrawlingSettings={crawlSettingsQuery.data || defaultCrawlOptions}
+      />
+    </Show>
+  );
+};
+
+interface RealCrawlingSettingsProps {
+  initialCrawlingSettings: CrawlOptions;
+}
+
+const RealCrawlingSettings = (props: RealCrawlingSettingsProps) => {
+  return (
     <div>
-      <div>Crawling settings</div>
+      <div>Crawling Options</div>
     </div>
   );
 };
