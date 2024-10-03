5314400b5be48b1582d9392da46cbc7cc56bb3e8
Drew Harris <harrisd@smu.edu>
2024-10-02 21:53:09 -0500

    fix: getting wrong crawl options

--- frontends/shared/ui/MultiStringInput.tsx
+++ frontends/shared/ui/MultiStringInput.tsx
@@ -9,19 +9,29 @@ interface MultiStringInputProps {
   inputClass?: string;
   addClass?: string;
   addLabel?: string;
+  placeholder?: string;
 }
+
+const addBlankStringIfEmpty = (value: string[]) => {
+  if (value.length === 0) {
+    return [""];
+  }
+  return value;
+};
 
 export const MultiStringInput = (props: MultiStringInputProps) => {
   const [proxyStore, setProxyStore] = createStore(
     // eslint-disable-next-line solid/reactivity
-    props.value.map((value) => ({
+    addBlankStringIfEmpty(props.value).map((value) => ({
       value,
       id: Math.random().toString(36).slice(2),
     })),
   );
 
   createEffect(() => {
-    props.onChange(proxyStore.map((item) => item.value));
+    props.onChange(
+      proxyStore.map((item) => item.value).filter((i) => i !== ""),
+    );
   });
 
   const updateValue = (id: string, value: string) => {
@@ -32,21 +42,27 @@ export const MultiStringInput = (props: MultiStringInputProps) => {
     <div class="flex min-w-[206px] flex-col gap-2 items-start">
       <For each={proxyStore}>
         {(entry) => (
-          <div class="flex items-center gap-2">
+          <div class="flex w-full items-center gap-2">
             <input
+              placeholder={props.placeholder}
               value={entry.value}
               onInput={(e) => {
                 updateValue(entry.id, e.currentTarget.value);
               }}
               class={cn(
-                "block rounded-md border border-neutral-300 px-3 py-1.5 shadow-sm placeholder:text-neutral-400 focus:outline-magenta-500 sm:text-sm sm:leading-6",
+                "block w-full rounded-md border border-neutral-300 px-3 py-1.5 shadow-sm placeholder:text-neutral-300 focus:outline-magenta-500 sm:text-sm sm:leading-6",
                 props.inputClass,
               )}
             />
             <button
+              type="button"
               class="text-neutral-400 hover:text-neutral-500 dark:text-neutral-300 dark:hover:text-neutral-400"
               onClick={() => {
-                setProxyStore((v) => v.filter((item) => item.id != entry.id));
+                if (proxyStore.length === 1) {
+                  updateValue(entry.id, "");
+                } else {
+                  setProxyStore((v) => v.filter((item) => item.id != entry.id));
+                }
               }}
             >
               <FiX />
@@ -55,6 +71,7 @@ export const MultiStringInput = (props: MultiStringInputProps) => {
         )}
       </For>
       <button
+        type="button"
         class={cn("flex gap-2 items-center justify-center", props.addClass)}
         onClick={() => {
           setProxyStore((v) => [
