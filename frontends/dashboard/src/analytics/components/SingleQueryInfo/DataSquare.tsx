251f59312c9451ab9004b07040d068bccf19dda2
skeptrune <nick.k@trieve.ai>
2024-10-04 14:19:44 -0700

    cleanup: make tables first in analytics tabs

--- frontends/dashboard/src/analytics/components/SingleQueryInfo/DataSquare.tsx
+++ frontends/dashboard/src/analytics/components/SingleQueryInfo/DataSquare.tsx
@@ -6,7 +6,7 @@ export const DataSquare = (props: {
     <div class="px-4 py-5 sm:p-6">
       <dt class="text-base font-normal text-gray-900">{props.label}</dt>
       <dd class="mt-1 flex items-baseline justify-start md:block lg:flex">
-        <div class="flex items-baseline text-xl font-semibold text-fuchsia-600">
+        <div class="flex items-baseline text-xl font-semibold text-magenta-600">
           {props.value}
         </div>
       </dd>
