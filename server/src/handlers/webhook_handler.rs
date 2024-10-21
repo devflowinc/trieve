b6d5716dc51fe338589ce6bb941dd06141885776
Drew Harris <harrisd@smu.edu>
2024-10-21 12:52:20 -0500

    feat: webhook request type

--- server/src/handlers/webhook_handler.rs
+++ server/src/handlers/webhook_handler.rs
@@ -1,18 +1,51 @@
-use actix_web::{web, HttpRequest, HttpResponse};
+use actix_web::{web, HttpResponse};
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
 #[derive(Serialize, Deserialize)]
 struct WebhookRespose {
     message: String,
 }
 
-// #[tracing::instrument(skip(pool))]
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub enum Operation {
+    Publish,
+    Archive,
+    Delete,
+    Unpublish,
+    ScheduledStart,
+    ScheduledEnd,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct WebhookRequest {
+    model_name: String,
+    new_value: Value,
+    previous_value: Value,
+    operation: Operation,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "snake_case")]
+pub struct WebhookQueryParams {
+    trieve_key: String,
+    trieve_dataset: String,
+    model_id: String,
+}
+
 pub async fn builder_webhook(
-    req: HttpRequest,
-    payload: web::Bytes,
+    payload: web::Json<WebhookRequest>,
+    query: web::Query<WebhookQueryParams>,
 ) -> Result<HttpResponse, actix_web::Error> {
+    let payload = payload.into_inner();
+    let query = query.into_inner();
+
+    log::info!("Webhook received: {:?}", payload);
+    log::info!("Query params: {:?}", query);
+
     Ok(HttpResponse::Ok().json(WebhookRespose {
         message: "Webhook received".to_string(),
     }))
 }
-
