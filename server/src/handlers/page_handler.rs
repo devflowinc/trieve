e3aef3e43c4fa2fb599c256834321d3e724e8c86
cdxker <fdenzell@gmail.com>
2024-10-24 15:17:36 -0700

    feature: jinja template to render the all component properties

--- server/src/handlers/page_handler.rs
+++ server/src/handlers/page_handler.rs
@@ -1,3 +1,4 @@
+use crate::handlers::chunk_handler::AutocompleteReqPayload;
 use crate::{
     data::models::{DatasetConfiguration, Pool, UnifiedId},
     errors::ServiceError,
@@ -6,19 +7,57 @@ use crate::{
 };
 use actix_web::{web, HttpResponse};
 use minijinja::context;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 use crate::data::models::Templates;
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize, Debug, Default)]
+enum PublicPageTheme {
+    #[default]
+    #[serde(rename = "light")]
+    Light,
+    #[serde(rename = "dark")]
+    Dark,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
 #[serde(rename_all = "camelCase")]
-pub struct PublicPageParams {
-    dataset_id: Option<uuid::Uuid>,
+pub struct PublicPageParameters {
+    dataset_id: uuid::Uuid,
+    base_url: String,
+    api_key: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    analytics: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    suggested_queries: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    responsive: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    chat: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    theme: Option<PublicPageTheme>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    search_options: Option<AutocompleteReqPayload>,
+    // openKeyCombination: { key?: string; label?: string; ctrl?: boolean }[],
+    #[serde(skip_serializing_if = "Option::is_none")]
+    brand_name: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    brand_logo_img_src_url: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    problem_link: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    accent_color: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    placeholder: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    default_search_queries: Option<Vec<String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    default_ai_questions: Option<Vec<String>>,
 }
 
 #[utoipa::path(
     get,
-    path = "/public_page",
+    path = "/public_page/{dataset_id}",
     context_path = "/api",
     tag = "Public",
     responses(
@@ -31,18 +70,13 @@ pub struct PublicPageParams {
     ),
 )]
 pub async fn public_page(
-    page_params: web::Query<PublicPageParams>,
+    dataset_id: web::Path<uuid::Uuid>,
     pool: web::Data<Pool>,
     templates: Templates<'_>,
 ) -> Result<HttpResponse, ServiceError> {
-    let Some(dataset_id) = page_params.dataset_id else {
-        println!("no dataset");
-        return Ok(HttpResponse::NotFound().finish());
-    };
+    let dataset_id = dataset_id.into_inner();
 
-    println!("id is present");
     let dataset = get_dataset_by_id_query(UnifiedId::TrieveUuid(dataset_id), pool).await?;
-    println!("dataset found");
 
     let config = DatasetConfiguration::from_json(dataset.server_configuration);
 
@@ -55,9 +89,12 @@ pub async fn public_page(
         let templ = templates.get_template("page.html").unwrap();
         let response_body = templ
             .render(context! {
-                datasetId => dataset_id,
-                baseUrl => base_server_url,
-                apiKey => config.PUBLIC_DATASET.api_key
+                params => PublicPageParameters {
+                    dataset_id,
+                    base_url: base_server_url.to_string(),
+                    api_key: config.PUBLIC_DATASET.api_key,
+                    ..Default::default()
+                }
             })
             .unwrap();
 
