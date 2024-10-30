c57520b007c90e1e8d169d17601aaeca486ccdf1
Dens Sumesh <dens.sumesh79@gmail.com>
2024-10-29 18:43:40 -0700

    bugfix: update bundler

--- clients/trieve-fumadocs-adapter/src/app/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as SearchDialog } from "./components/dialog/search-trieve.js";
-export { sync } from "./search/trieve.js";
-export { useTrieveSearch } from "./search/client.js";
--- clients/trieve-fumadocs-adapter/tsup.config.ts
+++ clients/trieve-fumadocs-adapter/tsup.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  format: ["cjs", "esm"],
-  entry: ["src/index.ts"],
+  format: "esm",
+  entry: [
+    "src/app/components/dialog/search-trieve.tsx",
+    "src/search/client/trieve.ts",
+    "src/search/client/client.ts",
+  ],
+  external: [
+    "fumadocs-core",
+    "fumadocs-ui",
+    "trieve-ts-sdk",
+    "next",
+    "react",
+    "react-dom",
+  ],
+  target: "es2020",
   dts: true,
-  shims: true,
-  skipNodeModulesBundle: true,
-  clean: true,
 });
