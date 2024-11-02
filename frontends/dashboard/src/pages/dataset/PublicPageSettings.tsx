8c1a0c0eaff9596c03af8883afa4adfff49c8c6c
Drew Harris <harrisd@smu.edu>
2024-11-01 20:01:30 -0500

    fix: set placeholder

--- frontends/dashboard/src/pages/dataset/PublicPageSettings.tsx
+++ frontends/dashboard/src/pages/dataset/PublicPageSettings.tsx
@@ -445,7 +445,7 @@ export const PublicPageSettings = () => {
             </div>
             <input
               placeholder="Search..."
-              value={""}
+              value={extraParams.placeholder || ""}
               onInput={(e) => {
                 setExtraParams("placeholder", e.currentTarget.value);
               }}
