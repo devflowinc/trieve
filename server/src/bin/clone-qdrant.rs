4eb2dba4ec9ca6c03913926cea078cd535c258de
Nick K <nick.k@trieve.ai>
2024-11-02 19:51:24 -0700

    cleanup: slow down clone qdrant worker + add logs

--- server/src/bin/clone-qdrant.rs
+++ server/src/bin/clone-qdrant.rs
@@ -16,28 +16,50 @@ async fn main() -> Result<(), ServiceError> {
     let qdrant_api_key = std::env::var("QDRANT_API_KEY").expect("QDRANT_API_KEY is not set");
     let collection_to_clone =
         std::env::var("COLLECTION_TO_CLONE").expect("COLLECTION_TO_CLONE is not set");
+    let timeout = std::env::var("QDRANT_TIMEOUT_SEC")
+        .unwrap_or("60".to_string())
+        .parse::<u64>()
+        .unwrap_or(60);
+
+    let sleep_wait_ms = std::env::var("TIMEOUT_MS")
+        .unwrap_or("200".to_string())
+        .parse::<u64>()
+        .unwrap_or(200);
 
     let mut offset = Some(uuid::Uuid::nil().to_string());
 
     while let Some(cur_offset) = offset {
         let original_qdrant_connection = Qdrant::from_url(&origin_qdrant_url)
             .api_key(qdrant_api_key.clone())
-            .timeout(std::time::Duration::from_secs(60))
+            .timeout(std::time::Duration::from_secs(timeout))
             .build()
-            .map_err(|_err| ServiceError::BadRequest("Failed to connect to Qdrant".to_string()))?;
+            .map_err(|err| {
+                ServiceError::BadRequest(format!("Failed to connect to new Qdrant {:?}", err))
+            })?;
         let new_qdrant_connection = Qdrant::from_url(&new_qdrant_url)
             .api_key(qdrant_api_key.clone())
-            .timeout(std::time::Duration::from_secs(60))
+            .timeout(std::time::Duration::from_secs(timeout))
             .build()
-            .map_err(|_err| ServiceError::BadRequest("Failed to connect to Qdrant".to_string()))?;
+            .map_err(|err| {
+                ServiceError::BadRequest(format!("Failed to connect to new Qdrant {:?}", err))
+            })?;
+
+        log::info!(
+            "Fetching points from original collection starting at: {}",
+            cur_offset
+        );
 
         let (origin_qdrant_points, new_offset) = scroll_qdrant_collection_ids_custom_url(
             collection_to_clone.clone(),
             Some(cur_offset.to_string()),
-            Some(1000),
+            Some(500),
             original_qdrant_connection,
         )
-        .await?;
+        .await
+        .map_err(|err| {
+            log::error!("Failed fetching points from qdrant {:?}", err);
+            ServiceError::BadRequest(format!("Failed fetching points from qdrant {:?}", err))
+        })?;
 
         let point_structs_to_upsert = origin_qdrant_points
             .iter()
@@ -57,6 +79,12 @@ async fn main() -> Result<(), ServiceError> {
             })
             .collect::<Vec<PointStruct>>();
 
+        log::info!(
+            "Upserting {} points into new Qdrant collection starting at: {}",
+            point_structs_to_upsert.len(),
+            cur_offset
+        );
+
         new_qdrant_connection
             .upsert_points(UpsertPointsBuilder::new(
                 collection_to_clone.clone(),
@@ -64,9 +92,16 @@ async fn main() -> Result<(), ServiceError> {
             ))
             .await
             .map_err(|err| {
-                log::error!("Failed inserting chunk to qdrant {:?}", err);
-                ServiceError::BadRequest(format!("Failed inserting chunk to qdrant {:?}", err))
+                log::error!("Failed inserting chunks to qdrant {:?}", err);
+                ServiceError::BadRequest(format!("Failed inserting chunks to qdrant {:?}", err))
             })?;
+
+        log::info!(
+            "Setting offset to: {}",
+            new_offset.clone().unwrap_or_default()
+        );
+
+        tokio::time::sleep(std::time::Duration::from_millis(sleep_wait_ms)).await;
 
         offset = new_offset;
     }
