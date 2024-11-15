218d32da33e5509ae4787398fa36eb7eeba1ea06
Drew Harris <harrisd@smu.edu>
2024-11-14 18:04:10 -0600

    feat: separate subpackage for vanilla js

--- clients/search-component/scripts/build.js
+++ clients/search-component/scripts/build.js
@@ -1,13 +1,30 @@
 const { build } = require("esbuild");
 
-build({
-  entryPoints: ["src/index.tsx"],
-  treeShaking: true,
-  outdir: "./dist",
-  sourcemap: false,
-  splitting: true,
-  bundle: true,
-  minify: true,
-  format: "esm",
-  target: ["es2020"],
-});
+const doBuild = async () => {
+  const reactBuild = await build({
+    entryPoints: ["src/index.tsx"],
+    treeShaking: true,
+    outdir: "./dist",
+    sourcemap: false,
+    splitting: true,
+    bundle: true,
+    minify: true,
+    format: "esm",
+    target: ["es2020"],
+    external: ["react", "react-dom"],
+  });
+
+  const vanillaJsBuild = await build({
+    entryPoints: ["src/vanilla.tsx"],
+    treeShaking: true,
+    outdir: "./dist/vanilla/",
+    sourcemap: false,
+    splitting: true,
+    bundle: true,
+    minify: true,
+    format: "esm",
+    target: ["es2020"],
+  });
+};
+
+doBuild();
