de45026ee459d49e577040ab29522df94f7b7fce
skeptrune <nick.k@trieve.ai>
2024-11-15 23:18:59 -0800

    wip: skeleton upload form for pdf2md

--- pdf2md/server/tailwind.config.js
+++ pdf2md/server/tailwind.config.js
@@ -6,7 +6,24 @@ module.exports = {
       sans: ["Quicksand", "system-ui", "sans-serif"],
       verdana: ["Verdana", "Geneva", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        magenta: {
+          DEFAULT: "#A33EB5",
+          50: "#E4C1EA",
+          100: "#DDB2E5",
+          200: "#CF93DA",
+          300: "#C275D0",
+          400: "#B557C5",
+          500: "#A33EB5",
+          600: "#7D308B",
+          700: "#582161",
+          800: "#321338",
+          900: "#0C050E",
+          950: "#000000",
+        },
+      },
+    },
   },
   plugins: [],
 };
