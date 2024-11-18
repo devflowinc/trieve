1eb1a52d61044c9d705c1b55728fd6b36534882e
Nick K <nick.k@trieve.ai>
2024-11-17 23:26:57 -0800

    feature: working API req on file form add

--- pdf2md/server/static/pdf2md.js
+++ pdf2md/server/static/pdf2md.js
@@ -7,30 +7,31 @@ fileUploadInput.addEventListener("change", (event) => {
     return;
   }
 
-  let base64 = null;
   const reader = new FileReader();
   reader.onload = (event) => {
-    base64 = event.target.result;
+    const base64 = event.target.result;
     console.log(base64);
-  };
 
-  const formData = {
-    base64_file: base64,
-  };
+    const formData = {
+      base64_file: base64,
+    };
 
-  fetch("/api/task", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: window.TRIEVE_API_KEY,
-    },
-    body: JSON.stringify(formData),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
+    fetch("/api/task", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: window.TRIEVE_API_KEY,
+      },
+      body: JSON.stringify(formData),
     })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  reader.readAsDataURL(file);
 });
