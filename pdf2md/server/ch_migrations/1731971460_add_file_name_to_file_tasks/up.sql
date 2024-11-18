67b2482d377afabe8b8603d89fd6146da9551e02
Drew Harris <harrisd@smu.edu>
2024-11-18 11:45:49 -0600

    feature: add table for tracking tasks

---
+++ pdf2md/server/ch_migrations/1731971460_add_file_name_to_file_tasks/up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE file_tasks ADD COLUMN IF NOT EXISTS file_name TEXT NOT NULL;
--- pdf2md/server/src/routes/jinja_templates.rs
+++ pdf2md/server/src/routes/jinja_templates.rs
@@ -2,7 +2,7 @@ use crate::{
     errors::{ErrorResponseBody, ServiceError},
     get_env, Templates,
 };
-use actix_web::{get, web, HttpResponse};
+use actix_web::{get, HttpResponse};
 use minijinja::context;
 
 #[utoipa::path(
@@ -29,20 +29,28 @@ pub async fn public_page(templates: Templates<'_>) -> Result<HttpResponse, Servi
 }
 
 #[utoipa::path(
-    get,
-    path = "/static/{file_name}",
-    context_path = "/static",
-    tag = "UI",
-    responses(
-        (status = 200, description = "File"),
-        (status = 400, description = "Service error relating to getting the file", body = ErrorResponseBody),
-    ),
-  )]
-#[get("/{file_name}")]
-pub async fn static_files(file_name: web::Path<String>) -> Result<HttpResponse, ServiceError> {
-    let sanitized_file_name = file_name.replace("..", "");
-    let file = std::fs::read_to_string(format!("./static/{}", sanitized_file_name))
-        .map_err(|_| ServiceError::InternalServerError("Failed to read file".to_string()))?;
+  get,
+  path = "/pdf/{id}",
+  context_path = "/",
+  tag = "UI",
+  responses(
+      (status = 200, description = "UI meant for public consumption"),
+      (status = 400, description = "Service error relating to loading the public page", body = ErrorResponseBody),
+  ),
+)]
+#[get("/pdf/{id}")]
+pub async fn view_pdf_page(templates: Templates<'_>) -> Result<HttpResponse, ServiceError> {
+    let templ = templates.get_template("view-pdf.html").unwrap();
+    let trieve_api_key = get_env!("API_KEY", "API_KEY should be set");
 
-    Ok(HttpResponse::Ok().body(file))
+    let pdf_url = "/static/testpdf.pdf";
+
+    let response_body = templ
+        .render(context! {
+            trieve_api_key,
+            pdf_url => pdf_url
+        })
+        .unwrap();
+
+    Ok(HttpResponse::Ok().body(response_body))
 }
