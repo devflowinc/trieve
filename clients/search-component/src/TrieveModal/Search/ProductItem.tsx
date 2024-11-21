6e482bc33c26bb7ba9bde71fb4df9c999feeccad
Drew Harris <harrisd@smu.edu>
2024-11-21 16:58:51 -0600

    cleanup: remove red border

--- clients/search-component/src/TrieveModal/Search/ProductItem.tsx
+++ clients/search-component/src/TrieveModal/Search/ProductItem.tsx
@@ -104,7 +104,7 @@ export const ProductItem = ({
   };
 
   return (
-    <li className="border border-red-500" key={item.chunk.id}>
+    <li className="border" key={item.chunk.id}>
       {group && (
         <button
           onClick={(e) => {
