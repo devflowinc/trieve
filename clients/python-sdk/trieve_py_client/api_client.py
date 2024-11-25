377f79a747d3f47c6c4cbff9df804afc59092173
Nick K <nick.k@trieve.ai>
2024-11-25 14:01:51 -0800

    bugfix: chunk routes on python SDK

--- clients/python-sdk/trieve_py_client/api_client.py
+++ clients/python-sdk/trieve_py_client/api_client.py
@@ -88,7 +88,7 @@ class ApiClient:
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'OpenAPI-Generator/0.12.11/python'
+        self.user_agent = 'OpenAPI-Generator/0.12.12/python'
         self.client_side_validation = configuration.client_side_validation
 
     def __enter__(self):
