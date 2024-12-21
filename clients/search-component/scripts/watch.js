09ef286499e71ce700f29efc1b6ff2295281667d
cdxker <fdenzell@gmail.com>
2024-12-20 18:51:17 -0800

    chore: refactor search component UX for running local dev with public page

--- clients/search-component/scripts/watch.js
+++ clients/search-component/scripts/watch.js
@@ -1,8 +1,7 @@
-const { context } = require("esbuild");
-const { options } = require("./build");
+const { context, build } = require("esbuild");
 
 const main = async () => {
-  let ctx1 = await context({
+  const ctx1 = await context({
     entryPoints: ["src/index.tsx"],
     treeShaking: true,
     outdir: "./dist",
@@ -14,6 +13,23 @@ const main = async () => {
     target: ["es2020"],
     external: ["react", "react-dom"],
   });
+  const vanillaJsBuild = await context({
+    entryPoints: ["src/vanilla/index.tsx"],
+    treeShaking: true,
+    outdir: "./dist/vanilla/",
+    sourcemap: false,
+    splitting: true,
+    bundle: true,
+    minify: true,
+    format: "esm",
+    target: ["es2020"],
+  });
+
+  Promise.all([
+    ctx1.watch(),
+    vanillaJsBuild.watch()
+  ])
+
 };
 
 main();
