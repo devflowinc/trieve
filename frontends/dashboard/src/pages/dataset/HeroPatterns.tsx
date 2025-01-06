511ece6facd64603c73ef8c601adbf679850fbfe
Vidyoot Senthil <vidyoots@gmail.com>
2025-01-06 14:27:47 -0600

    feature: use white as default demo page background color

--- frontends/dashboard/src/pages/dataset/HeroPatterns.tsx
+++ frontends/dashboard/src/pages/dataset/HeroPatterns.tsx
@@ -2,9 +2,6 @@ export const HeroPatterns: Record<
   string,
   (color: string, opacity: number) => string
 > = {
-  Blank: () => {
-    return "";
-  },
   Solid: (foregroundColor: string, foregroundOpacity: number) => {
     const encoded = btoa(
       `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1 1" width="1" height="1">
