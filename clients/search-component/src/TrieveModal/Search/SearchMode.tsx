ad69339a162665d242aa32584a7b3572e7f33460
Dens Sumesh <dens.sumesh79@gmail.com>
2025-01-09 13:59:16 -0800

    bugfix: remove search icon

--- clients/search-component/src/TrieveModal/Search/SearchMode.tsx
+++ clients/search-component/src/TrieveModal/Search/SearchMode.tsx
@@ -148,21 +148,6 @@ export const SearchMode = () => {
       </div>
       <div className={`input-wrapper ${props.type}`}>
         <div className="input-flex">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="search-icon"
-          >
-            <circle cx="11" cy="11" r="8"></circle>
-            <path d="m21 21-4.3-4.3"></path>
-          </svg>
           <input
             ref={inputRef}
             value={query}
