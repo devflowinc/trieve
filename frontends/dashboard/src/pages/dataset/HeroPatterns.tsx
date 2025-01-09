45318b276d3587b4aecb2d47a936418f58eb7ba5
Dens Sumesh <dens.sumesh79@gmail.com>
2025-01-08 19:30:34 -0800

    bugfix: fix dropdowns not working

--- frontends/dashboard/src/pages/dataset/HeroPatterns.tsx
+++ frontends/dashboard/src/pages/dataset/HeroPatterns.tsx
@@ -30,4 +30,9 @@ export const HeroPatterns: Record<
     );
     return `data:image/svg+xml;base64,${encoded}`;
   },
+  Blank: (foregroundColor: string, foregroundOpacity: number) => {
+    return `data:image/svg+xml;base64,${btoa(
+      `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1 1" width="1" height="1" fill="${foregroundColor}" fill-opacity="${foregroundOpacity}" ></svg>`,
+    )}`;
+  },
 };
