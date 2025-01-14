548ece68dc85ab9a23dc07a79c4f9dbb66f4fcb0
cdxker <fdenzell@gmail.com>
2025-01-13 21:05:33 -0800

    feature: update beta url to use the bunny CDN

--- server/src/handlers/page_handler.rs
+++ server/src/handlers/page_handler.rs
@@ -317,7 +317,7 @@ pub async fn public_page(
         .is_test_mode
         .unwrap_or(false)
     {
-        "https://test-search-component.trieve.ai".to_string()
+        "https://cdn.trieve.ai/beta/search-component".to_string()
     } else {
         std::env::var("SEARCH_COMPONENT_URL")
             .unwrap_or("https://search-component.trieve.ai".to_string())
