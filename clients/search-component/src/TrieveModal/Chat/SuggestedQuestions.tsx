2a10076dde9c6612658ffdb8bee4ba7ca9284475
skeptrune <nick.k@trieve.ai>
2025-01-22 18:48:59 -0800

    cleanup: fix flash on first link appearance + fix height on relead suggest button

--- clients/search-component/src/TrieveModal/Chat/SuggestedQuestions.tsx
+++ clients/search-component/src/TrieveModal/Chat/SuggestedQuestions.tsx
@@ -7,7 +7,7 @@ import { useAutoAnimate } from "@formkit/auto-animate/react";
 import { SparklesIcon } from "../icons";
 
 export const SuggestedQuestions = () => {
-  const { askQuestion, setCurrentQuestion } = useChatState();
+  const { askQuestion, messages, setCurrentQuestion } = useChatState();
   const {
     suggestedQuestions,
     isLoadingSuggestedQueries,
@@ -17,47 +17,53 @@ export const SuggestedQuestions = () => {
   const { props } = useModalState();
   const [parent] = useAutoAnimate();
 
+  if (messages.length) {
+    return null;
+  }
+
   return (
-    <div
-      className={cn(
-        props.inline && "tv-flex tv-gap-x-3 tv-flex-wrap tv-items-center",
-      )}
-    >
+    <div className="ai-message initial-message">
       <div
-        ref={parent}
         className={cn(
-          "questions tv-pt-2 ",
-          props.inline && "inline-questions !tv-pb-0",
+          props.inline && "tv-flex tv-gap-x-3 tv-flex-wrap tv-items-center"
         )}
       >
-        <button
-          onClick={refetchSuggestedQuestion}
-          disabled={isLoadingSuggestedQueries}
-          className="suggested-question tv-cursor-pointer tv-border tv-rounded-md tv-p-1 tv-text-xs disabled:tv-cursor-not-allowed tv-text-center"
-          title="Refresh suggested questions"
+        <div
+          ref={parent}
+          className={cn(
+            "questions tv-pt-2 ",
+            props.inline && "inline-questions !tv-pb-0"
+          )}
         >
-          <i className="fa-solid fa-arrow-rotate-right"></i>
-        </button>{" "}
-        {!props.inline && !suggestedQuestions.length && (
-          <p className="suggested-question tv-text-nowrap empty-state-loading">
-            Loading example questions...
-          </p>
-        )}
-        {suggestedQuestions?.map((q) => (
           <button
-            onClick={() => {
-              setCurrentQuestion(q);
-              askQuestion(q);
-            }}
-            key={q}
-            className={`suggested-question tv-flex tv-gap-1 tv-items-center ${
-              isLoadingSuggestedQueries ? "loading" : ""
-            }`}
+            onClick={refetchSuggestedQuestion}
+            disabled={isLoadingSuggestedQueries}
+            className="suggested-question tv-cursor-pointer tv-border tv-rounded-md tv-p-1 tv-text-xs disabled:tv-cursor-not-allowed tv-text-center"
+            title="Refresh suggested questions"
           >
-            <SparklesIcon width={15} height={15} />
-            {q}
-          </button>
-        ))}
+            <i className="fa-solid fa-arrow-rotate-right"></i>
+          </button>{" "}
+          {!suggestedQuestions.length && (
+            <span className="suggested-question tv-text-nowrap empty-state-loading">
+              Loading example questions...
+            </span>
+          )}
+          {suggestedQuestions?.map((q) => (
+            <button
+              onClick={() => {
+                setCurrentQuestion(q);
+                askQuestion(q);
+              }}
+              key={q}
+              className={`suggested-question tv-flex tv-gap-1 tv-items-center${
+                isLoadingSuggestedQueries ? " loading" : ""
+              }`}
+            >
+              <SparklesIcon width={15} height={15} />
+              {q}
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
