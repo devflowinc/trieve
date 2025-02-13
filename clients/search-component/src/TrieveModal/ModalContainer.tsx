d7fbc436e34c0337b70d59e5dc32b48c127a0ee4
Drew Harris <dsharris10@gmail.com>
2025-02-13 12:06:41 -0600

    feat: sizing

--- clients/search-component/src/TrieveModal/ModalContainer.tsx
+++ clients/search-component/src/TrieveModal/ModalContainer.tsx
@@ -5,18 +5,21 @@ import { PdfViewer, pdfViewState } from "./PdfView/PdfViewer";
 import { ChatModeSwitch } from "./ModeSwitch";
 import { cn } from "../utils/styles";
 import { Footer } from "./Footer";
+import { useSizing } from "../utils/hooks/useSizing";
 const SearchMode = lazy(() => import("./Search/SearchMode"));
 const ChatMode = lazy(() => import("./Chat/ChatMode"));
 
 export const ModalContainer = () => {
   const { mode, props } = useModalState();
   const [fullscreenPdfState] = useAtom(pdfViewState);
+
+  const componentClass = useSizing();
   return (
     <Suspense>
       <div
         id="trieve-search-modal"
         className={cn(
-          "tv-scroll-smooth tv-max-h-[40vh] tv-resize tv-w-[90vw] sm:tv-max-w-[800px] tv-rounded-lg focus:tv-outline-none tv-overflow-hidden tv-text-base tv-text-zinc-950 tv-bg-white",
+          "tv-scroll-smooth tv-resize tv-rounded-lg focus:tv-outline-none tv-overflow-hidden tv-text-base tv-text-zinc-950 tv-bg-white",
           `${mode === "chat" ? "chat-modal-mobile" : ""}${
             props.theme === "dark" ? " dark tv-dark" : ""
           }${
@@ -25,23 +28,10 @@ export const ModalContainer = () => {
               : " trieve-popup-modal"
           } ${props.type}`.trim(),
 
-          props.inline && "tv-border-2 tv-max-w-sm tv-min-w-full !tv-w-full",
-
-          !props.inline &&
-            "tv-top-[calc(40%-30vh)] tv-left-[50%] tv-shadow-2xl tv-fixed -tv-translate-x-[50%]",
-
-          props.type === "ecommerce" &&
-            props.inline &&
-            "tv-top-1 tv-max-w-[90rem] tv-w-[95vw] tv-px-4 tv-rounded-lg",
-
-          props.type === "ecommerce" &&
-            props.inline &&
-            "tv-top-1 tv-px-0 tv-pt-0 tv-max-w-full",
-          props.type === "ecommerce" &&
-            !props.inline &&
-            "tv-max-w-[90rem] tv-px-4",
+          props.inline && "tv-border-2",
 
           "tv-flex tv-flex-col tv-items-stretch",
+          componentClass,
         )}
         style={{
           zIndex: props.zIndex ? props.zIndex + 1 : 1001,
