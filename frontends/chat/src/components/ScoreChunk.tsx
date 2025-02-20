b1c8a841fe04794e1762244209c40b0f3b2fd6c1
Nick K <nick.k@trieve.ai>
2025-02-20 08:37:15 -0800

    cleanup(search): only show tag set in search playground when there is a non empty tag

--- frontends/chat/src/components/ScoreChunk.tsx
+++ frontends/chat/src/components/ScoreChunk.tsx
@@ -237,7 +237,7 @@ const ScoreChunk = (props: ScoreChunkProps) => {
               </span>
             </div>
           </Show>
-          <Show when={props.chunk.tag_set}>
+          <Show when={props.chunk.tag_set?.filter((tag) => tag).length}>
             <div class="flex space-x-2">
               <span class="text-nowrap font-semibold text-neutral-800 dark:text-neutral-200">
                 Tag Set:{" "}
