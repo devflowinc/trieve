42aa671bffb04bcbe49a5bdc2ca5eab714f9b580
Drew Harris <dsharris10@gmail.com>
2025-02-20 16:03:00 -0600

    feat: working ducking fix!!

--- clients/search-component/src/TrieveModal/Chat/ChatMode.tsx
+++ clients/search-component/src/TrieveModal/Chat/ChatMode.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useRef } from "react";
 import { useModalState } from "../../utils/hooks/modal-context";
 import { useChatState } from "../../utils/hooks/chat-context";
 import { ResponseMessage } from "./ResponseMessage";
@@ -8,10 +8,14 @@ import { FollowupQueries } from "./FollowupQueries";
 import { UserMessage } from "./UserMessage";
 import { InlineChatHeader } from "./InlineChatHeader";
 import { ChatInput } from "./ChatInput";
+import { useChatHeight } from "../../utils/hooks/useChatHeight";
 
 export const ChatMode = () => {
   const { modalRef } = useModalState();
   const { messages } = useChatState();
+
+  const actualChatRef = useRef<HTMLDivElement>(null);
+  const { minHeight, resetHeight } = useChatHeight(actualChatRef);
 
   const [ref, { entry }] = useIntersectionObserver();
   const isOnScreen = entry && entry.isIntersecting;
@@ -21,24 +25,47 @@ export const ChatMode = () => {
       <InlineChatHeader />
       <div
         ref={modalRef}
-        className="chat-modal-wrapper tv-flex-grow tv-pt-3 tv-pb-2 tv-px-2 tv-relative tv-overflow-auto tv-flex tv-flex-col tv-gap-4"
+        className="chat-modal-wrapper tv-justify-items-stretch tv-flex-grow tv-pt-3 tv-pb-2 tv-px-2 tv-relative tv-overflow-auto tv-flex"
       >
-        <SuggestedQuestions /> {/* Only shows with zero messages */}
-        {messages.map((message, i) => {
-          if (message.type === "user") {
-            return <UserMessage key={i} message={message} idx={i} />;
-          } else {
-            return <ResponseMessage key={i} message={message} idx={i} />;
-          }
-        })}
-        <FollowupQueries />
+        <ChatRuler minHeight={minHeight} />
         <div
-          ref={ref}
-          className="tv-z-50 tv-opacity-0 tv-mx-4 tv-w-4 tv-min-h-1 tv-h-1"
-        ></div>
+          className="tv-flex-col tv-h-full tv-outline tv-outline-green-500 tv-grow tv-flex tv-gap-4"
+          ref={actualChatRef}
+        >
+          <SuggestedQuestions /> {/* Only shows with zero messages */}
+          {messages.map((message, i) => {
+            if (message.type === "user") {
+              return <UserMessage key={i} message={message} idx={i} />;
+            } else {
+              return <ResponseMessage key={i} message={message} idx={i} />;
+            }
+          })}
+          <FollowupQueries />
+          <div
+            ref={ref}
+            className="tv-z-50 tv-opacity-0 tv-mx-4 tv-w-4 tv-min-h-1 tv-h-1"
+          ></div>
+        </div>
       </div>
       <ChatInput showShadow={!isOnScreen} />
     </Suspense>
+  );
+};
+
+// sits on the left side of chat in a flexbox to enforce the minimum height and control scroll
+const ChatRuler = ({ minHeight }: { minHeight: number }) => {
+  return (
+    <div
+      style={{
+        minHeight,
+      }}
+      className="tv-outline tv-outline-purple-500 tv-mr-2"
+    >
+      <div className="tv-w-8 tv-h-8 tv-bg-red-500"></div>
+      <div className="tv-w-8 tv-h-8 tv-bg-blue-500"></div>
+      <div className="tv-w-8 tv-h-8 tv-bg-red-500"></div>
+      <div className="tv-w-8 tv-h-8 tv-bg-blue-500"></div>
+    </div>
   );
 };
 
