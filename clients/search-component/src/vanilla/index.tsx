afd95ad16ba8ec661b08caef2764c06da9838035
Drew Harris <dsharris10@gmail.com>
2025-02-23 20:35:56 -0600

    feat: exports for new component

--- clients/search-component/src/vanilla/index.tsx
+++ clients/search-component/src/vanilla/index.tsx
@@ -2,6 +2,10 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 import { TrieveModalSearch } from "../TrieveModal";
 import { ModalProps } from "../utils/hooks/modal-context";
+import {
+  Recommendations,
+  RecommendationsConfig,
+} from "../recommendations/Recommendations";
 
 export function renderToDiv(element: HTMLElement, props: ModalProps) {
   if (props.cssRelease) {
@@ -10,27 +14,64 @@ export function renderToDiv(element: HTMLElement, props: ModalProps) {
         break;
       }
       case "stable": {
-        const link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.href = 'https://search-component.trieve.ai/dist/index.css';
+        const link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = "https://search-component.trieve.ai/dist/index.css";
         document.head.appendChild(link);
         break;
       }
       case "beta": {
-        const link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.href = 'https://test-search-component.trieve.ai/dist/index.css';
+        const link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = "https://test-search-component.trieve.ai/dist/index.css";
         document.head.appendChild(link);
         break;
       }
     }
   } else {
     // load stable default
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = 'https://search-component.trieve.ai/dist/index.css';
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "https://search-component.trieve.ai/dist/index.css";
     document.head.appendChild(link);
   }
 
   ReactDOM.createRoot(element).render(<TrieveModalSearch {...props} />);
+}
+
+export function renderRecommendationsToDiv(
+  element: HTMLElement,
+  props: RecommendationsConfig,
+) {
+  if (props.cssRelease) {
+    switch (props.cssRelease) {
+      case "none": {
+        break;
+      }
+      case "stable": {
+        const link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href =
+          "https://search-component.trieve.ai/dist/recommendations.css";
+        document.head.appendChild(link);
+        break;
+      }
+      case "beta": {
+        const link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href =
+          "https://test-search-component.trieve.ai/dist/recommendations.css";
+        document.head.appendChild(link);
+        break;
+      }
+    }
+  } else {
+    // load stable default
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "https://search-component.trieve.ai/dist/recommendations.css";
+    document.head.appendChild(link);
+  }
+
+  ReactDOM.createRoot(element).render(<Recommendations {...props} />);
 }
