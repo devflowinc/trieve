a71f5c9a506678ec588fbd8309dbd70c092117e4
cdxker <fdenzell@gmail.com>
2025-02-24 13:41:50 -0800

    feat(backfill): add the ability to specify a single dataset to migrate users to different qdrant instances

--- server/src/bin/backfill-qdrant-from-pg.rs
+++ server/src/bin/backfill-qdrant-from-pg.rs
@@ -54,6 +54,7 @@ async fn main() -> Result<(), ServiceError> {
     let web_pool = actix_web::web::Data::new(pool.clone());
 
     let redis_url = get_env!("REDIS_URL", "REDIS_URL should be set");
+    let queue_name = std::env::var("INGESTION_QUEUE_NAME").unwrap_or("sync_ingestion".to_string());
 
     let broccoli_queue = BroccoliQueue::builder(redis_url)
         .pool_connections(5.try_into().unwrap())
@@ -70,6 +71,10 @@ async fn main() -> Result<(), ServiceError> {
         .unwrap_or(uuid::Uuid::max());
 
     let mut offset = Some(start_offset);
+
+    let dataset_id = std::env::var("dataset_id")
+        .map(|dataset_str| uuid::Uuid::parse_str(&dataset_str).ok().unwrap())
+        .unwrap_or(uuid::Uuid::max());
 
     while let Some(cur_offset) = offset {
         use trieve_server::data::schema::chunk_group_bookmarks::dsl as chunk_group_bookmarks_columns;
@@ -89,6 +94,7 @@ async fn main() -> Result<(), ServiceError> {
                 chunk_metadata_columns::qdrant_point_id,
                 chunk_metadata_columns::dataset_id,
             ))
+            .filter(chunk_metadata_columns::dataset_id.eq(dataset_id))
             .filter(chunk_metadata_columns::id.ge(cur_offset))
             .order_by(chunk_metadata_columns::id)
             .limit(postgres_fetch_points_count)
@@ -301,7 +307,7 @@ async fn main() -> Result<(), ServiceError> {
                     for message in chunk_messages {
                         broccoli_queue
                             .publish(
-                                "sync_ingestion",
+                                &queue_name,
                                 Some(message.dataset_id.to_string()),
                                 &message,
                                 None,
