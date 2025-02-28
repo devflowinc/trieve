c9b2d0a3bf5da4e9eaaf0cf37e4aa68334e80164
Drew Harris <dsharris10@gmail.com>
2025-02-27 18:19:33 -0600

    cleanup: remove deprecated json

--- clients/trieve-shopify-extension/app/routes/app._dashboard.tsx
+++ clients/trieve-shopify-extension/app/routes/app._dashboard.tsx
@@ -103,7 +103,7 @@ export default function Dashboard() {
           queries: {
             // With SSR, we usually want to set some default staleTime
             // above 0 to avoid refetching immediately on the client
-            staleTime: 60 * 1000,
+            staleTime: 2 * 1000,
           },
         },
       }),
