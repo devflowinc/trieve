3eab549fa2d71d57be569f22fa2deeec3c8c7a99
Drew Harris <dsharris10@gmail.com>
2025-02-27 23:02:19 -0600

    squashme: stay with me here

---
+++ clients/trieve-shopify-extension/app/queries/scrapeOptions.ts
@@ -0,0 +1,24 @@
+import { QueryOptions } from "@tanstack/react-query";
+import { defaultCrawlOptions } from "app/components/DatasetSettings";
+import { TrieveSDK } from "trieve-ts-sdk";
+
+export const scrapeOptionsQuery = (trieve: TrieveSDK) => {
+  return {
+    queryKey: ["scrape_options", trieve.datasetId],
+    queryFn: async () => {
+      const crawlOptions = await trieve.trieve.fetch("/api/crawl", "get", {
+        datasetId: trieve.datasetId!,
+      });
+      const mappedCrawlOptions = crawlOptions[0]?.crawl_options
+        ? {
+            ...crawlOptions[0].crawl_options,
+            scrape_options: {
+              ...crawlOptions[0].crawl_options.scrape_options,
+              type: "shopify" as const,
+            },
+          }
+        : defaultCrawlOptions;
+      return mappedCrawlOptions;
+    },
+  } satisfies QueryOptions;
+};
