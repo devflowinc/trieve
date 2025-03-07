f96b603d5a28b077dd0b8ede22b96819de2182a5
Drew Harris <dsharris10@gmail.com>
2025-03-06 18:19:20 -0600

    fix: sticky checkbox button on dashboard

--- frontends/dashboard/src/pages/dataset/PublicPageSettings.tsx
+++ frontends/dashboard/src/pages/dataset/PublicPageSettings.tsx
@@ -1078,9 +1078,7 @@ const PublicPageControls = () => {
                 </div>
                 <input
                   type="checkbox"
-                  checked={
-                    (extraParams.showResultHighlights as boolean) || true
-                  }
+                  checked={extraParams.showResultHighlights === true}
                   onChange={(e) => {
                     setExtraParams(
                       "showResultHighlights",
