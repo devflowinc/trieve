d4f087263906c805b4e657e3cfdc60355e3a78c8
Drew Harris <dsharris10@gmail.com>
2025-03-12 16:33:07 -0500

    fix: remove trailing slash

--- clients/trieve-shopify-extension/app/env.ts
+++ clients/trieve-shopify-extension/app/env.ts
@@ -1,7 +1,7 @@
 export const getTrieveBaseUrl = (): string => {
   if (!(typeof document === "undefined")) {
     // @ts-ignore
-    return window?.env?.TRIEVE_BASE_URL ?? "https://api.trieve.ai/";
+    return window?.env?.TRIEVE_BASE_URL ?? "https://api.trieve.ai";
   }
-  return process.env.TRIEVE_BASE_URL ?? "https://api.trieve.ai/";
+  return process.env.TRIEVE_BASE_URL ?? "https://api.trieve.ai";
 };
