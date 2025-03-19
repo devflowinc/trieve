40a80187a1c9e40f8db4907bf10684a58276edaa
skeptrune <nick.k@trieve.ai>
2025-03-19 13:47:31 -0700

    bugfix(chat): build with makePersisted primitive

--- frontends/chat/src/components/Atoms/MessageVoting.tsx
+++ frontends/chat/src/components/Atoms/MessageVoting.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { makePersisted } from "@solid-primitives/storage";
 import { IconTypes } from "solid-icons";
 import {
@@ -22,7 +25,7 @@ const apiHost = import.meta.env.VITE_API_HOST as unknown as string;
 
 // Create store exepcted to be directly destructured, this is fine
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, solid/reactivity
-const [store, setStore] = makePersisted(createStore<MessageVoteStore>({}), {
+const [store, setStore] = makePersisted(createStore({}), {
   name: "vote-cache",
 });
 
@@ -30,8 +33,8 @@ export const MessageVoting = (props: MessageVotingProps) => {
   const userContext = useContext(UserContext);
 
   const updateVote = async (queryId: string, vote: number) => {
-    const prevVote = store[queryId];
-    setStore(queryId, vote);
+    const prevVote = (store as MessageVoteStore)[queryId];
+    setStore(queryId as never, vote as any);
     const dataset = userContext.currentDataset?.();
     if (dataset?.dataset.id) {
       const response = await fetch(apiHost + "/analytics/rag", {
@@ -50,7 +53,7 @@ export const MessageVoting = (props: MessageVotingProps) => {
       if (response.status !== 204) {
         console.error("Failed to update vote", response);
         // Rollback the vote
-        setStore(queryId, prevVote);
+        setStore(queryId as never, prevVote as any);
       }
     }
   };
@@ -68,44 +71,52 @@ export const MessageVoting = (props: MessageVotingProps) => {
       <div class="flex flex-col gap-2 pt-1">
         <Show
           fallback={
+            <button>
+              <VoteIcon
+                icon={BsHandThumbsUp}
+                onClick={(e) => {
+                  e.preventDefault();
+                  void updateVote(props.queryId, 1);
+                }}
+              />
+            </button>
+          }
+          when={(store as any)[props.queryId] === 1}
+        >
+          <button>
             <VoteIcon
-              icon={BsHandThumbsUp}
+              icon={BsHandThumbsUpFill}
               onClick={(e) => {
                 e.preventDefault();
-                void updateVote(props.queryId, 1);
+                void updateVote(props.queryId, 0);
               }}
             />
-          }
-          when={store[props.queryId] === 1}
-        >
-          <VoteIcon
-            icon={BsHandThumbsUpFill}
-            onClick={(e) => {
-              e.preventDefault();
-              void updateVote(props.queryId, 0);
-            }}
-          />
+          </button>
         </Show>
 
         <Show
           fallback={
+            <button>
+              <VoteIcon
+                icon={BsHandThumbsDown}
+                onClick={(e) => {
+                  e.preventDefault();
+                  void updateVote(props.queryId, -1);
+                }}
+              />
+            </button>
+          }
+          when={(store as any)[props.queryId] === -1}
+        >
+          <button>
             <VoteIcon
-              icon={BsHandThumbsDown}
+              icon={BsHandThumbsDownFill}
               onClick={(e) => {
                 e.preventDefault();
-                void updateVote(props.queryId, -1);
+                void updateVote(props.queryId, 0);
               }}
             />
-          }
-          when={store[props.queryId] === -1}
-        >
-          <VoteIcon
-            icon={BsHandThumbsDownFill}
-            onClick={(e) => {
-              e.preventDefault();
-              void updateVote(props.queryId, 0);
-            }}
-          />
+          </button>
         </Show>
       </div>
     </>
