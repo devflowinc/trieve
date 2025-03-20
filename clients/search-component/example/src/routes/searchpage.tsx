690eff6218112d9fe18e7e5442e65ecec2485335
skeptrune <nick.k@trieve.ai>
2025-03-20 00:18:34 -0700

    feat(searchpage): add the ability for heirarchical category filters

--- clients/search-component/example/src/routes/searchpage.tsx
+++ clients/search-component/example/src/routes/searchpage.tsx
@@ -2,10 +2,306 @@ import "../../../dist/index.css";
 import "../custom-css/searchpage.css";
 import { TrieveModalSearch } from "../../../src/index";
 import { createFileRoute } from "@tanstack/react-router";
+import { TagProp } from "../../../src/utils/hooks/modal-context";
 
 export const Route = createFileRoute("/searchpage")({
   component: ECommerce,
 });
+
+const options: TagProp[] = [
+  {
+    label: "Wallpaper",
+    tag: "Wallpaper",
+    child: {
+      key: "wallpaperChild",
+      title: "Wallpaper Options",
+      selectionType: "multiple",
+      filterType: "match_any",
+      options: [
+        {
+          label: "Natural",
+          tag: "Natural",
+        },
+        {
+          label: "Paper",
+          tag: "Paper",
+        },
+        {
+          label: "Synthetic",
+          tag: "Synthetic",
+        },
+        {
+          label: "Vinyl",
+          tag: "Vinyl",
+        },
+      ],
+    },
+  },
+  {
+    label: "Countertops & Stone",
+    tag: "Countertops & Stone",
+    child: {
+      key: "countertopsChild",
+      title: "Countertops & Stone Options",
+      selectionType: "multiple",
+      filterType: "match_any",
+      options: [
+        {
+          label: "Engineered & Composite",
+          tag: "Engineered & Composite",
+        },
+        {
+          label: "Laminate",
+          tag: "Laminate",
+        },
+        {
+          label: "Solid Surface",
+          tag: "Solid Surface",
+        },
+        {
+          label: "Stone",
+          tag: "Stone",
+        },
+      ],
+    },
+  },
+  {
+    label: "Tile",
+    tag: "Tile",
+  },
+  {
+    label: "Wood Flooring",
+    tag: "Wood Flooring",
+  },
+  {
+    label: "LVT & Specialty Flooring",
+    tag: "LVT & Specialty Flooring",
+    child: {
+      key: "lvtFlooringChild",
+      title: "LVT & Specialty Flooring Options",
+      selectionType: "multiple",
+      filterType: "match_any",
+      options: [
+        {
+          label: "Laminate",
+          tag: "Laminate",
+        },
+        {
+          label: "Luxury Vinyl Tile",
+          tag: "Luxury Vinyl Tile",
+        },
+        {
+          label: "Resilient",
+          tag: "Resilient",
+        },
+      ],
+    },
+  },
+  {
+    label: "Paint",
+    tag: "Paint",
+  },
+  {
+    label: "Carpet & Carpet Tile",
+    tag: "Carpet & Carpet Tile",
+  },
+  {
+    label: "Fabric & Leather",
+    tag: "Fabric & Leather",
+    child: {
+      key: "fabricLeatherChild",
+      title: "Fabric & Leather Options",
+      selectionType: "multiple",
+      filterType: "match_any",
+      options: [
+        {
+          label: "Cotton",
+          tag: "Cotton",
+        },
+        {
+          label: "Leather",
+          tag: "Leather",
+        },
+        {
+          label: "Linen",
+          tag: "Linen",
+        },
+        {
+          label: "Sheer",
+          tag: "Sheer",
+        },
+        {
+          label: "Synthetic",
+          tag: "Synthetic",
+        },
+        {
+          label: "Velvet",
+          tag: "Velvet",
+        },
+        {
+          label: "Wool",
+          tag: "Wool",
+        },
+      ],
+    },
+  },
+  {
+    label: "Paneling",
+    tag: "Paneling",
+    child: {
+      key: "panelingChild",
+      title: "Paneling Options",
+      selectionType: "multiple",
+      filterType: "match_any",
+      options: [
+        {
+          label: "Decorative",
+          tag: "Decorative",
+        },
+        {
+          label: "Cork",
+          tag: "Cork",
+        },
+        {
+          label: "Laminate",
+          tag: "Laminate",
+        },
+        {
+          label: "Plastic & Synthetics",
+          tag: "Plastic & Synthetics",
+        },
+        {
+          label: "Wood & Wood Alternatives",
+          tag: "Wood & Wood Alternatives",
+        },
+      ],
+    },
+  },
+  {
+    label: "Hardware",
+    tag: "Hardware",
+  },
+  {
+    label: "Faucets",
+    tag: "Faucets",
+    child: {
+      key: "fixtureTypeChild",
+      title: "Faucets Type Options",
+      selectionType: "multiple",
+      filterType: "match_any",
+      options: [
+        {
+          label: "Accessories",
+          tag: "Accessories",
+        },
+        {
+          label: "Bar Faucets",
+          tag: "Bar Faucets",
+        },
+        {
+          label: "Bath Faucets",
+          tag: "Bath Faucets",
+        },
+        {
+          label: "Claw Foot Tubs",
+          tag: "Claw Foot Tubs",
+        },
+        {
+          label: "Console Sink",
+          tag: "Console Sink",
+        },
+        {
+          label: "Drop In Tubs",
+          tag: "Drop In Tubs",
+        },
+        {
+          label: "Drop-In Sink",
+          tag: "Drop-In Sink",
+        },
+        {
+          label: "Freestanding Tubs",
+          tag: "Freestanding Tubs",
+        },
+        {
+          label: "Kitchen Faucets",
+          tag: "Kitchen Faucets",
+        },
+        {
+          label: "Pedestal Sink",
+          tag: "Pedestal Sink",
+        },
+        {
+          label: "Pot Fillers",
+          tag: "Pot Fillers",
+        },
+        {
+          label: "Semi-Recessed Sink",
+          tag: "Semi-Recessed Sink",
+        },
+        {
+          label: "Shower & Tub Trim",
+          tag: "Shower & Tub Trim",
+        },
+        {
+          label: "Shower Only Trim",
+          tag: "Shower Only Trim",
+        },
+        {
+          label: "Undermount Sink",
+          tag: "Undermount Sink",
+        },
+        {
+          label: "Vessel Sink",
+          tag: "Vessel Sink",
+        },
+        {
+          label: "Wall Hung Vanity Cabinet",
+          tag: "Wall Hung Vanity Cabinet",
+        },
+        {
+          label: "Wall-Mounted Sink",
+          tag: "Wall-Mounted Sink",
+        },
+      ],
+    },
+  },
+  {
+    label: "Cabinets",
+    tag: "Cabinets",
+  },
+  {
+    label: "Decking",
+    tag: "Decking",
+  },
+  {
+    label: "Area Rugs",
+    tag: "Area Rugs",
+  },
+  {
+    label: "Blinds & Shades",
+    tag: "Blinds & Shades",
+  },
+  {
+    label: "Ceiling",
+    tag: "Ceiling",
+    child: {
+      key: "ceilingChild",
+      title: "Ceiling Options",
+      selectionType: "multiple",
+      filterType: "match_any",
+      options: [
+        {
+          label: "Panel",
+          tag: "Panel",
+        },
+        {
+          label: "Tile",
+          tag: "Tile",
+        },
+      ],
+    },
+  },
+];
 
 export default function ECommerce() {
   const baseUrl = import.meta.env.VITE_API_BASE_URL;
@@ -26,101 +322,20 @@ export default function ECommerce() {
             allowSwitchingModes={false}
             brandColor="#a33eb5"
             type="ecommerce"
+            searchOptions={{
+              search_type: "hybrid",
+              page_size: 20,
+            }}
             inlineCarousel={true}
             searchPageProps={{
               filterSidebarProps: {
                 sections: [
                   {
                     key: "categories",
-                    title: "Categories",
-                    options: [
-                      {
-                        label: "Backsplash",
-                        tag: "Backsplash",
-                        description:
-                          "Set this to true anytime the image seems to include backsplashes or a kitchen counter in general.",
-                      },
-                      {
-                        label: "Flooring",
-                        tag: "Flooring",
-                        description:
-                          "Set this to true anytime the image seems to include floors.",
-                      },
-                      {
-                        label: "Countertops",
-                        tag: "Countertops",
-                        description:
-                          "Set this to true anytime the image seems to include countertops or something that looks vaguely like a countertop.",
-                      },
-                      {
-                        label: "Cabinets",
-                        tag: "Cabinets",
-                        description:
-                          "Set this to true anytime the image seems to include cabinets.",
-                      },
-                      {
-                        label: "Hardware",
-                        tag: "Hardware",
-                        description:
-                          "Set this to true anytime the image seems to include or like it could include hardware like cabinet handles or knobs.",
-                      },
-                      {
-                        label: "Sinks",
-                        tag: "Sinks",
-                        description:
-                          "Set this to true anytime the image seems to include sinks.",
-                      },
-                      {
-                        label: "Rugs",
-                        tag: "Rugs",
-                        description:
-                          "Set this to true anytime the image seems to include or like it could include a rug.",
-                      },
-                      {
-                        label: "Indoor Wall Covering",
-                        tag: "Wallcovering",
-                        description:
-                          "Set this to true anytime the image seems to include indoor walls. If paint is present, set this to true as well. Literally any time you see a wall, set this to true.",
-                      },
-                      {
-                        label: "Paint",
-                        tag: "Paint",
-                        description:
-                          "Set this to true anytime the image seems to include walls. If wallcovering is present, set this to true as well. Literally any time you see a wall, set this to true.",
-                      },
-                      {
-                        label: "Hardwood Flooring",
-                        tag: "Engineered Hardwood",
-                        description:
-                          "Set this to true anytime the image seems to include or like it could include hardwood flooring.",
-                      },
-                      {
-                        label: "Bathroom Flooring",
-                        tag: "Bathroom Flooring",
-                        description:
-                          "Set this to true anytime the image seems to include or like it could include bathroom flooring.",
-                      },
-                      {
-                        label: "Shower Wall",
-                        tag: "Shower Wall",
-                        description:
-                          "Set this to true anytime the image seems to include or like it could include shower walls. Specifically shower. Bathrooms in general should have this set to true.",
-                      },
-                      {
-                        label: "Fabric",
-                        tag: "Fabric",
-                        description:
-                          "Set this to true anytime the image seems to include or like it could include fabric. Upholstery, curtains, etc.",
-                      },
-                      {
-                        label: "Home Exterior Patterns",
-                        tag: "Home Exterior",
-                        description:
-                          "Set this to true anytime the image seems to include or like it could include outdoor walls. Literally if you see an outdoor space, set this to true.",
-                      },
-                    ],
+                    title: "Category",
                     selectionType: "single",
                     filterType: "match_any",
+                    options,
                   },
                 ],
               },
@@ -141,12 +356,12 @@ export default function ECommerce() {
                     type: "text",
                     filterSidebarSectionKey: "description",
                     prompt:
-                      "Write 1 sentence describing in high detail the way you see the space in the image in terms of color, luminance, and style. Make a specific callout to something unique so a reader knows you actually saw the image. \n\n",
+                      "Write a single short sentence (maximum 10 words) describing in high detail the way you see the space in the image in terms of color, luminance, and style. Make a specific callout to something unique so a reader knows you actually saw the image. \n\n",
                     inferenceInputLabel:
                       "How the AI understands your image (editable)",
                   },
                   {
-                    title: "Category Selection",
+                    title: "Filter Selection",
                     description:
                       "Select the material(s) you want to change and are interested in getting recommendations for.",
                     type: "tags",
--- clients/search-component/src/TrieveModal/ModalContainer.tsx
+++ clients/search-component/src/TrieveModal/ModalContainer.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useMemo } from "react";
 import { useModalState } from "../utils/hooks/modal-context";
 import { useAtom } from "jotai";
 import { PdfViewer, pdfViewState } from "./PdfView/PdfViewer";
@@ -16,40 +16,39 @@ export const ModalContainer = () => {
 
   const componentClass = useSizing();
 
-  return (
-    <>
-      {createPortal(
-        <div
-          id="trieve-search-modal"
-          className={cn(
-            "tv-flex tv-flex-col tv-items-stretch",
-            "tv-scroll-smooth tv-rounded-lg focus:tv-outline-none tv-overflow-hidden tv-text-base tv-text-zinc-950 tv-bg-white",
-            `${mode === "chat" ? "chat-modal-mobile" : ""}${
-              props.theme === "dark" ? " dark tv-dark" : ""
-            }${
-              props.inline
-                ? " trieve-inline-modal tv-trieve-inline-modal"
-                : " trieve-popup-modal"
-            } ${props.type}`.trim(),
-            !props.inline && "md:tv-resize-y",
-            props.inline && "tv-border-2",
-            componentClass,
-          )}
-          style={{
-            zIndex: (props.zIndex ?? 1000) + 1,
-            display: open || props.inline ? "flex" : "none",
-          }}
-        >
-          {!props.inline && !fullscreenPdfState && <ChatModeSwitch />}
-          <Suspense>
-            {mode === "search" && !fullscreenPdfState && <SearchMode />}
-          </Suspense>
-          {mode === "chat" && !fullscreenPdfState && <ChatMode />}
-          {fullscreenPdfState && <PdfViewer {...fullscreenPdfState} />}
-          <Footer />
-        </div>,
-        document.body,
-      )}
-    </>
-  );
+  const component = useMemo(() => {
+    return (
+      <div
+        id="trieve-search-modal"
+        className={cn(
+          "tv-flex tv-flex-col tv-items-stretch",
+          "tv-scroll-smooth tv-rounded-lg focus:tv-outline-none tv-overflow-hidden tv-text-base tv-text-zinc-950 tv-bg-white",
+          `${mode === "chat" ? "chat-modal-mobile" : ""}${
+            props.theme === "dark" ? " dark tv-dark" : ""
+          }${
+            props.inline
+              ? " trieve-inline-modal tv-trieve-inline-modal"
+              : " trieve-popup-modal"
+          } ${props.type}`.trim(),
+          !props.inline && "md:tv-resize-y",
+          props.inline && "tv-border-2",
+          componentClass,
+        )}
+        style={{
+          zIndex: (props.zIndex ?? 1000) + 1,
+          display: open || props.inline ? "flex" : "none",
+        }}
+      >
+        {!props.inline && !fullscreenPdfState && <ChatModeSwitch />}
+        <Suspense>
+          {mode === "search" && !fullscreenPdfState && <SearchMode />}
+        </Suspense>
+        {mode === "chat" && !fullscreenPdfState && <ChatMode />}
+        {fullscreenPdfState && <PdfViewer {...fullscreenPdfState} />}
+        <Footer />
+      </div>
+    );
+  }, [props, mode, open, fullscreenPdfState, componentClass]);
+
+  return <>{props.inline ? component :  createPortal(component, document.body)}</>;
 };
