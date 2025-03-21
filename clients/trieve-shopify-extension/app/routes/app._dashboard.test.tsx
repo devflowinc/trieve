b59b0d1f7ca8f7e75f98a843049cd7ca547bb674
Drew Harris <dsharris10@gmail.com>
2025-03-21 11:48:09 -0700

    feat: kv shopify test

--- clients/trieve-shopify-extension/app/routes/app._dashboard.test.tsx
+++ clients/trieve-shopify-extension/app/routes/app._dashboard.test.tsx
@@ -1,16 +1,18 @@
-import { Box } from "@shopify/polaris";
-import { useQuery } from "@tanstack/react-query";
-import { Loader } from "app/loaders";
+import { Box, TextField } from "@shopify/polaris";
+import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
+import { Loader, setMetafield } from "app/loaders";
 import {
   createClientLoader,
   useClientAdminApi,
 } from "app/loaders/clientLoader";
 import { createServerLoader } from "app/loaders/serverLoader";
-import { themeSettingsQuery } from "app/queries/onboarding";
+import { testStringQuery, themeSettingsQuery } from "app/queries/onboarding";
+import { useState } from "react";
 import { type Dataset } from "trieve-ts-sdk";
 
 const load: Loader = async ({ queryClient, trieve, adminApiFetcher }) => {
   await queryClient.ensureQueryData(themeSettingsQuery(adminApiFetcher));
+  await queryClient.ensureQueryData(testStringQuery(adminApiFetcher));
 };
 
 export const loader = createServerLoader(load);
@@ -19,11 +21,30 @@ export const clientLoader = createClientLoader(load);
 export default function Dataset() {
   const fetcher = useClientAdminApi();
   const { data: theme } = useQuery(themeSettingsQuery(fetcher));
+  const { data: testString, refetch } = useSuspenseQuery(
+    testStringQuery(fetcher),
+  );
+  const [inputValue, setInputValue] = useState(testString);
+
+  const update = () => {
+    setMetafield(fetcher, "test-field", inputValue);
+  };
 
   return (
     <Box paddingBlockStart="400">
-      <div>TEst</div>
-      <pre>{JSON.stringify(theme)}</pre>
+      <pre>{JSON.stringify(testString)}</pre>
+      <button className="block" onClick={update}>
+        save
+      </button>
+      <button className="block" onClick={() => refetch()}>
+        Refetch
+      </button>
+      <TextField
+        autoComplete=""
+        label="enter string to save"
+        value={inputValue}
+        onChange={(e) => setInputValue(e)}
+      />
     </Box>
   );
 }
