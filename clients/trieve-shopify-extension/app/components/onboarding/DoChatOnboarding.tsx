61f2fa9025d0665042444d4fc2eec882f00fbb06
Drew Harris <dsharris10@gmail.com>
2025-03-26 17:49:48 -0500

    feat: link to online store preview in onboarding

--- clients/trieve-shopify-extension/app/components/onboarding/DoChatOnboarding.tsx
+++ clients/trieve-shopify-extension/app/components/onboarding/DoChatOnboarding.tsx
@@ -1,17 +1,18 @@
 import { Button, Text } from "@shopify/polaris";
+import { CheckIcon } from "@shopify/polaris-icons";
 import { useQuery } from "@tanstack/react-query";
 import { useTrieve } from "app/context/trieveContext";
 import { allChatsQuery } from "app/queries/analytics/chat";
 import { OnboardingBody } from "app/utils/onboarding";
 import { useEffect } from "react";
 
-export const DoChatOnboarding: OnboardingBody = ({
-  broadcastCompletion,
-  goToNextStep,
-}) => {
+export const DoChatOnboarding: OnboardingBody = ({ broadcastCompletion }) => {
   const { trieve } = useTrieve();
 
   const { data: chats } = useQuery(allChatsQuery(trieve, {}, 1));
+  const shopUrl = shopify?.config?.shop
+    ? `https://${shopify.config.shop}`
+    : null;
 
   useEffect(() => {
     if (chats && chats.topics.length > 0) {
@@ -21,16 +22,41 @@ export const DoChatOnboarding: OnboardingBody = ({
     }
   });
 
-  const complete = chats?.topics.length && chats?.topics.length > 0;
+  const complete =
+    chats?.topics.length !== undefined && chats?.topics.length > 0;
+
+  const viewStore = () => {
+    if (shopUrl) {
+      window.open(shopUrl, "_blank");
+    }
+  };
 
   return (
     <div className="grid w-full py-2">
-      <div className="flex flex-col gap-1">
-        <Text as="p">
-          {complete === true
-            ? "Component Added"
-            : "Chat with your products using the Trieve chat widget."}
-        </Text>
+      <div className="flex justify-between items-center gap-1">
+        {complete === false && (
+          <Text as="p">
+            Chat with your products using the Trieve chat widget.
+          </Text>
+        )}
+        {complete && (
+          <div className="w-full flex flex-col justify-center items-center">
+            <CheckIcon
+              fill="#2A845A"
+              color="#2A845A"
+              style={{ height: "50px" }}
+            />
+            <Text as="p">
+              {chats.topics.length}{" "}
+              {chats.topics.length === 1 ? "conversation" : "conversations"}{" "}
+              completed
+            </Text>
+          </div>
+        )}
+
+        {shopUrl && !complete && (
+          <Button onClick={viewStore}>View Store</Button>
+        )}
       </div>
     </div>
   );
