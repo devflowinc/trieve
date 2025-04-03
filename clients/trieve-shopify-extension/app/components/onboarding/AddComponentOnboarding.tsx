f2b29ff61c96a8db0fbd072be35ad85f071ffe82
Drew Harris <dsharris10@gmail.com>
2025-04-02 19:18:54 -0500

    feat: tutorial videos for component installation

--- clients/trieve-shopify-extension/app/components/onboarding/AddComponentOnboarding.tsx
+++ clients/trieve-shopify-extension/app/components/onboarding/AddComponentOnboarding.tsx
@@ -1,6 +1,7 @@
 import { useRouteLoaderData } from "@remix-run/react";
-import { Button, Text } from "@shopify/polaris";
-import { CheckIcon } from "@shopify/polaris-icons";
+import { Button, Text, Modal } from "@shopify/polaris";
+import { createPortal } from "react-dom";
+import { CheckIcon, ExternalIcon, XIcon } from "@shopify/polaris-icons";
 import { useQuery } from "@tanstack/react-query";
 import { useClientAdminApi } from "app/loaders/clientLoader";
 import {
@@ -11,7 +12,7 @@ import {
 import { cn } from "app/utils/cn";
 import { OnboardingBody } from "app/utils/onboarding";
 import { useShopName } from "app/utils/useShopName";
-import { useEffect, useMemo, useState } from "react";
+import { ReactNode, useEffect, useMemo, useState } from "react";
 import { ThemeSelect, ThemeChoice } from "./ThemeSelect"; // Import the new component
 
 const getShortThemeId = (fullGid: string): string | null => {
@@ -21,6 +22,58 @@ const getShortThemeId = (fullGid: string): string | null => {
     return match[1];
   }
   return null;
+};
+
+interface TutorialVideoProps {
+  title: string;
+  url: string;
+  editStoreButton?: ReactNode;
+}
+const TutorialVideo = (props: TutorialVideoProps) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <button className="opacity-80" onClick={() => setOpen(true)}>
+        Watch Tutorial
+      </button>
+      {open &&
+        createPortal(
+          <div
+            onClick={() => setOpen(false)}
+            className="bg-neutral-800/20 h-full w-full fixed top-0 left-0 z-[800]"
+          >
+            <div className="flex flex-col items-center justify-center h-full z-[900] w-full">
+              <div className="w-full h-full bg-neutral-800/10 flex flex-col items-center justify-center">
+                <div
+                  onClick={(e) => e.stopPropagation()}
+                  className="bg-white shadow rounded-lg overflow-hidden"
+                >
+                  <div className="p-2 pl-4 py-3 font-semibold flex justify-between items-center">
+                    {props.title}
+                    <div className="flex items-center gap-4">
+                      {props.editStoreButton}
+                      <button onClick={() => setOpen(false)} className="p-1">
+                        <XIcon width={23} height={23}></XIcon>
+                      </button>
+                    </div>
+                  </div>
+                  <iframe
+                    className="w-[80vw] aspect-video"
+                    src="https://www.youtube.com/embed/_FUHj3XF8O0?si=1WCOl7evpyH5j4WY"
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerPolicy="strict-origin-when-cross-origin"
+                    allowFullScreen
+                  ></iframe>
+                </div>
+              </div>
+            </div>
+          </div>,
+          document.body,
+        )}
+    </>
+  );
 };
 
 export const AddComponentOnboarding: OnboardingBody = ({
@@ -160,7 +213,18 @@ export const AddComponentOnboarding: OnboardingBody = ({
                 : "Add the global search component"}
             </Text>
             {!globalComplete && getDeeplink() && (
-              <Button onClick={openDeepLink}>Add to {themeName}</Button>
+              <div className="flex flex-col gap-2">
+                <Button onClick={openDeepLink}>Add to {themeName}</Button>
+                <TutorialVideo
+                  title="Add The Global Search Component"
+                  url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                  editStoreButton={
+                    <Button icon={ExternalIcon} onClick={openDeepLink}>
+                      Add to {themeName}
+                    </Button>
+                  }
+                />
+              </div>
             )}
             {globalComplete && (
               <CheckIcon
@@ -177,7 +241,18 @@ export const AddComponentOnboarding: OnboardingBody = ({
                 : "Add the product chat component"}
             </Text>
             {!pdpComplete && getPdpDeepLink() && (
-              <Button onClick={openPdpDeepLink}>Add to {themeName}</Button>
+              <div className="flex flex-col gap-2">
+                <Button onClick={openPdpDeepLink}>Add to {themeName}</Button>
+                <TutorialVideo
+                  title="Add The Product Chat Component"
+                  url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                  editStoreButton={
+                    <Button icon={ExternalIcon} onClick={openPdpDeepLink}>
+                      Add to {themeName}
+                    </Button>
+                  }
+                />
+              </div>
             )}
             {pdpComplete && (
               <CheckIcon
