95a2d814f184d7afe91f81cf2035098860305b2d
cdxker <fdenzell@gmail.com>
2025-04-02 19:56:16 -0700

    fix(dashboard): make MB conversion acurate

--- frontends/dashboard/src/utils/formatNumbers.ts
+++ frontends/dashboard/src/utils/formatNumbers.ts
@@ -27,11 +27,11 @@ export const formatStorageBytes = (bytes: number) => {
 export const formatStorageMb = (mb: number) => {
   if (mb < 1000) {
     return `${formatNumberWithCommas(mb)} mb`;
-  } else if (mb <= 9000000000) {
-    const gb = mb / 1000000000;
+  } else if (mb < 1000000) {
+    const gb = mb / 1000;
     return `${formatNumberWithCommas(gb)} gb`;
   } else {
-    const tb = mb / 50000000000;
+    const tb = mb / 1000000;
     return `${formatNumberWithCommas(tb)} tb`;
   }
 };
