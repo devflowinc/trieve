57dfaf9d0baf868ed81af9fd105501baaf03598c
cdxker <fdenzell@gmail.com>
2025-04-09 03:09:03 -0700

    feature: update example terraform to use t4 or l4 gpu's. removed cloudSQL / maanged redis in favor for cloud agnostic alternatives

--- terraform/gcloud/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "redis_host" {
- description = "The IP address of the instance."
- value = "${google_redis_instance.my_memorystore_redis_instance.host}"
-}
-
-output "postgres_host" {
- description = "The IP address of the instance."
- value = "${google_sql_database_instance.instance.ip_address}"
-}
--- terraform/gcloud/variables.tf
+++ terraform/gcloud/variables.tf
@@ -1,6 +1,6 @@
 variable "cluster_name" {
   default = "test-cluster"
-  type = string
+  type    = string
 }
 
 variable "project" {
@@ -9,31 +9,25 @@ variable "project" {
 
 variable "region" {
   default = "us-central1"
-  type = string
+  type    = string
 }
 
 variable "zone" {
   default = "us-central1-a"
-  type = string
-}
-
-variable "postgres-root-password" {
-    type = string
-  
-}
-
-variable "qdrant-machine-type" {
-    default = "e2-standard-4"
-    type = string
+  type    = string
 }
 
 variable "standard-machine-type" {
-    default = "e2-highcpu-4"
-    type = string
+  default = "n2-standard-4"
+  type    = string
 }
 
 variable "gpu-machine-type" {
-    default = "g2-standard-4"
-    type = string
-  
+  default = "g2-standard-4"
+  type    = string
+}
+
+variable "gpu-accelerator-type" {
+  default = "nvidia-l4"
+  type    = string
 }
