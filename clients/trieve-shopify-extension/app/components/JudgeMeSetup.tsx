f205508a8c427b652eb9113836ac839b1d301102
Drew Harris <drew@drewh.net>
2025-04-16 13:23:57 -0500

    feat: ui for connecting judge account

--- clients/trieve-shopify-extension/app/components/JudgeMeSetup.tsx
+++ clients/trieve-shopify-extension/app/components/JudgeMeSetup.tsx
@@ -1,7 +1,26 @@
-import { Button, Card, Text } from "@shopify/polaris";
-import { useMutation } from "@tanstack/react-query";
+import { Button, Card, SkeletonBodyText, Text } from "@shopify/polaris";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { type JudgeMeKeyInfo } from "app/routes/api.judgeme.info";
 
 export const JudgeMeSetup = () => {
+  const {
+    data: judgeMeInfo,
+    status,
+    error,
+  } = useQuery({
+    queryKey: ["judgeMeInfo"],
+    queryFn: async () => {
+      const repsonse = await fetch("/api/judgeme/info");
+      if (!repsonse.ok) {
+        throw new Error("Failed to get judge.me info", {
+          cause: await repsonse.text(),
+        });
+      }
+      const data = (await repsonse.json()) as JudgeMeKeyInfo;
+      return data.judgeMeKey;
+    },
+  });
+
   const getRedirectUrlMutation = useMutation({
     mutationFn: async () => {
       const response = await fetch("/auth/judgeme/login");
@@ -15,24 +34,39 @@ export const JudgeMeSetup = () => {
     },
   });
 
+  // Set inner component
+  let inner = null;
+  if (status === "pending") {
+    inner = <SkeletonBodyText></SkeletonBodyText>;
+  } else if (status === "error") {
+    inner = <div className="text-red-800">Error: {error.message}</div>;
+  } else if (status === "success" && !judgeMeInfo) {
+    inner = (
+      <Button
+        onClick={() => {
+          getRedirectUrlMutation.mutate();
+        }}
+      >
+        Setup Judge.me
+      </Button>
+    );
+  } else if (status === "success" && judgeMeInfo) {
+    inner = (
+      <div className="text-green-800">Successfully connected to Judge.me</div>
+    );
+  }
+
   return (
     <Card>
       <Text variant="headingLg" as="h1">
-        Reset Widgets and Onboarding
+        Connect Judge.me
       </Text>
       <Text variant="bodyMd" as="p">
-        Clears all app metafields, onboarding data, and widget configuration.
+        Connect your Judge.me reviews with your products in Trieve.
       </Text>
+
       <div className="h-3"></div>
-      <div className="flex items-center gap-4">
-        <Button
-          onClick={() => {
-            getRedirectUrlMutation.mutate();
-          }}
-        >
-          Setup Judge.me
-        </Button>
-      </div>
+      <div className="flex items-center gap-4">{inner}</div>
     </Card>
   );
 };
