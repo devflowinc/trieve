e28705175f18fe15636da167eae549eca8617c2a
Drew Harris <drew@drewh.net>
2025-05-04 21:38:42 -0500

    feat: chunk data hook

--- clients/trieve-shopify-extension/extensions/enrich-content-block/src/ActionExtension.tsx
+++ clients/trieve-shopify-extension/extensions/enrich-content-block/src/ActionExtension.tsx
@@ -7,8 +7,17 @@ import {
   useApi,
 } from "@shopify/ui-extensions-react/admin";
 import { TrieveProvider, useTrieve } from "./TrieveProvider";
+import { useChunkExtraContent } from "./useChunkExtraContent";
 
 const TARGET = "admin.product-details.block.render";
+
+function extractShopifyProductId(gid: string): string | undefined {
+  const parts = gid.substring(6).split("/"); // Remove "gid://" and then split
+  if (parts.length === 3 && parts[0] === "shopify" && parts[1] === "Product") {
+    return parts[2];
+  }
+  return undefined;
+}
 
 export default reactExtension(TARGET, () => (
   <TrieveProvider>
@@ -17,15 +26,18 @@ export default reactExtension(TARGET, () => (
 ));
 
 function App() {
-  const trieve = useTrieve();
   const { data } = useApi(TARGET);
   const productId = data.selected[0].id;
+  const simplifiedProductId = extractShopifyProductId(productId);
+
+  const { extraContent, loading, updateContent } =
+    useChunkExtraContent(simplifiedProductId);
 
   return (
     <AdminBlock title="Enrich Content">
       <BlockStack>
         <Text fontWeight="bold">Hi</Text>
-        <Text>Current product:{productId}</Text>
+        <Text>Current product:{simplifiedProductId}</Text>
       </BlockStack>
     </AdminBlock>
   );
