418a2aa895ae2021d976c692aadcab7e51ff4b84
Drew Harris <drew@drewh.net>
2025-05-07 14:32:31 -0500

    feat: llm fill-in

--- clients/trieve-shopify-extension/extensions/enrich-content-block/src/useChunkExtraContent.ts
+++ clients/trieve-shopify-extension/extensions/enrich-content-block/src/useChunkExtraContent.ts
@@ -4,6 +4,8 @@ import { useTrieve } from "./TrieveProvider";
 export const useChunkExtraContent = (productId: string | undefined) => {
   const trieve = useTrieve();
   const [extraContent, setExtraContent] = useState<string>("");
+  const [loading, setLoading] = useState(true);
+  const [aiLoading, setAILoading] = useState(false);
 
   // Fetch current product extra content chunk
   const getData = async () => {
@@ -28,6 +30,48 @@ export const useChunkExtraContent = (productId: string | undefined) => {
     } catch {
       setExtraContent("");
     }
+    setLoading(false);
+  };
+
+  const generateAIDescription = async () => {
+    if (!productId) {
+      console.info("Tried to generate AI description without id");
+      return;
+    }
+    setLoading(true);
+    setAILoading(true);
+    const topic = await trieve.createTopic({
+      owner_id: "shopify-enrich-content-block",
+      first_user_message: "Describe this product",
+      name: "Shopify Enrich Content Block",
+    });
+
+    const message = await trieve.createMessage({
+      topic_id: topic.id,
+      new_message_content:
+        "Describe this product. Generate a description for an online shop. Keep it to 3 sentences maximum. Do not include an introduction or welcome message",
+      use_group_search: true,
+      filters: {
+        must: [
+          {
+            field: "group_tracking_ids",
+            match_all: [productId],
+          },
+        ],
+      },
+      llm_options: {
+        stream_response: false,
+      },
+    });
+
+    const response = message.split("||").at(1);
+    if (!response) {
+      console.error("No response from AI");
+      return;
+    }
+    setExtraContent(response);
+    setLoading(false);
+    setAILoading(false);
   };
 
   const updateContent = async (content: string) => {
@@ -35,14 +79,16 @@ export const useChunkExtraContent = (productId: string | undefined) => {
       console.info("Tried to update product content without id");
       return;
     }
+    setLoading(true);
 
-    const result = await trieve.createChunk({
+    await trieve.createChunk({
       chunk_html: content,
       tracking_id: `${productId}-pdp-content`,
       upsert_by_tracking_id: true,
       weight: 100,
       group_tracking_ids: [productId],
     });
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -55,5 +101,8 @@ export const useChunkExtraContent = (productId: string | undefined) => {
   return {
     extraContent,
     updateContent,
+    generateAIDescription,
+    loading,
+    aiLoading,
   };
 };
