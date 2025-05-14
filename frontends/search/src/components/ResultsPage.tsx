9da73442921bed03021ebfa658464557b6bf2716
Dens Sumesh <dens.sumesh79@gmail.com>
2025-05-13 17:51:06 -0700

    feature: add autocomplete search over groups to the frontend

--- frontends/search/src/components/ResultsPage.tsx
+++ frontends/search/src/components/ResultsPage.tsx
@@ -433,6 +433,10 @@ const ResultsPage = (props: ResultsPageProps) => {
           searchRoute = "chunk/autocomplete";
           requestBody["extend_results"] =
             props.search.debounced.extendResults ?? false;
+          groupUnique = props.search.debounced.groupUniqueSearch;
+          if (groupUnique) {
+            searchRoute = "chunk_group/group_oriented_autocomplete";
+          }
         }
       }
 
