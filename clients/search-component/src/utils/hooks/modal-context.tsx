7c0ffd81260342f33c6abf7fbe324e11ddadf3b0
Vidyoot Senthil <vidyoots@gmail.com>
2025-05-27 14:02:33 -0700

    feature: add must not filter tool call

--- clients/search-component/src/utils/hooks/modal-context.tsx
+++ clients/search-component/src/utils/hooks/modal-context.tsx
@@ -95,9 +95,15 @@ export const defaultSearchToolCallOptions: SearchToolCallOptions = {
     "Call this tool anytime it seems like we need to skip the search step. This tool tells our system that the user is asking about what they were previously shown.",
 };
 
+export const defaultNotFilterToolCallOptions: NotFilterToolCallOptions = {
+  userMessageTextPrefix: "Here is the user query:",
+  toolDescription:
+    "Set to true if the query is not interested in the products they were shown previously or would like to see something different. Ensure that this is only set to true when the user wants to see something different from the previously returned results or is not interested in those previously returned results.",
+};
+
 export const defaultPriceToolCallOptions: PriceToolCallOptions = {
   toolDescription:
-    "Only call this function if the query includes details about a price. Decide on which price filters to apply to the available catalog being used within the knowledge base to respond. If the question is slightly like a product name, respond with no filters (all false).",
+    "Only call this function if the query includes details about a price. Decide on which price filters to apply to the available catalog being used within the knowledge base to respond. If the question is slightly like a product name, respond with no filters (all false). If they don't specify a price number, then respond with no filters (all false).",
   minPriceDescription:
     "Minimum price of the product. Only set this if a minimum price is mentioned in the query.",
   maxPriceDescription:
@@ -108,6 +114,11 @@ export interface PriceToolCallOptions {
   toolDescription: string;
   minPriceDescription?: string;
   maxPriceDescription?: string;
+}
+
+export interface NotFilterToolCallOptions {
+  userMessageTextPrefix?: string;
+  toolDescription: string;
 }
 
 export interface FilterSidebarSection {
@@ -147,7 +158,6 @@ export function isDefaultSearchQuery(
   return typeof question === "object" && "query" in question;
 }
 
-
 export type ModalProps = {
   datasetId: string;
   apiKey: string;
@@ -182,6 +192,7 @@ export type ModalProps = {
   relevanceToolCallOptions?: RelevanceToolCallOptions;
   priceToolCallOptions?: PriceToolCallOptions;
   searchToolCallOptions?: SearchToolCallOptions;
+  notFilterToolCallOptions?: NotFilterToolCallOptions;
   defaultSearchMode?: SearchModes;
   usePagefind?: boolean;
   type?: ModalTypes;
@@ -252,6 +263,7 @@ const defaultProps = {
   baseUrl: "https://api.trieve.ai",
   relevanceToolCallOptions: defaultRelevanceToolCallOptions,
   priceToolCallOptions: defaultPriceToolCallOptions,
+  notFilterToolCallOptions: defaultNotFilterToolCallOptions,
   defaultSearchMode: "search" as SearchModes,
   placeholder: "Search...",
   chatPlaceholder: "Ask Anything...",
--- frontends/dashboard/src/pages/dataset/PublicPageSettings.tsx
+++ frontends/dashboard/src/pages/dataset/PublicPageSettings.tsx
@@ -27,6 +27,7 @@ import {
   $OpenApiTs,
   ChunkMetadata,
   CrawlRequest,
+  NotFilterToolCallOptions,
   PriceToolCallOptions,
   PublicPageTabMessage,
   RelevanceToolCallOptions,
@@ -86,11 +87,17 @@ export const defaultSearchToolCallOptions: SearchToolCallOptions = {
 
 export const defaultPriceToolCallOptions: PriceToolCallOptions = {
   toolDescription:
-    "Only call this function if the query includes details about a price. Decide on which price filters to apply to the available catalog being used within the knowledge base to respond. If the question is slightly like a product name, respond with no filters (all false).",
+    "Only call this function if the query includes details about a price. Decide on which price filters to apply to the available catalog being used within the knowledge base to respond. If the question is slightly like a product name, respond with no filters (all false). If they don't specify a price number, then respond with no filters (all false).",
   minPriceDescription:
     "Minimum price of the product. Only set this if a minimum price is mentioned in the query.",
   maxPriceDescription:
     "Maximum price of the product. Only set this if a maximum price is mentioned in the query.",
+};
+
+export const defaultNotFilterToolCallOptions: NotFilterToolCallOptions = {
+  userMessageTextPrefix: "Here is the user query:",
+  toolDescription:
+    "Set to true if the query is not interested in the products they were shown previously or would like to see something different. Ensure that this is only set to true when the user wants to see something different from the previously returned results or is not interested in those previously returned results.",
 };
 
 const searchTypeOptions = [
@@ -1752,6 +1759,77 @@ const PublicPageControls = () => {
                   onInput={(e) =>
                     setExtraParams(
                       "searchToolCallOptions",
+                      "userMessageTextPrefix",
+                      e.currentTarget.value,
+                    )
+                  }
+                  rows="4"
+                  name="messageToQueryPrompt"
+                  id="messageToQueryPrompt"
+                  class="block w-full rounded-md border-[0.5px] border-neutral-300 px-3 py-1.5 shadow-sm placeholder:text-neutral-400 focus:outline-magenta-500 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+          </div>
+        </details>
+
+        <details class="my-4">
+          <summary class="cursor-pointer text-sm font-medium">
+            Not Filter Tool Options
+          </summary>
+          <div class="mt-4 space-y-4">
+            <div class="grid grid-cols-2 gap-4">
+              <div class="grow">
+                <div class="flex items-center gap-1">
+                  <label class="block" for="">
+                    Not Filter Tool Description
+                  </label>
+                  <Tooltip
+                    tooltipText="Description of the not filter tool provided to the model."
+                    body={
+                      <FaRegularCircleQuestion class="h-3 w-3 text-black" />
+                    }
+                  />
+                </div>
+                <textarea
+                  value={
+                    (extraParams.notFilterToolCallOptions?.toolDescription ||
+                      defaultNotFilterToolCallOptions.toolDescription) as string
+                  }
+                  onInput={(e) =>
+                    setExtraParams(
+                      "notFilterToolCallOptions",
+                      "toolDescription",
+                      e.currentTarget.value,
+                    )
+                  }
+                  rows="4"
+                  name="messageToQueryPrompt"
+                  id="messageToQueryPrompt"
+                  class="block w-full rounded-md border-[0.5px] border-neutral-300 px-3 py-1.5 shadow-sm placeholder:text-neutral-400 focus:outline-magenta-500 sm:text-sm sm:leading-6"
+                />
+              </div>
+              <div class="grow">
+                <div class="flex items-center gap-1">
+                  <label class="block" for="">
+                    Not Filter Prompt
+                  </label>
+                  <Tooltip
+                    tooltipText="Prompt to the model to use the search tool."
+                    body={
+                      <FaRegularCircleQuestion class="h-3 w-3 text-black" />
+                    }
+                  />
+                </div>
+                <textarea
+                  value={
+                    (extraParams.notFilterToolCallOptions
+                      ?.userMessageTextPrefix ||
+                      defaultNotFilterToolCallOptions.userMessageTextPrefix) as string
+                  }
+                  onInput={(e) =>
+                    setExtraParams(
+                      "notFilterToolCallOptions",
                       "userMessageTextPrefix",
                       e.currentTarget.value,
                     )
